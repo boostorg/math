97e50116bb566a110c7cbcaaca066604ee447523
John Maddock <john@johnmaddock.co.uk>
2006-06-10 12:42:55 +0000

    Fix warning
    [SVN r2975]

--- include/boost/math/concepts/real_concept.hpp
+++ include/boost/math/concepts/real_concept.hpp
@@ -240,7 +240,7 @@ inline float real_cast<float, concepts::real_concept>(concepts::real_concept r)
 template <>
 inline double real_cast<double, concepts::real_concept>(concepts::real_concept r)
 {
-   return r.value();
+   static_cast<double>(return r.value());
 }
 template <>
 inline long double real_cast<long double, concepts::real_concept>(concepts::real_concept r)
