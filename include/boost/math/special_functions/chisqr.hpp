818cfd0bbfab93ce378128c4f833a85b747a3d95
Paul A. Bristow <pbristow@hetp.u-net.com>
2006-06-23 18:30:40 +0000

    Improved error messages and try at warning and constraining probability outside 0 to 1
    [SVN r3027]

--- include/boost/math/special_functions/chisqr.hpp
+++ include/boost/math/special_functions/chisqr.hpp
@@ -35,7 +35,7 @@
 // For very large values of both degrees_of_freedom1 and degrees_of_freedom2,
 // greater than 10^5, a normal approximation is used.
 // If only one of degrees_of_freedom1 or degrees_of_freedom2 is
-// greater than 10^5 then a chi sq 2 approximation is used,
+// greater than 10^5 then a chisqr approximation is used,
 // see Abramowitz and Stegun (1965).
 // TODO???
 
@@ -45,8 +45,11 @@
 #include <boost/math/special_functions/gamma.hpp> // for gamma.
 	using boost::math::gamma_Q; //
 
-#include <boost/math/tools/roots.hpp> // for domain_error.
+#include <boost/math/tools/roots.hpp>
+#include <boost/math/tools/error_handling.hpp> // for domain_error.
 	using boost::math::tools::domain_error;
+	using boost::math::tools::overflow_error;
+	using boost::math::tools::underflow_error;
 
 namespace boost
 {
@@ -64,27 +67,33 @@ namespace boost
 
 			// Degrees of freedom argument may be integral, signed, or unsigned, or floating-point.
 			if(degrees_of_freedom <= DFT(0.))
-			{ // Degrees of freedom must not be negative!
-				domain_error<DFT>(BOOST_CURRENT_FUNCTION, "<= 0 degrees of freedom argument to the Chisqr function!");
+			{ // Degrees of freedom must be > 0!
+				return domain_error<DFT>(BOOST_CURRENT_FUNCTION, "degrees of freedom argument is %1%, but must be > 0 !", degrees_of_freedom);
 			}
-			if(chisqr < DFT(0.))
-			{ // chisqr must not be negative!
-				domain_error<DFT>(BOOST_CURRENT_FUNCTION, "< 0 chisqr argument to the Chisqr function!");
+			if(chisqr < FPT(0.))
+			{ // chisqr must be > 0!
+				return domain_error<FPT>(BOOST_CURRENT_FUNCTION, "chisqr argument is %1%, but must be > 0 !", chisqr);
 			}
 
 			// FPT degrees_of_freedom = static_cast<FPT>(degrees_of_freedom); // In case degrees_of_freedom was an integral type.
 
-			// Calculate probability of chisqr chisqr using the incomplete beta function.
+			// Calculate probability of chisqr using the incomplete beta function.
 			FPT probability = gamma_Q(static_cast<FPT>(degrees_of_freedom) * static_cast<FPT>(0.5), chisqr * static_cast<FPT>(0.5));
 			// Expect 0 <= probability <= 1.
-			//BOOST_ASSERT(probability >= static_cast<FPT>(0.));
-			//BOOST_ASSERT(probability <= static_cast<FPT>(1.));
-	  	// Numerical errors might cause it to drift slightly < 0 or > 1?
-	  	// This might cause trouble later.
-	  	// Might also consider constraining to fit the 0 to 1 range of probability
-	  	// by changing any value just outside to the limit 0 or 1?
-			// TODO this assert fires if with argument wrong AND BOOST_MATH_THROW_ON_DOMAIN_ERROR is NOT defined,
-			// so probability == NaN, which is not helpful, but uncertain what, if anything, to do here.			
+
+	  	// Numerical errors might cause probability to be slightly outside the range < 0 or > 1.
+	  	// This might cause trouble downstream, so warn, possibly throw exception, but constrain to the limits.
+			if (probability < static_cast<FPT>(0.))
+			{
+				domain_error<FPT>(BOOST_CURRENT_FUNCTION, "probability %1% is < 0, so has been constrained to zero !", probability);
+				return static_cast<FPT>(0.); // Constrain to zero.
+			}
+			if(probability > static_cast<FPT>(1.))
+			{
+				domain_error<FPT>(BOOST_CURRENT_FUNCTION, "probability %1% is > 1, so has been constrained to unity!", probability);
+				return static_cast<FPT>(1.); // Constrain to unity.
+			}
+
 			return probability;
 		//	return (chisqr > static_cast<FPT>(0)) ? static_cast<FPT>(1.)	- probability : probability;
 		} // chisqr
