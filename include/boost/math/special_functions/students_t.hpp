f6b8971322ab392490d326b6c0ca7c94122287f4
Paul A. Bristow <pbristow@hetp.u-net.com>
2006-06-27 09:45:59 +0000

    updated to use JM promoted arguments
    [SVN r3034]

--- include/boost/math/special_functions/students_t.hpp
+++ include/boost/math/special_functions/students_t.hpp
@@ -1,59 +1,194 @@
-// Copyright Paul A. Bristow 2006.
+// boost\math\special_functions\students_t.hpp
+
+// Copyright John Maddock & Paul A. Bristow 2006.
 
 // Use, modification and distribution are subject to the
 // Boost Software License, Version 1.0.
 // (See accompanying file LICENSE_1_0.txt
 // or copy at http://www.boost.org/LICENSE_1_0.txt)
 
-// boost\math\special_functions\students_t.hpp
-
 // This statistical distribution was published by W. Gosset in 1908 [23]. His employer,
 // Guinness Breweries, required him to publish under a pseudonym, so he chose “Student.”
+// "Student" (W.S. Gosset) (1908) The probable error of a mean. Biometrika 6(1):1--25
 
 // http://en.wikipedia.org/wiki/Student%27s_t-distribution
 
-// The cumulative distribution function is given by an incomplete beta function,
+// The cumulative distribution function is given by an incomplete beta function.
+// Abramowitz and Stegun, formula 26.5.27 (1966)
 
 // A recent review of calculating t quantiles is at:
 // http://www.mth.kcl.ac.uk/~shaww/web_page/papers/Tdistribution06.pdf
 
+// A lookup table of quantiles of the t distribution
+// for 1 to 25 in steps of 0.1 is provided in CSV form at:
+// www.mth.kcl.ac.uk/~shaww/web_page/papers/Tsupp/tquantiles.csv
+
 #ifndef BOOST_MATH_SPECIAL_STUDENTS_T_HPP
 #define BOOST_MATH_SPECIAL_STUDENTS_T_HPP
 
-#include <boost/math/special_functions/beta.hpp> // for ibeta.
-#include <boost/math/tools/roots.hpp> // for domain_error.
+#include <boost/math/special_functions/beta.hpp> // for ibeta(a, b, x).
+#include <boost/math/tools/roots.hpp> // for domain_error & logic_error.
+
+#include <boost/type_traits/is_floating_point.hpp>
+#include <boost/type_traits/is_integral.hpp>
+#include <boost/type_traits/is_same.hpp>
+#include <boost/mpl/if.hpp>
+
+#include <limits>
+	using std::numeric_limits;
 
 namespace boost
 {
 	namespace math
 	{
-		template <class DFT, class FPT>
-		FPT students_t(DFT degrees_of_freedom, FPT t); // Declaration.
+      namespace detail
+			{
+				// If either T or U is an integer type, 
+				// pretend it was a double (for the purposes of further analysis).
+				// Then pick the wider of the two floating-point types
+				// as the actual signature to forward to.
+				// For example:
+				// foo(int, short) -> foo(double, double);
+				// foo(int, float) -> foo(double, double);
+				// foo(int, double) -> foo(double, double);
+				// foo(double, float) -> foo(double, double);
+				// foo(any-int-or-float-type, long double) -> foo(long double, long double);
+				// but float foo(float, float) is unchanged.
+
+         template <class T>
+         struct promote_arg
+         { // if T integral, then promote to double.
+            typedef typename mpl::if_<is_integral<T>, double, T>::type type;
+         };
+
+         template <class T, class U>
+         struct promote_arg2 
+         { // Promote, if necessary, & pick the wider of the two floating-point types.
+           // for both parameter types, if integral promote to double.
+            typedef typename promote_arg<T>::type TP; // Perhaps promoted.
+            typedef typename promote_arg<U>::type UP; // Perhaps promoted.
+
+            typedef typename mpl::if_c<
+               is_floating_point<TP>::value && is_floating_point<UP>::value,
+               typename mpl::if_c<
+                  is_same<long double, TP>::value || is_same<long double, UP>::value,
+                  long double,
+                  typename mpl::if_c<
+                     is_same<double, TP>::value || is_same<double, UP>::value,
+                     double,
+                     float
+                  >::type
+               >::type,
+               typename mpl::if_<is_convertible<TP, UP>, UP, TP>::type
+            >::type type;
+         }; // promote_arg2
+
+		    template <class FPT>
+		    FPT students_t_imp(FPT degrees_of_freedom, FPT t)
+		    { // Implementation of Probability of Student's t.
+			    // Abramowitz and Stegun, formula 26.5.27 (1966)
+
+	        using boost::math::ibeta; // ibeta(a, b, x)
+	        using boost::math::tools::domain_error;
+	        using boost::math::tools::logic_error;
+
+			    // Degrees_of_freedom argument may be integral, signed, or unsigned, or floating point.
+          // If necessary, it has already been promoted from an integral type.
+			    if(degrees_of_freedom <= 0)
+			    { // Degrees of freedom must be > 0!
+				    return domain_error<FPT>(BOOST_CURRENT_FUNCTION, "degrees of freedom argument is %1%, but must be > 0 !", degrees_of_freedom);
+			    }
+			    FPT z = degrees_of_freedom / (degrees_of_freedom + t * t);
+			    // Calculate probability of Student's t using the incomplete beta function.
+			    // probability = ibeta(degrees_of_freedom/2, 1/2, degrees_of_freedom/ (degrees_of_freedom + t*t))
+          FPT probability = ibeta(degrees_of_freedom / 2, static_cast<FPT>(0.5), z) / 2;
+			    // Check 0 <= probability probability <= 1.  
+	  	    // Numerical errors might cause probability to be slightly outside the range < 0 or > 1.
+	  	    // This might cause trouble downstream, so warn, possibly throw exception, but constrain to the limits.
+			    if (probability < static_cast<FPT>(0.))
+			    {
+				    logic_error<FPT>(BOOST_CURRENT_FUNCTION, "probability %1% is < 0, so has been constrained to zero !", probability);
+				    return static_cast<FPT>(0.); // Constrain to zero if logic_error does not throw.
+			    }
+			    if(probability > static_cast<FPT>(1.))
+			    {
+				    logic_error<FPT>(BOOST_CURRENT_FUNCTION, "probability %1% is > 1, so has been constrained to unity!", probability);
+				    return static_cast<FPT>(1.); // Constrain to unity if logic_error does not throw.
+			    }
+			    return (t > 0 ? 1	- probability : probability);
+		    } // students_t_imp
+    } // namespace detail
 
 		template <class DFT, class FPT>
-		FPT students_t(DFT degrees_of_freedom, FPT t)
-		{ // Probability of Student's t implementation.
-			using boost::math::ibeta; // ibeta(a, b, x)
-			using boost::math::tools::domain_error;
-
-			// Degrees of freedom argument may be integral, signed, or unsigned, or floating point.
-			if(degrees_of_freedom <= DFT(0))
-			{ // Degrees of freedom must not be negative!
-				domain_error<DFT>(BOOST_CURRENT_FUNCTION, "Negative degrees of freedom argument to the students_t function!");
-			}
-			FPT dd = static_cast<FPT>(degrees_of_freedom); // In case degrees_of_freedom was an integral type.
-			FPT z = dd/(dd + t * t);
-			// Calculate probability of Student's t using the incomplete beta function.
-			FPT result = (static_cast<FPT>(0.5) * ibeta(dd * static_cast<FPT>(0.5), static_cast<FPT>(0.5), z));
-			// Check 0 <= probability result <= 1.
-			BOOST_ASSERT(result >= static_cast<FPT>(0));
-			BOOST_ASSERT(result <= static_cast<FPT>(1.));
-	  	// Numerical errors might cause it to drift slightly < 0 or > 1.
-	  	// This might cause trouble later.
-	  	// Might also consider constraining to fit the range 0 to 1
-	  	// by changing any value just outside to the limit 0 or 1?
-			return (t > static_cast<FPT>(0)) ? static_cast<FPT>(1.)	- result : result;
+      inline typename detail::promote_arg2<FPT, DFT>::type // return type is the wider of the two (?promoted) floating point types.
+         students_t(DFT degrees_of_freedom, FPT t)
+		{ 
+         typedef typename detail::promote_arg2<FPT, DFT>::type promote_type; // Arguments type.
+         return detail::students_t_imp(static_cast<promote_type>(degrees_of_freedom), static_cast<promote_type>(t));
 		} // students_t
+
+    namespace detail
+    {
+ 		  template <class FPT>
+		  FPT students_t_inv_imp(FPT degrees_of_freedom, FPT probability)
+		  { // Inverse cumulative Distribution Function or Quintile (percentile / 100) of Student's t Probability.
+			  using boost::math::ibeta_inv; // ibeta(a, b, x)
+			  using boost::math::tools::domain_error;
+
+			  // Degrees of freedom argument may be integral, signed, or unsigned, or floating point.
+			  if(degrees_of_freedom <= 0)
+			  { // Degrees of freedom must be > 0!
+				  return domain_error<FPT>(BOOST_CURRENT_FUNCTION, "degrees of freedom argument is %1%, but must be > 0 !", degrees_of_freedom);
+			  }
+			  if((probability < 0) || (probability > 1))
+			  { // probability must be >= 0 and <= 1!
+				  return domain_error<FPT>(BOOST_CURRENT_FUNCTION, "probability argument is %1%, but must be >= 0 and <= 1 !", probability);
+			  }
+			  // Special cases, regardless of degrees_of_freedom.
+			  if (probability == 0)
+				  return -numeric_limits<FPT>::infinity();
+			  if (probability == 1)
+				  return +numeric_limits<FPT>::infinity();
+			  if (probability == static_cast<FPT>(0.5))
+				  return 0; 
+			  // Calculate quantile of Student's t using the incomplete beta function.
+			  if ((probability > FPT(0.25)) && (probability < FPT(0.75)) )
+			  { // probability is middling.
+				  FPT z = 1 - 2 * probability;
+				  z = ibeta_inv(FPT(0.5), degrees_of_freedom / 2 , fabs(z));
+				  FPT t = sqrt(degrees_of_freedom * z / (1 - z));
+				  return (probability < FPT(0.5)) ? -t : t;
+			  }
+			  else
+			  { // probability is small or large.
+				  int sign = -1;
+				  if (probability >= 0.5)
+				  { // large and > 0.75
+					  probability = 1 - probability;
+					  sign = +1;
+				  }
+				  FPT z = ibeta_inv(degrees_of_freedom / 2, static_cast<FPT>(0.5), 2 * probability);
+				  if (((numeric_limits<FPT>::max)() * z) < degrees_of_freedom)
+				  {
+					  return sign * (numeric_limits<FPT>::max)();
+				  }
+				  else
+				  {
+					  return sign * sqrt(degrees_of_freedom/ z - degrees_of_freedom);
+				  }
+			  }
+		  } // students_t_inv_imp
+    } // namespace detail
+
+    template <class DFT, class FPT>
+    inline typename detail::promote_arg2<FPT, DFT>::type // return type is the wider of the two (?promoted) floating point types.
+      students_t_inv(DFT degrees_of_freedom, FPT probability)
+		{ 
+         typedef typename detail::promote_arg2<FPT, DFT>::type promote_type; // Arguments type.
+         return detail::students_t_inv_imp(static_cast<promote_type>(degrees_of_freedom), static_cast<promote_type>(probability));
+		} // students_t_inv
+
+
 	} // namespace math
 } // namespace boost
 
