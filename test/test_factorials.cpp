a46ac4c5aa30db3c7cb3fbf794e6a59fde5012c5
Paul A. Bristow <pbristow@hetp.u-net.com>
2006-07-06 09:57:43 +0000

    disabled warnings in test and added output.
    [SVN r3067]

--- test/test_factorials.cpp
+++ test/test_factorials.cpp
@@ -3,6 +3,13 @@
 //  Boost Software License, Version 1.0. (See accompanying file
 //  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
+#ifdef _MSC_VER
+#  pragma warning(disable: 4127) // conditional expression is constant.
+#  pragma warning(disable: 4512) // assignment operator could not be generated.
+#  pragma warning(disable: 4996) // 'std::char_traits<char>::copy' was declared deprecated.
+#endif
+
+
 #include <boost/math/concepts/real_concept.hpp>
 #include <boost/test/included/test_exec_monitor.hpp>
 #include <boost/test/floating_point_comparison.hpp>
@@ -15,10 +22,10 @@ template <class T>
 void test_spots(T)
 {
    //
-   // basic sanity checks, tolerance is 10 decimal places expressed as a percentage,
+   // Basic sanity checks, tolerance is 10 decimal places expressed as a percentage,
    // One check per domain of the implementation:
    //
-   T tolerance = boost::math::tools::epsilon<T>() * 100 * 2;  // 2 eps as a persent
+   T tolerance = boost::math::tools::epsilon<T>() * 100 * 2;  // 2 eps as a percent.
    BOOST_CHECK_CLOSE(
       ::boost::math::factorial<T>(0),
       static_cast<T>(1), tolerance);
@@ -38,7 +45,7 @@ void test_spots(T)
       ::boost::math::unchecked_factorial<T>(10),
       static_cast<T>(3628800L), tolerance);
 
-   tolerance = boost::math::tools::epsilon<T>() * 100 * 20;  // 20 eps as a persent
+   tolerance = boost::math::tools::epsilon<T>() * 100 * 20;  // 20 eps as a percent.
    unsigned i = boost::math::max_factorial<T>::value;
    BOOST_CHECK_CLOSE(
       ::boost::math::unchecked_factorial<T>(i),
@@ -63,6 +70,24 @@ int test_main(int, char* [])
    test_spots(boost::math::concepts::real_concept(0.1));
    return 0;
 }
+
+/*
+
+Output is:
+
+------ Build started: Project: test_factorials, Configuration: Debug Win32 ------
+Compiling...
+test_factorials.cpp
+Linking...
+Autorun "i:\boost-06-05-03-1300\libs\math\test\Math_test\debug\test_factorials.exe"
+Running 1 test case...
+*** No errors detected
+Build Time 0:05
+Build log was saved at "file://i:\boost-06-05-03-1300\libs\math\test\Math_test\test_factorials\Debug\BuildLog.htm"
+test_factorials - 0 error(s), 0 warning(s)
+========== Build: 1 succeeded, 0 failed, 0 up-to-date, 0 skipped ==========
+
+*/
 
 
 
