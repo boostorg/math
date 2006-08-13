8cc9b6d50ddc3aaa59bdc2e1188584b80380b91a
Paul A. Bristow <pbristow@hetp.u-net.com>
2006-08-13 11:39:58 +0000

    commented out distributions now obselete. Need to redo all these with the distribution classes.
    [SVN r3138]

--- include/boost/math/special_functions/math_fwd.hpp
+++ include/boost/math/special_functions/math_fwd.hpp
@@ -1,5 +1,7 @@
 // math_fwd.hpp
 
+// TODO revise completely for new distribution classes.
+
 // Copyright Paul A. Bristow 2006.
 // Copyright John Maddock 2006.
 
@@ -42,43 +44,43 @@ namespace boost
 
    // Binomial distribution.
 
-   template <class AT, class RT> // Binomial distribution (k, n, x)
-   // Probability of number of events between 0 and k-1 inclusive, if expected probability of success events is success_fraction.
-   typename tools::promote_arg3<AT, AT, RT>::type
-   // Return type is the wider of the two (perhaps promoted) floating-point types.
-   binomial(AT k, AT n, RT success_fraction);
+   //template <class AT, class RT> // Binomial distribution (k, n, x)
+   //// Probability of number of events between 0 and k-1 inclusive, if expected probability of success events is success_fraction.
+   //typename tools::promote_arg3<AT, AT, RT>::type
+   //// Return type is the wider of the two (perhaps promoted) floating-point types.
+   //binomial(AT k, AT n, RT success_fraction);
 
-   template <class AT, class RT> // Binomial distribution complement (k, n, x)
-   // Probability of number of events between 0 and k-1 inclusive, if expected mean is x.
-   typename tools::promote_arg3<AT, AT, RT>::type
-   binomial_c(AT k, AT n, RT success_fraction);
+   //template <class AT, class RT> // Binomial distribution complement (k, n, x)
+   //// Probability of number of events between 0 and k-1 inclusive, if expected mean is x.
+   //typename tools::promote_arg3<AT, AT, RT>::type
+   //binomial_c(AT k, AT n, RT success_fraction);
 
-   template <class AT, class RT>
-   // success_fraction if number of events between 0 and k-1 inclusive, and probability p.
-   typename tools::promote_arg3<AT, AT, RT>::type
-   binomial_inv(AT k,  AT n, RT probability); // Binomial distribution inverse (k, n, p)
+   //template <class AT, class RT>
+   //// success_fraction if number of events between 0 and k-1 inclusive, and probability p.
+   //typename tools::promote_arg3<AT, AT, RT>::type
+   //binomial_inv(AT k,  AT n, RT probability); // Binomial distribution inverse (k, n, p)
 
    // cbrt - cube root.
    template <class RT>
    RT cbrt(RT z);
 
    // chi_sqr functions.
-   template <class AT, class RT> // probability chisqr(degrees_of_freedom, chisqr)
-   typename tools::promote_arg2<RT, AT>::type
-   // return type is the wider of the two (?promoted) floating point types.
-   chisqr(AT degrees_of_freedom, RT chisqr);
+   //template <class AT, class RT> // probability chisqr(degrees_of_freedom, chisqr)
+   //typename tools::promote_arg2<RT, AT>::type
+   //// return type is the wider of the two (?promoted) floating point types.
+   //chisqr(AT degrees_of_freedom, RT chisqr);
 
-   template <class AT, class RT> // complement probability chisqr_c(degrees_of_freedom, chisqr)
-   typename tools::promote_arg2<RT, AT>::type
-   chisqr_c(AT degrees_of_freedom, RT chisqr);
+   //template <class AT, class RT> // complement probability chisqr_c(degrees_of_freedom, chisqr)
+   //typename tools::promote_arg2<RT, AT>::type
+   //chisqr_c(AT degrees_of_freedom, RT chisqr);
 
-   template <class AT, class RT> // chisqr = chisqr_inv(degrees_of_freedom,  probability)
-   typename tools::promote_arg2<RT, AT>::type
-   chisqr_inv(AT degrees_of_freedom, RT probability);
+   //template <class AT, class RT> // chisqr = chisqr_inv(degrees_of_freedom,  probability)
+   //typename tools::promote_arg2<RT, AT>::type
+   //chisqr_inv(AT degrees_of_freedom, RT probability);
 
-   template <class AT, class RT> // degrees_of_freedom = chisqr_inv_df(chisqr, probability)
-   typename tools::promote_arg2<RT, AT>::type // <RT, AT> but both RT.
-   chisqr_df_inv(RT chisqr, RT probability);
+   //template <class AT, class RT> // degrees_of_freedom = chisqr_inv_df(chisqr, probability)
+   //typename tools::promote_arg2<RT, AT>::type // <RT, AT> but both RT.
+   //chisqr_df_inv(RT chisqr, RT probability);
 
    // erf & erfc error functions.
    template <class RT> // Error function.
@@ -105,22 +107,22 @@ namespace boost
 
    // Fisher-Snedecor functions.
 
-   template <class AT, class RT>
-   typename tools::promote_arg3<AT, AT, RT>::type
-   // return type is the wider of the three (possibly promoted) floating point types.
-   fisher(AT degrees_of_freedom1, AT degrees_of_freedom2, RT fisher);
+   //template <class AT, class RT>
+   //typename tools::promote_arg3<AT, AT, RT>::type
+   //// return type is the wider of the three (possibly promoted) floating point types.
+   //fisher(AT degrees_of_freedom1, AT degrees_of_freedom2, RT fisher);
 
-   template <class AT, class RT>
-   typename tools::promote_arg3<AT, AT, RT>::type
-   fisher_c(AT degrees_of_freedom1, AT degrees_of_freedom2, RT fisher);
+   //template <class AT, class RT>
+   //typename tools::promote_arg3<AT, AT, RT>::type
+   //fisher_c(AT degrees_of_freedom1, AT degrees_of_freedom2, RT fisher);
 
-   template <class AT, class RT>
-   typename tools::promote_arg3<AT, AT, RT>::type
-   fisher_inv(AT degrees_of_freedom1, AT degrees_of_freedom2, RT fisher);
+   //template <class AT, class RT>
+   //typename tools::promote_arg3<AT, AT, RT>::type
+   //fisher_inv(AT degrees_of_freedom1, AT degrees_of_freedom2, RT fisher);
 
-   template <class AT, class RT>
-   typename tools::promote_arg3<AT, AT, RT>::type
-   fisher_c_inv(AT degrees_of_freedom1, AT degrees_of_freedom2, RT fisher);
+   //template <class AT, class RT>
+   //typename tools::promote_arg3<AT, AT, RT>::type
+   //fisher_c_inv(AT degrees_of_freedom1, AT degrees_of_freedom2, RT fisher);
 
    // Fpclassify - classify floating-point as NaN or infinity...
    template <class T>
@@ -163,9 +165,9 @@ namespace boost
    //typename tools::promote_arg2<RT, AT>::type
    //students_t(AT degrees_of_freedom1, RT probability); //  probability from t.
 
-   template <class AT, class RT>
-   typename tools::promote_arg2<RT, AT>::type
-   students_t_inv(AT degrees_of_freedom1, RT probability); // t from probability.
+   //template <class AT, class RT>
+   //typename tools::promote_arg2<RT, AT>::type
+   //students_t_inv(AT degrees_of_freedom1, RT probability); // t from probability.
 
    // sqrt
    template <class T>
