a151830ed47b3462754bf89d3040051d5aaf4041
John Maddock <john@johnmaddock.co.uk>
2006-08-30 15:48:21 +0000

    Fixed corner cases. Implemented parameter estimator.
    [SVN r3167]

--- include/boost/math/distributions/chi_squared.hpp
+++ include/boost/math/distributions/chi_squared.hpp
@@ -15,11 +15,6 @@
 
 namespace boost{ namespace math{ 
    
-namespace detail{
-
-
-} // namespace detail
-
 template <class RealType>
 class chi_squared_distribution
 {
@@ -40,10 +35,10 @@ public:
 
    // Parameter estimation:
    static RealType estimate_degrees_of_freedom(
-      RealType difference_from_mean,
+      RealType difference_from_variance,
       RealType alpha,
       RealType beta,
-      RealType sd,
+      RealType variance,
       RealType hint = 100);
 
 private:
@@ -66,10 +61,28 @@ RealType pdf(const chi_squared_distribution<RealType>& dist, const RealType& chi
          BOOST_CURRENT_FUNCTION, degrees_of_freedom, &error_result))
       return error_result;
 
-   if((chi_square <= 0) || !(boost::math::isfinite)(chi_square))
+   if((chi_square < 0) || !(boost::math::isfinite)(chi_square))
    {
       return tools::domain_error<RealType>(
          BOOST_CURRENT_FUNCTION, "Chi Square parameter was %1%, but must be > 0 !", chi_square);
+   }
+
+   if(chi_square == 0)
+   {
+      // Handle special cases:
+      if(degrees_of_freedom < 2)
+      {
+         return tools::overflow_error<RealType>(
+            BOOST_CURRENT_FUNCTION, 0);
+      }
+      else if(degrees_of_freedom == 2)
+      {
+         return 0.5f;
+      }
+      else
+      {
+         return 0;
+      }
    }
 
    return gamma_P_derivative(degrees_of_freedom / 2, chi_square / 2) / 2;
@@ -85,7 +98,7 @@ RealType cdf(const chi_squared_distribution<RealType>& dist, const RealType& chi
          BOOST_CURRENT_FUNCTION, degrees_of_freedom, &error_result))
       return error_result;
 
-   if((chi_square <= 0) || !(boost::math::isfinite)(chi_square))
+   if((chi_square < 0) || !(boost::math::isfinite)(chi_square))
    {
       return tools::domain_error<RealType>(
          BOOST_CURRENT_FUNCTION, "Chi Square parameter was %1%, but must be > 0 !", chi_square);
@@ -120,7 +133,7 @@ RealType cdf(const complemented2_type<chi_squared_distribution<RealType>, RealTy
          BOOST_CURRENT_FUNCTION, degrees_of_freedom, &error_result))
       return error_result;
 
-   if((chi_square <= 0) || !(boost::math::isfinite)(chi_square))
+   if((chi_square < 0) || !(boost::math::isfinite)(chi_square))
    {
       return tools::domain_error<RealType>(
          BOOST_CURRENT_FUNCTION, "Chi Square parameter was %1%, but must be > 0 !", chi_square);
@@ -156,6 +169,77 @@ inline RealType variance(const chi_squared_distribution<RealType>& dist)
 { // Variance of Chi-Squared distribution = 2v.
   return 2 * dist.degrees_of_freedom();
 } // variance
+
+//
+// Parameter estimation comes last:
+//
+namespace detail{
+
+template <class RealType>
+struct df_estimator
+{
+   df_estimator(RealType a, RealType b, RealType variance, RealType delta)
+      : alpha(a), beta(b), ratio(delta/variance) {}
+
+   RealType operator()(const RealType& df)
+   {
+      if(df <= tools::min_value<RealType>())
+         return 1;
+      chi_squared_distribution<RealType> cs(df);
+
+      RealType result;
+      if(ratio > 0)
+      {
+         RealType r = 1 + ratio;
+         result = cdf(cs, quantile(complement(cs, alpha)) / r) - beta;
+      }
+      else
+      {
+         RealType r = 1 - ratio;
+         result = cdf(cs, quantile(cs, alpha) / r) - beta;
+      }
+      return result;
+   }
+private:
+   RealType alpha, beta, ratio;
+};
+
+}
+
+template <class RealType>
+RealType chi_squared_distribution<RealType>::estimate_degrees_of_freedom(
+   RealType difference_from_variance,
+   RealType alpha,
+   RealType beta,
+   RealType variance,
+   RealType hint = 100)
+{
+   //
+   // Check for domain errors:
+   //
+   RealType error_result;
+   if(false == detail::check_probability(
+         BOOST_CURRENT_FUNCTION, alpha, &error_result)
+         && detail::check_probability(BOOST_CURRENT_FUNCTION, beta, &error_result))
+      return error_result;
+
+   if(hint <= 0)
+      hint = 1;
+
+   detail::df_estimator<RealType> f(alpha, beta, variance, difference_from_variance);
+   tools::eps_tolerance<RealType> tol(tools::digits<RealType>());
+   boost::uintmax_t max_iter = 10000;
+   std::pair<RealType, RealType> r = tools::bracket_and_solve_root(f, hint, RealType(2), false, tol, max_iter);
+   RealType result = r.first + (r.second - r.first) / 2;
+   if(max_iter == 10000)
+   {
+      tools::logic_error<RealType>(BOOST_CURRENT_FUNCTION, "Unable to locate solution in a reasonable time:"
+         " either there is no answer to how many degrees of freedom are required"
+         " or the answer is infinite.  Current best guess is %1%", result);
+   }
+   return result;
+}
+
 
 
 } // namespace math
