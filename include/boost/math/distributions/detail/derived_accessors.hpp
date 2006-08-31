5577b49a33ec4956d3849284bb494935168b5579
Paul A. Bristow <pbristow@hetp.u-net.com>
2006-08-31 16:56:28 +0000

    Spelling and a few comments & refs.
    [SVN r3174]

--- include/boost/math/distributions/detail/derived_accessors.hpp
+++ include/boost/math/distributions/detail/derived_accessors.hpp
@@ -6,24 +6,25 @@
 #ifndef BOOST_STATS_DERIVED_HPP
 #define BOOST_STATS_DERIVED_HPP
 
-//
 // This file implements various common properties of distributions
-// that can be implemented in terms of other properties.
+// that can be implemented in terms of other properties:
+// variance OR standard deviation (see note below),
+// hazard, cumulative hazard (chf), coefficient_of_variation.
 //
-// Of course there may be more efficient means of implementing these 
+// Note that while both variance and standard_deviation are provided
+// here, each distribution MUST SPECIALIZE AT LEAST ONE OF THESE
+// otherwise these two versions will just call each other over and over
+// until stack space runs out ...
+
+// Of course there may be more efficient means of implementing these
 // that are specific to a particular distribution, but these generic
 // versions give these properties "for free" with most distributions.
 //
 // In order to make use of this header, it must be included AT THE END
-// of the distribution header, AFTER the distribution and it's core
+// of the distribution header, AFTER the distribution and its core
 // property accessors have been defined: this is so that compilers
 // that implement 2-phase lookup and early-type-checking of templates
 // can find the definitions refered to herein.
-//
-// Note that while both variance and standard_deiviation are provided
-// here, each distribution must specialise at least one of these
-// otherwise these two versions will just call each other over and over
-// until stack space runs out....
 //
 namespace boost{ namespace math{
 
@@ -46,7 +47,8 @@ inline typename Distribution::value_type variance(const Distribution& dist)
 
 template <class Distribution, class RealType>
 typename Distribution::value_type hazard(const Distribution& dist, const RealType& x)
-{
+{ // hazard function
+  // http://www.itl.nist.gov/div898/handbook/eda/section3/eda362.htm#HAZ
    typedef typename Distribution::value_type value_type;
    value_type p = cdf(complement(dist, x));
    value_type d = pdf(dist, x);
@@ -56,14 +58,15 @@ typename Distribution::value_type hazard(const Distribution& dist, const RealTyp
    if(d == 0)
    {
       // This protects against 0/0, but is it the right thing to do?
-      return 0;  
+      return 0;
    }
    return d / p;
 }
 
 template <class Distribution, class RealType>
 inline typename Distribution::value_type chf(const Distribution& dist, const RealType& x)
-{
+{ // cumulative hazard function.
+  // http://www.itl.nist.gov/div898/handbook/eda/section3/eda362.htm#HAZ
    using namespace std;
    return -log(cdf(complement(dist, x)));
 }
@@ -79,7 +82,6 @@ typename Distribution::value_type coefficient_of_variation(const Distribution& d
          BOOST_CURRENT_FUNCTION, 0);
    return d / m;
 }
-
 
 } // namespace math
 } // namespace boost
