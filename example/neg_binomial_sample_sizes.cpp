a65a57f36065b10f66dd4bd50bfcc7bdd8a60127
Paul A. Bristow <pbristow@hetp.u-net.com>
2006-11-22 18:40:53 +0000

    Revised after split from confidence limits.
    [SVN r3438]

--- example/neg_binomial_sample_sizes.cpp
+++ example/neg_binomial_sample_sizes.cpp
@@ -12,79 +12,48 @@
 #endif
 
 #include <iostream>
+using std::cout;
+using std::endl;
+using std::fixed;
+using std::right;
 #include <iomanip>
-#include <boost/math/distributions/negative_binomial.hpp>
+using std::setprecision;
+using std::setw;
 
-void estimate_max_sample_size(double p, unsigned successes)
+#include <boost/math/distributions/negative_binomial.hpp>
+using boost::math::negative_binomial;
+  // RealType estimate_number_of_trials(
+  // RealType k,     // number of failures, k >= 0.
+  // RealType p,     // success fraction 0 <= p <= 1.
+  // RealType probability) // probability threshold 0 <= p <= 0.
+ 
+void estimate_number_of_trials(double failures, double p)
 {
    // trials = number of trials
+   // failures = number of failures before achieving required success(es).
    // p         = success ratio.
-   // successes = Total number of observed successes.
+   // successes = required number of successes.
    //
    // Calculate how many trials we can have to ensure the
-   // maximum number of successes does not exceed "successes".
-   // A typical use would be failure analysis, where you want
-   // zero or fewer "successes" with some probability.
-   //
-   using namespace std;
-   using namespace boost::math;
+   // required number of successes DOES exceed "successes".
 
-   // Print out general info:
-   cout <<
-      "________________________\n"
-      "Maximum Number of Trials\n"
-      "________________________\n\n";
-   cout << setprecision(7);
-   cout << setw(40) << left << "Success ratio" << "=  " << p << "\n";
-   cout << setw(40) << left << "Maximum Number of \"successes\" permitted" << "=  " << successes << "\n";
-   //
-   // Define a table of confidence intervals:
-   //
+   // Define a table of significance levels:
    double alpha[] = { 0.5, 0.25, 0.1, 0.05, 0.01, 0.001, 0.0001, 0.00001 };
-   //
+
+  cout << "\n""Number of failures = " << failures;
+  cout << ",   Success fraction = " << 100 * p << "%" << endl;
    // Print table header:
-   //
    cout << "\n\n"
            "____________________________\n"
            "Confidence        Max Number\n"
            " Value (%)        Of Trials \n"
            "____________________________\n";
-   //
    // Now print out the data for the table rows.
-   //
-   for(unsigned i = 0; i < sizeof(alpha)/sizeof(alpha[0]); ++i)
-   {
-      // Confidence value:
-      cout << fixed << setprecision(3) << setw(10) << right << 100 * (1-alpha[i]);
-      // calculate trials:
-      double t = negative_binomial::estimate_number_of_trials(complement(successes, p, alpha[i]));
-      t = floor(t);
-      // Print Trials:
-      cout << fixed << setprecision(0) << setw(15) << right << t << endl;
-   }
-   cout << endl;
-} // void estimate_max_sample_size(double p, unsigned successes)
-
-void estimate_number_of_trials(double failures, double p)
-{
-   // Define a table of significance levels:
-   double alpha[] = { 0.5, 0.25, 0.1, 0.05, 0.01, 0.001, 0.0001, 0.00001 };
-   using namespace boost::math;
-    // static RealType estimate_number_of_trials(
-  //RealType k,     // number of failures, k >= 0.
-  //RealType p,     // success fraction 0 <= p <= 1.
-  //RealType probability) // probability threshold 0 <= p <= 0.
-  using namespace std;
-  cout << "\n""Number of failures = " << failures;
-  cout << ",   Success ratio = " << 100 * p << "%" << endl;
-  cout << "Confidence %     trials   complement" << endl;
   for(unsigned i = 0; i < sizeof(alpha)/sizeof(alpha[0]); ++i)
    { // Confidence values %:
       cout << fixed << setprecision(3) << setw(10) << right << 100 * (1-alpha[i]) << "      "
       // estimate_number_of_trials
-      << setw(6) << right<< int(negative_binomial::estimate_number_of_trials(failures, p, alpha[i]/2)) << "  "
-      << setw(6) << right<< int(negative_binomial::estimate_number_of_trials(boost::math::complement(failures, p, alpha[i]/2)))
-      << endl;
+      << setw(6) << right<< int(negative_binomial::estimate_number_of_trials(failures, p, alpha[i]/2))  << endl;
    }
    cout << endl;
 } // void estimate_number_of_trials(double fails, double p)
@@ -92,152 +61,119 @@ void estimate_number_of_trials(double failures, double p)
 
 int main()
 {
-   estimate_max_sample_size(1.0/1000, 0);
-   estimate_max_sample_size(1.0/10000, 0);
-   estimate_max_sample_size(1.0/100000, 0);
-   estimate_max_sample_size(1.0/1000000, 0);
-
 	 estimate_number_of_trials(5, 0.5);
 	 estimate_number_of_trials(50, 0.5);
 	 estimate_number_of_trials(500, 0.5);
 	 estimate_number_of_trials(50, 0.1);
 	 estimate_number_of_trials(500, 0.1);
 	 estimate_number_of_trials(5, 0.9);
-
+	 estimate_number_of_trials(10-5, 0.4); // See Evans example in Wikipedia.
    return 0;
 } // int main()
 
 
 /*
 
+------ Build started: Project: neg_binomial_sample_sizes, Configuration: Debug Win32 ------
+Compiling...
+neg_binomial_sample_sizes.cpp
+Linking...
 Autorun "i:\boost-06-05-03-1300\libs\math\test\Math_test\debug\neg_binomial_sample_sizes.exe"
-________________________
-Maximum Number of Trials
-________________________
-Success ratio                           =  0.001
-Maximum Number of "successes" permitted =  0
+Number of failures = 5,   Success fraction = 50%
 ____________________________
 Confidence        Max Number
  Value (%)        Of Trials 
 ____________________________
-    50.000              1
-    75.000              1
-    90.000              1
-    95.000              1
-    99.000              1
-    99.900              1
-    99.990              1
-    99.999              1
-________________________
-Maximum Number of Trials
-________________________
-Success ratio                           =  0.0001000
-Maximum Number of "successes" permitted =  0
+    50.000          13
+    75.000          15
+    90.000          17
+    95.000          19
+    99.000          23
+    99.900          27
+    99.990          32
+    99.999          36
+Number of failures = 50.000,   Success fraction = 50.000%
 ____________________________
 Confidence        Max Number
  Value (%)        Of Trials 
 ____________________________
-    50.000              1
-    75.000              1
-    90.000              1
-    95.000              1
-    99.000              1
-    99.900              1
-    99.990              1
-    99.999              1
-________________________
-Maximum Number of Trials
-________________________
-Success ratio                           =  0.0000100
-Maximum Number of "successes" permitted =  0
+    50.000         108
+    75.000         113
+    90.000         118
+    95.000         122
+    99.000         130
+    99.900         139
+    99.990         147
+    99.999         155
+Number of failures = 500.000,   Success fraction = 50.000%
 ____________________________
 Confidence        Max Number
  Value (%)        Of Trials 
 ____________________________
-    50.000              1
-    75.000              1
-    90.000              1
-    95.000              1
-    99.000              1
-    99.900              1
-    99.990              1
-    99.999              1
-________________________
-Maximum Number of Trials
-________________________
-Success ratio                           =  0.0000010
-Maximum Number of "successes" permitted =  0
+    50.000        1022
+    75.000        1038
+    90.000        1054
+    95.000        1064
+    99.000        1085
+    99.900        1110
+    99.990        1131
+    99.999        1150
+Number of failures = 50.000,   Success fraction = 10.000%
 ____________________________
 Confidence        Max Number
  Value (%)        Of Trials 
 ____________________________
-    50.000              1
-    75.000              1
-    90.000              1
-    95.000              1
-    99.000              1
-    99.900              1
-    99.990              1
-    99.999              1
-Number of failures = 5,   Success ratio = 50%
-Confidence %     trials   complement
-    50.000          13       8
-    75.000          15       7
-    90.000          17       6
-    95.000          19       6
-    99.000          23       5
-    99.900          27       5
-    99.990          32       5
-    99.999          36       5
-Number of failures = 50.000,   Success ratio = 50.000%
-Confidence %     trials   complement
-    50.000         108      94
-    75.000         113      90
-    90.000         118      85
-    95.000         122      83
-    99.000         130      78
-    99.900         139      73
-    99.990         147      69
-    99.999         155      65
-Number of failures = 500.000,   Success ratio = 50.000%
-Confidence %     trials   complement
-    50.000        1022     979
-    75.000        1038     965
-    90.000        1054     950
-    95.000        1064     940
-    99.000        1085     922
-    99.900        1110     902
-    99.990        1131     885
-    99.999        1150     870
-Number of failures = 50.000,   Success ratio = 10.000%
-Confidence %     trials   complement
-    50.000         553     462
-    75.000         588     432
-    90.000         626     403
-    95.000         651     385
-    99.000         701     352
-    99.900         763     317
-    99.990         818     289
-    99.999         869     266
-Number of failures = 500.000,   Success ratio = 10.000%
-Confidence %     trials   complement
-    50.000        5150    4864
-    75.000        5254    4766
-    90.000        5364    4665
-    95.000        5434    4602
-    99.000        5574    4480
-    99.900        5739    4341
-    99.990        5880    4227
-    99.999        6006    4129
-Number of failures = 5.000,   Success ratio = 90.000%
-Confidence %     trials   complement
-    50.000           6       5
-    75.000           7       5
-    90.000           7       5
-    95.000           8       5
-    99.000           9       5
-    99.900          10       5
-    99.990          12       5
-    99.999          13       5
+    50.000         553
+    75.000         588
+    90.000         626
+    95.000         651
+    99.000         701
+    99.900         763
+    99.990         818
+    99.999         869
+Number of failures = 500.000,   Success fraction = 10.000%
+____________________________
+Confidence        Max Number
+ Value (%)        Of Trials 
+____________________________
+    50.000        5150
+    75.000        5254
+    90.000        5364
+    95.000        5434
+    99.000        5574
+    99.900        5739
+    99.990        5880
+    99.999        6006
+Number of failures = 5.000,   Success fraction = 90.000%
+____________________________
+Confidence        Max Number
+ Value (%)        Of Trials 
+____________________________
+    50.000           6
+    75.000           7
+    90.000           7
+    95.000           8
+    99.000           9
+    99.900          10
+    99.990          12
+    99.999          13
+Number of failures = 5.000,   Success fraction = 40.000%
+____________________________
+Confidence        Max Number
+ Value (%)        Of Trials 
+____________________________
+    50.000          17
+    75.000          20
+    90.000          23
+    95.000          25
+    99.000          30
+    99.900          36
+    99.990          42
+    99.999          48
+Build Time 0:03
+Build log was saved at "file://i:\boost-06-05-03-1300\libs\math\test\Math_test\neg_binomial_sample_sizes\Debug\BuildLog.htm"
+neg_binomial_sample_sizes - 0 error(s), 0 warning(s)
+========== Build: 1 succeeded, 0 failed, 0 up-to-date, 0 skipped ==========
+
 
 */
