9e9da20092977a4009b37b253e2eecc05dfe5deb
Paul A. Bristow <pbristow@hetp.u-net.com>
2006-12-06 18:06:26 +0000

    Changed to more accurate kurtosis realType(-6)/5 but still problem with infinity.
    [SVN r3504]

--- include/boost/math/distributions/uniform.hpp
+++ include/boost/math/distributions/uniform.hpp
@@ -4,6 +4,9 @@
 //  Boost Software License, Version 1.0. (See accompanying file
 //  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
+// TODO deal with infinity as special better - or remove.
+// 
+
 #ifndef BOOST_STATS_UNIFORM_HPP
 #define BOOST_STATS_UNIFORM_HPP
 
