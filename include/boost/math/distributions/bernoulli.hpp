5867c943c158a709c9c674b903cce459bc093eb4
Paul A. Bristow <pbristow@hetp.u-net.com>
2006-12-30 10:58:19 +0000

    1st commit, tests OK
    [SVN r3578]

--- include/boost/math/distributions/bernoulli.hpp
+++ include/boost/math/distributions/bernoulli.hpp
@@ -9,6 +9,7 @@
 // or copy at http://www.boost.org/LICENSE_1_0.txt)
 
 // http://en.wikipedia.org/wiki/bernoulli_distribution
+// http://mathworld.wolfram.com/BernoulliDistribution.html
 
 // bernoulli distribution is the discrete probability distribution of
 // the number (k) of successes, in a single Bernoulli trials.
@@ -123,105 +124,6 @@ namespace boost
         return m_p;
       }
 
-      enum interval_type
-      { //
-        clopper_pearson_exact_interval,
-        jeffreys_prior_interval
-      }; // class bernoulli_distribution
-
-      // Estimation of the success fraction parameter.
-      // The best estimate is actually simply successes/trials,
-      // these functions are used
-      // to obtain confidence intervals for the success fraction.
-      //
-      static RealType estimate_lower_bound_on_p(
-        RealType trials,
-        RealType successes,
-        RealType probability,
-        interval_type t = clopper_pearson_exact_interval)
-      {
-        RealType result; // of checks.
-        if(false == bernoulli_detail::check_dist_and_k(
-          BOOST_CURRENT_FUNCTION, trials, RealType(0), successes, &result)
-          &&
-          bernoulli_detail::check_dist_and_prob(
-          BOOST_CURRENT_FUNCTION, trials, RealType(0), probability, &result))
-        { return result; }
-
-        if(successes == 0)
-        {
-          return 0;
-        }
-
-        // NOTE!!! The Clopper Pearson formula uses "successes" not
-        // "successes+1" as usual to get the lower bound,
-        // see http://www.itl.nist.gov/div898/handbook/prc/section2/prc241.htm
-        return (t == clopper_pearson_exact_interval) ? ibeta_inv(successes, trials - successes + 1, probability)
-          : ibeta_inv(successes + 0.5f, trials - successes + 0.5f, probability);
-      }
-      static RealType estimate_upper_bound_on_p(
-        RealType trials,
-        RealType successes,
-        RealType probability,
-        interval_type t = clopper_pearson_exact_interval)
-      {
-        // Error checks:
-        RealType result;
-        if(false == bernoulli_detail::check_dist_and_k(
-          BOOST_CURRENT_FUNCTION, trials, RealType(0), successes, &result)
-          &&
-          bernoulli_detail::check_dist_and_prob(
-          BOOST_CURRENT_FUNCTION, trials, RealType(0), probability, &result))
-        { return result; }
-
-        if(trials == successes)
-          return 1;
-
-        return (t == clopper_pearson_exact_interval) ? ibetac_inv(successes + 1, trials - successes, probability)
-          : ibetac_inv(successes + 0.5f, trials - successes + 0.5f, probability);
-      }
-      // Estimate number of trials parameter:
-      //
-      // "How many trials do I need to be P% sure of seeing k events?"
-      //    or
-      // "How many trials can I have to be P% sure of seeing fewer than k events?"
-      //
-      static RealType estimate_minimum_number_of_trials(
-        RealType k,     // number of events
-        RealType p,     // success fraction
-        RealType alpha) // risk level
-      {
-        // Error checks:
-        RealType result;
-        if(false == bernoulli_detail::check_dist_and_k(
-          BOOST_CURRENT_FUNCTION, k, p, k, &result)
-          &&
-          bernoulli_detail::check_dist_and_prob(
-          BOOST_CURRENT_FUNCTION, k, p, alpha, &result))
-        { return result; }
-
-        result = ibetac_invb(k + 1, p, alpha);  // returns n - k
-        return result + k;
-      }
-
-      static RealType estimate_maximum_number_of_trials(
-        RealType k,     // number of events
-        RealType p,     // success fraction
-        RealType alpha) // risk level
-      {
-        // Error checks:
-        RealType result;
-        if(false == bernoulli_detail::check_dist_and_k(
-          BOOST_CURRENT_FUNCTION, k, p, k, &result)
-          &&
-          bernoulli_detail::check_dist_and_prob(
-          BOOST_CURRENT_FUNCTION, k, p, alpha, &result))
-        { return result; }
-
-        result = ibeta_invb(k + 1, p, alpha);  // returns n - k
-        return result + k;
-      }
-
     private:
       RealType m_p; // success_fraction
     }; // template <class RealType> class bernoulli_distribution
@@ -245,7 +147,7 @@ namespace boost
     template <class RealType>
     inline RealType mean(const bernoulli_distribution<RealType>& dist)
     { // Mean of bernoulli distribution = p (n = 1).
-      return  dist.success_fraction();
+      return dist.success_fraction();
     } // mean
 
     template <class RealType>
@@ -268,23 +170,23 @@ namespace boost
       RealType result; // of checks.
       if(false == bernoulli_detail::check_dist_and_k(
         BOOST_CURRENT_FUNCTION,
-        dist.success_fraction(),
-        k,
+        dist.success_fraction(), // 0 to 1
+        k, // 0 or 1
         &result))
       {
         return result;
       }
       // Assume k is integral.
-      if (k == 1)
+      if (k == 0)
+      {
+        return 1 - dist.success_fraction(); // 1 - p
+      }
+      else if (k == 1)
       {
         return dist.success_fraction(); // p
       }
-      else if (k == 0)
-      {
-        return 1 - dist.success_fraction(); // q = 1-p
-      }
       else
-      {
+      { // k < 0 or k > 1 So should never get here.
         return 0;
       }
     } // pdf
@@ -304,12 +206,12 @@ namespace boost
         return result;
       }
       if (k < 0)
-      {
+      { // Should never get here.
         return 0;
       }
-      else if (k < 1)
-      { // includes k == 0.
-        1 - p;
+      else if (k == 0)
+      {
+        return 1 - p;
       }
       else
       { // k >= 1
@@ -334,12 +236,12 @@ namespace boost
         return result;
       }
       if (k < 0)
-      {
+      { // Should never get here.
         return 1;
       }
-      else if (k < 1)
-      { // includes k == 0.
-        p;
+      else if (k == 0)
+      {
+        return p;
       }
       else
       { // k >= 1
@@ -347,80 +249,29 @@ namespace boost
       }
     } // bernoulli cdf complement
 
-    namespace detail
-    {
-      template <class RealType>
-      struct bernoulli_functor
-      {
-        bernoulli_functor(const bernoulli_distribution<RealType>& d, const RealType& target, bool c = false)
-          : dist(d), t(target), complement(c) {}
-
-        RealType operator()(const RealType k)
-        {
-          if(k >= dist.trials())
-            return 1; // any positive value will do.
-          return complement ? t - cdf(boost::math::complement(dist, k)) : cdf(dist, k) - t;
-        }
-      private:
-        const bernoulli_distribution<RealType>& dist;
-        RealType t;
-        bool complement;
-      }; // struct bernoulli_functor
-    } // namespace detail
-
     template <class RealType>
     RealType quantile(const bernoulli_distribution<RealType>& dist, const RealType& p)
-    { // Quantile or Percent Point bernoulli function.
-      // Return the number of expected successes k,
+    { // Quantile or Percent Point Bernoulli function.
+      // Return the number of expected successes k either 0 or 1.
       // for a given probability p.
-      //
-      // Error checks:
-      using namespace std;  // ADL of std names
-      RealType result;
+     
+      RealType result; // of error checks:
       if(false == bernoulli_detail::check_dist_and_prob(
         BOOST_CURRENT_FUNCTION,
-        dist.trials(),
         dist.success_fraction(),
         p,
         &result))
       {
         return result;
-      }
-
-      // Special cases:
-      //
-      if(p == 0)
-      {  // There may actually be no answer to this question,
-        // since the probability of zero successes may be non-zero,
-        // but zero is the best we can do:
-        return 0;
-      }
-      if(p == 1)
-      {  // Probability of n or fewer successes is always one,
-        // so n is the most sensible answer here:
-        return dist.trials();
-      }
-      if (p <= pow(1 - dist.success_fraction(), dist.trials()))
+      }  
+      if (p <= (1 - dist.success_fraction()))
       { // p <= pdf(dist, 0) == cdf(dist, 0)
-        return 0; // So the only reasonable result is zero.
-      } // And root finder would fail otherwise.
-
-      // Solve for quantile numerically:
-      //
-      detail::bernoulli_functor<RealType> f(dist, p);
-      tools::eps_tolerance<RealType> tol(tools::digits<RealType>());
-      boost::uintmax_t max_iter = 1000;
-      std::pair<RealType, RealType> r = tools::bracket_and_solve_root(
-        f,
-        dist.trials() / 2,
-        static_cast<RealType>(8),
-        true,
-        tol,
-        max_iter);
-      if(max_iter >= 1000)
-        tools::logic_error<RealType>(BOOST_CURRENT_FUNCTION, "Unable to locate the root within a reasonable number of iterations, closest approximation so far was %1%", r.first);
-      // return centre point of range found:
-      return r.first + (r.second - r.first) / 2;
+        return 0; 
+      } 
+      else
+      {
+        return 1;
+      }
     } // quantile
 
     template <class RealType>
@@ -435,7 +286,6 @@ namespace boost
       RealType result;
       if(false == bernoulli_detail::check_dist_and_prob(
         BOOST_CURRENT_FUNCTION,
-        dist.trials(),
         dist.success_fraction(),
         q,
         &result))
@@ -443,44 +293,15 @@ namespace boost
         return result;
       }
 
-      // Special cases:
-      //
-      if(q == 1)
-      {  // There may actually be no answer to this question,
-        // since the probability of zero successes may be non-zero,
-        // but zero is the best we can do:
+      if (q <= 1 - dist.success_fraction())
+      { // // q <= cdf(complement(dist, 0)) == pdf(dist, 0)
+        return 1;
+      }
+      else
+      {
         return 0;
       }
-      if(q == 0)
-      {  // Probability of greater than n successes is always zero,
-        // so n is the most sensible answer here:
-        return dist.trials();
-      }
-
-      if (-q <= powm1(1 - dist.success_fraction(), dist.trials()))
-      { // // q <= cdf(complement(dist, 0)) == pdf(dist, 0)
-        return 0; // So the only reasonable result is zero.
-      } // And root finder would fail otherwise.
-
-      // Need to consider the case where
-      //
-      // Solve for quantile numerically:
-      //
-      detail::bernoulli_functor<RealType> f(dist, q, true);
-      tools::eps_tolerance<RealType> tol(tools::digits<RealType>());
-      boost::uintmax_t max_iter = 1000;
-      std::pair<RealType, RealType> r = tools::bracket_and_solve_root(
-        f,
-        dist.trials() / 2,
-        static_cast<RealType>(8),
-        true,
-        tol,
-        max_iter);
-      if(max_iter >= 1000)
-        tools::logic_error<RealType>(BOOST_CURRENT_FUNCTION, "Unable to locate the root within a reasonable number of iterations, closest approximation so far was %1%", r.first);
-      // return centre point of range found:
-      return r.first + (r.second - r.first) / 2;
-    } // quantile
+    } // quantile complemented.
 
     template <class RealType>
     inline RealType mode(const bernoulli_distribution<RealType>& dist)
@@ -493,8 +314,6 @@ namespace boost
     {
       using namespace std;; // Aid ADL for sqrt.
       RealType p = dist.success_fraction();
-      RealType q = 1 - dist.success_fraction();
-      RealType result = (q - p) / sqrt(p * q);
       return (1 - 2 * p) / sqrt(p * (1 - p));
     }
 
