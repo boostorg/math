deaa36b8d7b10284f42aa5d2d31578fd2b02737b
John Maddock <john@johnmaddock.co.uk>
2008-06-28 11:24:02 +0000

    Fix copyright declaration.
    [SVN r46805]

--- include/boost/math/distributions/detail/generic_quantile.hpp
+++ include/boost/math/distributions/detail/generic_quantile.hpp
@@ -1,3 +1,7 @@
+//  Copyright John Maddock 2008.
+//  Use, modification and distribution are subject to the
+//  Boost Software License, Version 1.0. (See accompanying file
+//  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
 #ifndef BOOST_MATH_DISTIBUTIONS_DETAIL_GENERIC_QUANTILE_HPP
 #define BOOST_MATH_DISTIBUTIONS_DETAIL_GENERIC_QUANTILE_HPP
