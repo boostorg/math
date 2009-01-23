0b8d45eb88bf7562f6839295bd5c22aae792f2cf
Paul A. Bristow <pbristow@hetp.u-net.com>
2009-01-23 09:46:27 +0000

    New copyright date.
    [SVN r50738]

--- include/boost/math/distributions.hpp
+++ include/boost/math/distributions.hpp
@@ -1,5 +1,5 @@
 //  Copyright John Maddock 2006, 2007.
-//  Copyright Paul A. Bristow 2006, 2007.
+//  Copyright Paul A. Bristow 2006, 2007, 2009.
 
 //  Use, modification and distribution are subject to the
 //  Boost Software License, Version 1.0. (See accompanying file
