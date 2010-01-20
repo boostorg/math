ec2df239b0d79891dab1860f9c10526a828d2759
Paul A. Bristow <pbristow@hetp.u-net.com>
2010-01-20 18:43:07 +0000

    Added licence info
    [SVN r59160]

--- dot_net_example/boost_math/AssemblyInfo.cpp
+++ dot_net_example/boost_math/AssemblyInfo.cpp
@@ -1,5 +1,11 @@
 #include "stdafx.h"
 
+// Copyright Paul A. Bristow & John Maddock 2009
+// Use, modification and distribution are subject to the
+// Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt
+// or copy at http://www.boost.org/LICENSE_1_0.txt)
+
 using namespace System;
 using namespace System::Reflection;
 using namespace System::Runtime::CompilerServices;
