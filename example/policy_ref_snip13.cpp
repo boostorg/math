287ef662ade4523737ca264e18f2b3c4ac48fdb3
Paul A. Bristow <pbristow@hetp.u-net.com>
2010-08-31 14:05:46 +0000

    Avoid name ambiguity.
    [SVN r65150]

--- example/policy_ref_snip13.cpp
+++ example/policy_ref_snip13.cpp
@@ -1,4 +1,5 @@
 //  Copyright John Maddock 2007.
+// Copyright Paul A. Bristow 2010
 //  Use, modification and distribution are subject to the
 //  Boost Software License, Version 1.0. (See accompanying file
 //  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
@@ -7,48 +8,68 @@
 // and comments, don't change any of the special comment mark-ups!
 
 #ifdef _MSC_VER
-#pragma warning (disable : 4189) //  'd' : local variable is initialized but not referenced
+#  pragma warning (disable : 4189) //  'd' : local variable is initialized but not referenced
 #endif
+
+#include <iostream>
+using std::cout; using std::endl;
+
+#include <exception>
+using std::domain_error;
+
 
 //[policy_ref_snip13
 
 #include <boost/math/distributions/cauchy.hpp>
 
-namespace myspace{
+namespace myspace
+{ // using namespace boost::math::policies; // May be convenientin myspace.
 
-using namespace boost::math::policies;
+  // Define a policy called my_policy to use.
+  using boost::math::policies::policy;
+  
+// In this case we want all the distribution accessor functions to compile,
+// even if they are mathematically undefined, so
+// make the policy assert_undefined.
+  using boost::math::policies::assert_undefined;
 
-// Define a policy to use, in this case we want all the distribution
-// accessor functions to compile, even if they are mathematically
-// undefined:
 typedef policy<assert_undefined<false> > my_policy;
 
+// Finally apply this policy to type double.
 BOOST_MATH_DECLARE_DISTRIBUTIONS(double, my_policy)
-
-}
+} // namespace myspace
 
 // Now we can use myspace::cauchy etc, which will use policy
 // myspace::mypolicy:
 //
-// This compiles but raises a domain error at runtime:
-//
+// This compiles but raises a domain error at runtime.
+// (Caution, if you omit the try'n'catch blocks, it will just terminate,
+// giving no clues as to why.  So try'n'catch blocks are strongly recommended.
+
 void test_cauchy()
 {
    try
    {
       double d = mean(myspace::cauchy());
    }
-   catch(const std::domain_error& e)
+   catch(const domain_error& e)
    {
-      std::cout << e.what() << std::endl;
+      cout << e.what() << endl;
    }
 }
 
 //]
 
-#include <iostream>
-
 int main()
 {
    test_cauchy();
 }
+
+/*
+
+Output:
+
+policy_snip_13.vcxproj -> J:\Cpp\MathToolkit\test\Math_test\Release\policy_snip_13.exe
+  Error in function boost::math::mean(cauchy<double>&): The Cauchy distribution does not have a mean: the only possible return value is 1.#QNAN.
+
+  */
