ded3b78a058f2bd28e80dca08206e6e9ca815a74
John Maddock <john@johnmaddock.co.uk>
2010-11-22 19:00:55 +0000

    Update distribution concept checks to test ad-hoc member functions as well as the generic ones. Fix typo in docs and regenerate.
    [SVN r66689]

--- include/boost/math/concepts/distributions.hpp
+++ include/boost/math/concepts/distributions.hpp
@@ -12,6 +12,7 @@
 #define BOOST_MATH_DISTRIBUTION_CONCEPT_HPP
 
 #include <boost/math/distributions/complement.hpp>
+#include <boost/math/distributions/fwd.hpp>
 #ifdef BOOST_MSVC
 #pragma warning(push)
 #pragma warning(disable: 4100)
@@ -115,12 +116,12 @@ std::pair<RealType, RealType> support(const distribution_archetype<RealType>& di
 template <class Distribution>
 struct DistributionConcept
 {
+   typedef typename Distribution::value_type value_type;
+
    void constraints()
    {
       function_requires<CopyConstructibleConcept<Distribution> >();
       function_requires<AssignableConcept<Distribution> >();
-
-      typedef typename Distribution::value_type value_type;
 
       const Distribution& dist = DistributionConcept<Distribution>::get_object();
 
@@ -188,6 +189,202 @@ struct DistributionConcept
       v = quantile(complement(dist, li));
       v = hazard(dist, li);
       v = chf(dist, li);
+      test_extra_members(dist);
+   }
+   template <class D>
+   static void test_extra_members(const D&)
+   {}
+   template <class R, class P>
+   static void test_extra_members(const boost::math::bernoulli_distribution<R, P>& d)
+   {
+      value_type r = d.success_fraction();
+   }
+   template <class R, class P>
+   static void test_extra_members(const boost::math::beta_distribution<R, P>& d)
+   {
+      value_type r1 = d.alpha();
+      value_type r2 = d.beta();
+      r1 = boost::math::beta_distribution<R, P>::find_alpha(r1, r2);
+      r1 = boost::math::beta_distribution<R, P>::find_beta(r1, r2);
+      r1 = boost::math::beta_distribution<R, P>::find_alpha(r1, r2, r1);
+      r1 = boost::math::beta_distribution<R, P>::find_beta(r1, r2, r1);
+   }
+   template <class R, class P>
+   static void test_extra_members(const boost::math::binomial_distribution<R, P>& d)
+   {
+      value_type r = d.success_fraction();
+      r = d.trials();
+      r = Distribution::find_lower_bound_on_p(r, r, r);
+      r = Distribution::find_lower_bound_on_p(r, r, r, Distribution::clopper_pearson_exact_interval);
+      r = Distribution::find_lower_bound_on_p(r, r, r, Distribution::jeffreys_prior_interval);
+      r = Distribution::find_upper_bound_on_p(r, r, r);
+      r = Distribution::find_upper_bound_on_p(r, r, r, Distribution::clopper_pearson_exact_interval);
+      r = Distribution::find_upper_bound_on_p(r, r, r, Distribution::jeffreys_prior_interval);
+      r = Distribution::find_minimum_number_of_trials(r, r, r);
+      r = Distribution::find_maximum_number_of_trials(r, r, r);
+   }
+   template <class R, class P>
+   static void test_extra_members(const boost::math::cauchy_distribution<R, P>& d)
+   {
+      value_type r = d.location();
+      r = d.scale();
+   }
+   template <class R, class P>
+   static void test_extra_members(const boost::math::chi_squared_distribution<R, P>& d)
+   {
+      value_type r = d.degrees_of_freedom();
+      r = Distribution::find_degrees_of_freedom(r, r, r, r);
+      r = Distribution::find_degrees_of_freedom(r, r, r, r, r);
+   }
+   template <class R, class P>
+   static void test_extra_members(const boost::math::exponential_distribution<R, P>& d)
+   {
+      value_type r = d.lambda();
+   }
+   template <class R, class P>
+   static void test_extra_members(const boost::math::extreme_value_distribution<R, P>& d)
+   {
+      value_type r = d.scale();
+      r = d.location();
+   }
+   template <class R, class P>
+   static void test_extra_members(const boost::math::fisher_f_distribution<R, P>& d)
+   {
+      value_type r = d.degrees_of_freedom1();
+      r = d.degrees_of_freedom2();
+   }
+   template <class R, class P>
+   static void test_extra_members(const boost::math::gamma_distribution<R, P>& d)
+   {
+      value_type r = d.scale();
+      r = d.shape();
+   }
+   template <class R, class P>
+   static void test_extra_members(const boost::math::inverse_chi_squared_distribution<R, P>& d)
+   {
+      value_type r = d.scale();
+      r = d.degrees_of_freedom();
+   }
+   template <class R, class P>
+   static void test_extra_members(const boost::math::inverse_gamma_distribution<R, P>& d)
+   {
+      value_type r = d.scale();
+      r = d.shape();
+   }
+   template <class R, class P>
+   static void test_extra_members(const boost::math::hypergeometric_distribution<R, P>& d)
+   {
+      unsigned u = d.defective();
+      u = d.sample_count();
+      u = d.total();
+   }
+   template <class R, class P>
+   static void test_extra_members(const boost::math::laplace_distribution<R, P>& d)
+   {
+      value_type r = d.scale();
+      r = d.location();
+   }
+   template <class R, class P>
+   static void test_extra_members(const boost::math::logistic_distribution<R, P>& d)
+   {
+      value_type r = d.scale();
+      r = d.location();
+   }
+   template <class R, class P>
+   static void test_extra_members(const boost::math::lognormal_distribution<R, P>& d)
+   {
+      value_type r = d.scale();
+      r = d.location();
+   }
+   template <class R, class P>
+   static void test_extra_members(const boost::math::negative_binomial_distribution<R, P>& d)
+   {
+      value_type r = d.success_fraction();
+      r = d.successes();
+      r = Distribution::find_lower_bound_on_p(r, r, r);
+      r = Distribution::find_upper_bound_on_p(r, r, r);
+      r = Distribution::find_minimum_number_of_trials(r, r, r);
+      r = Distribution::find_maximum_number_of_trials(r, r, r);
+   }
+   template <class R, class P>
+   static void test_extra_members(const boost::math::non_central_beta_distribution<R, P>& d)
+   {
+      value_type r1 = d.alpha();
+      value_type r2 = d.beta();
+      r1 = d.non_centrality();
+   }
+   template <class R, class P>
+   static void test_extra_members(const boost::math::non_central_chi_squared_distribution<R, P>& d)
+   {
+      value_type r = d.degrees_of_freedom();
+      r = d.non_centrality();
+      r = Distribution::find_degrees_of_freedom(r, r, r);
+      r = Distribution::find_degrees_of_freedom(boost::math::complement(r, r, r));
+      r = Distribution::find_non_centrality(r, r, r);
+      r = Distribution::find_non_centrality(boost::math::complement(r, r, r));
+   }
+   template <class R, class P>
+   static void test_extra_members(const boost::math::non_central_f_distribution<R, P>& d)
+   {
+      value_type r = d.degrees_of_freedom1();
+      r = d.degrees_of_freedom2();
+      r = d.non_centrality();
+   }
+   template <class R, class P>
+   static void test_extra_members(const boost::math::non_central_t_distribution<R, P>& d)
+   {
+      value_type r = d.degrees_of_freedom();
+      r = d.non_centrality();
+   }
+   template <class R, class P>
+   static void test_extra_members(const boost::math::normal_distribution<R, P>& d)
+   {
+      value_type r = d.scale();
+      r = d.location();
+      r = d.mean();
+      r = d.standard_deviation();
+   }
+   template <class R, class P>
+   static void test_extra_members(const boost::math::pareto_distribution<R, P>& d)
+   {
+      value_type r = d.scale();
+      r = d.shape();
+   }
+   template <class R, class P>
+   static void test_extra_members(const boost::math::poisson_distribution<R, P>& d)
+   {
+      value_type r = d.mean();
+   }
+   template <class R, class P>
+   static void test_extra_members(const boost::math::rayleigh_distribution<R, P>& d)
+   {
+      value_type r = d.sigma();
+   }
+   template <class R, class P>
+   static void test_extra_members(const boost::math::students_t_distribution<R, P>& d)
+   {
+      value_type r = d.degrees_of_freedom();
+      r = d.find_degrees_of_freedom(r, r, r, r);
+      r = d.find_degrees_of_freedom(r, r, r, r, r);
+   }
+   template <class R, class P>
+   static void test_extra_members(const boost::math::triangular_distribution<R, P>& d)
+   {
+      value_type r = d.lower();
+      r = d.mode();
+      r = d.upper();
+   }
+   template <class R, class P>
+   static void test_extra_members(const boost::math::weibull_distribution<R, P>& d)
+   {
+      value_type r = d.scale();
+      r = d.shape();
+   }
+   template <class R, class P>
+   static void test_extra_members(const boost::math::uniform_distribution<R, P>& d)
+   {
+      value_type r = d.lower();
+      r = d.upper();
    }
 private:
    static Distribution& get_object()
