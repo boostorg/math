40ab64c0ea1d91c48ee9c8437209369c7786fa8b
Paul A. Bristow <pbristow@hetp.u-net.com>
2011-04-15 14:58:43 +0000

    Added changesign
    [SVN r71285]

--- test/test_sign.cpp
+++ test/test_sign.cpp
@@ -1,5 +1,5 @@
 // Copyright John Maddock 2008
-
+//  (C) Copyright Paul A. Bristow 2011 (added tests for changesign)
 // Use, modification and distribution are subject to the
 // Boost Software License, Version 1.0.
 // (See accompanying file LICENSE_1_0.txt
@@ -27,6 +27,15 @@ void test_spots(RealType /*T*/, const char* /*type_name*/)
    RealType c = -1;
    BOOST_CHECK_EQUAL((boost::math::signbit)(a), 0);
    BOOST_CHECK_EQUAL((boost::math::sign)(a), 0);
+   BOOST_CHECK_EQUAL((boost::math::changesign)(b), RealType(-1));
+   BOOST_CHECK_EQUAL((boost::math::changesign)(c), RealType(+1));
+   BOOST_CHECK_EQUAL((boost::math::changesign)(a), RealType(0));
+
+   // Compare to formula for changsign(x) = copysign(x, signbit(x) ? 1.0 : -1.0)
+   BOOST_CHECK_EQUAL((boost::math::changesign)(b),
+      boost::math::copysign(b, boost::math::signbit(b) ? RealType(1.) : RealType(-1.) ));
+
+
    BOOST_CHECK_EQUAL((boost::math::copysign)(b, a), RealType(1));
    BOOST_CHECK_EQUAL((boost::math::copysign)(c, a), RealType(1));
    a = 1;
@@ -49,6 +58,7 @@ void test_spots(RealType /*T*/, const char* /*type_name*/)
    BOOST_CHECK_EQUAL((boost::math::sign)(a), -1);
    BOOST_CHECK_EQUAL((boost::math::copysign)(b, a), RealType(-1));
    BOOST_CHECK_EQUAL((boost::math::copysign)(c, a), RealType(-1));
+
    if(std::numeric_limits<RealType>::has_infinity)
    {
       a = std::numeric_limits<RealType>::infinity();
@@ -56,11 +66,14 @@ void test_spots(RealType /*T*/, const char* /*type_name*/)
       BOOST_CHECK_EQUAL((boost::math::sign)(a), 1);
       BOOST_CHECK_EQUAL((boost::math::copysign)(b, a), RealType(1));
       BOOST_CHECK_EQUAL((boost::math::copysign)(c, a), RealType(1));
+      BOOST_CHECK_EQUAL((boost::math::changesign)(a), -a);
+
       a = -std::numeric_limits<RealType>::infinity();
       BOOST_CHECK((boost::math::signbit)(a) != 0);
       BOOST_CHECK_EQUAL((boost::math::sign)(a), -1);
       BOOST_CHECK_EQUAL((boost::math::copysign)(b, a), RealType(-1));
       BOOST_CHECK_EQUAL((boost::math::copysign)(c, a), RealType(-1));
+      BOOST_CHECK_EQUAL((boost::math::changesign)(a), -a);
    }
 #if !defined(__SUNPRO_CC) && !defined(BOOST_INTEL)
    if(std::numeric_limits<RealType>::has_quiet_NaN)
@@ -70,11 +83,17 @@ void test_spots(RealType /*T*/, const char* /*type_name*/)
       BOOST_CHECK_EQUAL((boost::math::sign)(a), 1);
       BOOST_CHECK_EQUAL((boost::math::copysign)(b, a), RealType(1));
       BOOST_CHECK_EQUAL((boost::math::copysign)(c, a), RealType(1));
+      // BOOST_CHECK_EQUAL((boost::math::changesign)(a), -a); // NaN comparison fails always!
+      BOOST_CHECK_EQUAL((boost::math::signbit)((boost::math::changesign)(a)), 1);
+
       a = -std::numeric_limits<RealType>::quiet_NaN();
       BOOST_CHECK((boost::math::signbit)(a) != 0);
       BOOST_CHECK_EQUAL((boost::math::sign)(a), -1);
       BOOST_CHECK_EQUAL((boost::math::copysign)(b, a), RealType(-1));
       BOOST_CHECK_EQUAL((boost::math::copysign)(c, a), RealType(-1));
+      //BOOST_CHECK_EQUAL((boost::math::changesign)(a), -a); // NaN comparison fails always!
+      BOOST_CHECK_EQUAL((boost::math::signbit)((boost::math::changesign)(a)), 0);
+
    }
 #endif
    //
