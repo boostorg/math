2f1c05528cdb50b75cb59031d0b5bd24b3061ab9
John Maddock <john@johnmaddock.co.uk>
2012-04-21 10:29:44 +0000

    Add licence.
    [SVN r78110]

--- example/owens_t_drv.cpp
+++ example/owens_t_drv.cpp
@@ -1,3 +1,8 @@
+// (C) Benjamin Sobotta 2012
+
+//  Use, modification and distribution are subject to the
+//  Boost Software License, Version 1.0. (See accompanying file
+//  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
 #include <boost/math/special_functions/owens_t.hpp>
 #include <iostream>
