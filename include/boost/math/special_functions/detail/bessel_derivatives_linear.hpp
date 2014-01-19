d5099fc86a3ab005419155d6f1ee4363400bb5a4
jzmaddock <john@johnmaddock.co.uk>
2014-01-19 16:44:49 +0000

    Suppress some msvc warnings.

--- include/boost/math/special_functions/detail/bessel_derivatives_linear.hpp
+++ include/boost/math/special_functions/detail/bessel_derivatives_linear.hpp
@@ -19,7 +19,7 @@ namespace boost{ namespace math{ namespace detail{
 template <class T, class Tag, class Policy>
 inline T bessel_j_derivative_linear(T v, T x, Tag tag, Policy pol)
 {
-   return (boost::math::detail::cyl_bessel_j_imp<T>(v-1, x, Tag(), pol) - boost::math::detail::cyl_bessel_j_imp<T>(v+1, x, Tag(), pol)) / 2;
+   return (boost::math::detail::cyl_bessel_j_imp<T>(v-1, x, tag, pol) - boost::math::detail::cyl_bessel_j_imp<T>(v+1, x, tag, pol)) / 2;
 }
 
 template <class T, class Policy>
@@ -43,7 +43,7 @@ inline T bessel_i_derivative_linear(T v, T x, Policy pol)
 template <class T, class Tag, class Policy>
 inline T bessel_k_derivative_linear(T v, T x, Tag tag, Policy pol)
 {
-   return (boost::math::detail::cyl_bessel_k_imp<T>(v-1, x, Tag(), pol) + boost::math::detail::cyl_bessel_k_imp<T>(v+1, x, Tag(), pol)) / -2;
+   return (boost::math::detail::cyl_bessel_k_imp<T>(v-1, x, tag, pol) + boost::math::detail::cyl_bessel_k_imp<T>(v+1, x, tag, pol)) / -2;
 }
 
 template <class T, class Policy>
@@ -55,7 +55,7 @@ inline T bessel_k_derivative_linear(T v, T x, const bessel_int_tag& tag, Policy 
 template <class T, class Tag, class Policy>
 inline T bessel_y_derivative_linear(T v, T x, Tag tag, Policy pol)
 {
-   return (boost::math::detail::cyl_neumann_imp<T>(v-1, x, Tag(), pol) - boost::math::detail::cyl_neumann_imp<T>(v+1, x, Tag(), pol)) / 2;
+   return (boost::math::detail::cyl_neumann_imp<T>(v-1, x, tag, pol) - boost::math::detail::cyl_neumann_imp<T>(v+1, x, tag, pol)) / 2;
 }
 
 template <class T, class Policy>
