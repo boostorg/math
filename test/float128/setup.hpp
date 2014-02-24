108785d553ebdd761451cd7f6fd83248e544738b
jzmaddock <jzmaddock@gmail.com>
2014-02-24 18:53:50 +0000

    Add missing pow overload to cstdfloat.hpp. Fix Intel-Windows compilation of the tests. Make the float128 tests an explicit test case in the Jamfile.

--- test/float128/setup.hpp
+++ test/float128/setup.hpp
@@ -10,13 +10,14 @@
 #endif
 
 #define BOOST_MATH_OVERFLOW_ERROR_POLICY ignore_error
-#define BOOST_MATH_SMALL_CONSTANT(x) x
 
 #include <boost/cstdfloat.hpp>
+#include <boost/static_assert.hpp>
 
 #define ALL_TESTS    test(boost::floatmax_t(0), "boost::floatmax_t");
 typedef boost::floatmax_t test_type_1;
    
+BOOST_STATIC_ASSERT_MSG(std::numeric_limits<boost::floatmax_t>::digits == 113, "These tests should only be run for 128-bit floating point types.");
 
 #ifndef BOOST_MATH_TEST_TYPE
 #define BOOST_TEST_MAIN
