be4b5ad3cb48f0c398c47dc6bc48b59b38d91057
jzmaddock <jzmaddock@gmail.com>
2014-11-26 19:07:18 +0000

    [polygamma] Tweak expected error rates.

--- test/float128/test_polygamma.cpp
+++ test/float128/test_polygamma.cpp
@@ -28,7 +28,7 @@ void expected_results()
       ".*",                          // platform
       ".*",                          // test type(s)
       ".*",                          // test data group
-      ".*", 350, 100);               // test function
+      ".*", 500, 200);               // test function
    //
    // Finish off by printing out the compiler/stdlib/platform names,
    // we do this to make it easier to mark up expected error rates.
