cc5628b2a44eae6f233c0b958c1816903631905e
jzmaddock <jzmaddock@gmail.com>
2014-11-26 19:12:35 +0000

    [polygamma] tweak expected error rates.

--- test/test_polygamma.cpp
+++ test/test_polygamma.cpp
@@ -65,7 +65,7 @@ void expected_results()
       ".*",                          // platform
       largest_type,                  // test type(s)
       ".*",                          // test data group
-      ".*", 30, 10);                 // test function
+      ".*", 40, 10);                 // test function
    //
    // Finish off by printing out the compiler/stdlib/platform names,
    // we do this to make it easier to mark up expected error rates.
