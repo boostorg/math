aedcfb355b082fdb5380ab9d17314089e332681f
jzmaddock <john@johnmaddock.co.uk>
2015-06-18 18:35:15 +0100

    Fix template name used only on Darwin.

--- include/boost/math/special_functions/relative_difference.hpp
+++ include/boost/math/special_functions/relative_difference.hpp
@@ -71,7 +71,7 @@ namespace boost{
 
 #if defined(macintosh) || defined(__APPLE__) || defined(__APPLE_CC__)
       template <>
-      inline double relative_error<double>(double a, double b)
+      inline double relative_difference<double>(double a, double b)
       {
          BOOST_MATH_STD_USING
          //
