f6b60b701f651f9ae34454589a6f1a02b59ca466
jzmaddock <john@johnmaddock.co.uk>
2015-07-01 18:36:26 +0100

    Fix overflow handling.

--- reporting/accuracy/test_bessel_i_prime.cpp
+++ reporting/accuracy/test_bessel_i_prime.cpp
@@ -4,6 +4,7 @@
 //  Boost Software License, Version 1.0. (See accompanying file
 //  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
+#define BOOST_MATH_OVERFLOW_ERROR_POLICY ignore_error
 #include "bindings.hpp"
 #include "../../test/test_bessel_i_prime.hpp"
 #include <boost/math/special_functions/bessel_prime.hpp>
