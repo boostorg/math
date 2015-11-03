ac2943e0531ba1c1b9d1c0c957f7676cf014c353
Jeremy W. Murphy <jeremy.william.murphy@gmail.com>
2015-11-03 23:59:29 +1100

    Unit tests for arithmetic in general, especially for zero.

--- test/test_polynomial.cpp
+++ test/test_polynomial.cpp
@@ -38,6 +38,7 @@ boost::array<double, 3> const d2b = {{-7, 5, 6}};
 boost::array<double, 3> const d2c = {{31, -21, -22}};
 boost::array<double, 1> const d0a = {{6}};
 boost::array<double, 1> const d0b = {{3}};
+boost::array<double, 1> const d0c = {{1}};
 
 PR const a(d3a.begin(), d3a.end());
 PR const b(d1a.begin(), d1a.end());
@@ -49,11 +50,16 @@ PR const e(d2c.begin(), d2c.end());
 PR const f(d0b.begin(), d0b.end());
 PR const g(d3c.begin(), d3c.end());
 PR const zero = zero_element(std::multiplies< polynomial<double> >());
+PR const one(d0c.begin(), d0c.end());
+
+BOOST_AUTO_TEST_CASE( test_properties )
+{
+    BOOST_CHECK_THROW(zero.degree(), std::logic_error);
+}
 
 BOOST_AUTO_TEST_CASE( test_division )
 {
-    BOOST_CHECK_THROW(quotient_remainder(a, zero), std::domain_error);
-    
+
     answer<double> result = quotient_remainder(a, b);
     BOOST_CHECK_EQUAL(result.quotient, q);
     BOOST_CHECK_EQUAL(result.remainder, r);
@@ -68,6 +74,10 @@ BOOST_AUTO_TEST_CASE( test_division )
     BOOST_CHECK_EQUAL(result.quotient, g);
     BOOST_CHECK_EQUAL(result.remainder, zero);
     BOOST_CHECK_EQUAL(a, g * f + zero); // Sanity check.
+
+    BOOST_CHECK_EQUAL(a / a, one);
+    BOOST_CHECK_EQUAL(a % a, zero);
+    // BOOST_CHECK_EQUAL(zero / zero, zero); // TODO
 }
 
 BOOST_AUTO_TEST_CASE( test_gcd )
@@ -78,4 +88,40 @@ BOOST_AUTO_TEST_CASE( test_gcd )
     PR bb(d6.begin(), d6.end());
     PR foo = gcd(aa, bb);
     BOOST_CHECK_EQUAL(foo, bb);
+}
+
+// Sanity checks to make sure I didn't break it.
+BOOST_AUTO_TEST_CASE( test_addition )
+{
+    PR result = a + b; // different degree
+    boost::array<double, 4> tmp = {{8, -5, -4, 3}};
+    PR expected(tmp.begin(), tmp.end());
+    BOOST_CHECK_EQUAL(result, expected);
+    BOOST_CHECK_EQUAL(a + 0.0, a);
+    BOOST_CHECK_EQUAL(a + b, b + a);
+}
+
+BOOST_AUTO_TEST_CASE( test_subtraction )
+{
+    BOOST_CHECK_EQUAL(a - 0.0, a);
+    BOOST_CHECK_EQUAL(0.0 - a, -a);
+    BOOST_CHECK_EQUAL(a - zero, a);
+    BOOST_CHECK_EQUAL(zero - a, -a);
+    BOOST_CHECK_EQUAL(a - a, zero);
+}
+
+BOOST_AUTO_TEST_CASE( test_multiplication )
+{
+    BOOST_CHECK_EQUAL(a * 0.0, zero);
+    BOOST_CHECK_EQUAL(a * zero, zero);
+    BOOST_CHECK_EQUAL(zero * 0.0, zero);
+    BOOST_CHECK_EQUAL(zero * zero, zero);
+    BOOST_CHECK_EQUAL(a * b, b * a);
+}
+
+BOOST_AUTO_TEST_CASE( test_arithmetic_relations )
+{
+    BOOST_CHECK_EQUAL(a * 2, a + a);
+    BOOST_CHECK_EQUAL(a, (a * a) / a);
+    BOOST_CHECK_EQUAL(a - b, -b + a);
 }
