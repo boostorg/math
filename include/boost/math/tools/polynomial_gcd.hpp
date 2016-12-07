2f141cf756ec1e5107dc30865898868ee5fba7f3
Jeremy W. Murphy <jeremy.william.murphy@gmail.com>
2016-12-08 10:18:55 +1100

    Consolidate the polynomial normalization gcd_trait.

--- include/boost/math/tools/polynomial_gcd.hpp
+++ include/boost/math/tools/polynomial_gcd.hpp
@@ -60,28 +60,7 @@ struct gcd_traits_polynomial_defaults : public gcd_traits_defaults< boost::math:
             x /= d;
         }
     }
-};
 
-
-
-
-//
-// Special handling for polynomials:
-// Note that gcd_traits_polynomial is templated on the coefficient type T,
-// not on polynomial<T>.
-//
-template <typename T, typename Enabled = void>
-struct gcd_traits_polynomial 
-{};
-
-
-// gcd_traits for Z[x].
-template <typename T>
-struct gcd_traits_polynomial<T, typename enable_if_c< std::numeric_limits<T>::is_integer >::type > : public gcd_traits_polynomial_defaults<T>
-{
-    typedef boost::math::tools::polynomial<T> polynomial_type;
-    using gcd_traits_polynomial_defaults<T>::normalize;
-    
     inline static void
     subtract(polynomial_type &a, polynomial_type const &b)
     {
@@ -109,61 +88,34 @@ struct gcd_traits_polynomial<T, typename enable_if_c< std::numeric_limits<T>::is
 };
 
 
-// TODO: Enable for multiprecision floating point types.
-template <typename T>
-struct gcd_traits_polynomial<T, typename disable_if_c< std::numeric_limits<T>::is_integer >::type > : public gcd_traits_polynomial_defaults<T>
-{
-    typedef boost::math::tools::polynomial<T> polynomial_type;
-    typedef gcd_traits_defaults< polynomial_type > parent_class;
-    using typename parent_class::method_type;
-    // static const method_type method = method_type::method_euclid;
+//
+// Special handling for polynomials:
+// Note that gcd_traits_polynomial is templated on the coefficient type T,
+// not on polynomial<T>.
+//
+template <typename T, typename Enabled = void>
+struct gcd_traits_polynomial 
+{};
 
-    inline static void
-    subtract(polynomial_type &a, polynomial_type const &b)
-    {
-        using std::modf;
-        
-        // We want to use Stepanov's implementation as often as possible because
-        // it results in the smallest coefficients, however, whole numbers take
-        // precedence.
-        T const r = constant_coefficient(a) / constant_coefficient(b);
-        T r_int;
-        T const r_frac = modf(r, &r_int);
-        // Should we also consider 0.25, 0.125, etc?
-        if (r_frac == T(0) || r_frac == T(0.5))
-        {
-            // Stepanov's implementation; suffers from floating point inaccuracy
-            // when r does not divide ___ evenly.
-            a -= r * b;
-            // normalize coefficients so that leading coefficient is whole
-            if (a && modf(a.data().back(), &r_int) != T(0))
-                a /= a.data().back();
-        }
-        else
-        {
-            // Antoine Joux's implementation: produces huge coefficients.
-            T const tmp = constant_coefficient(a);
-            a *= constant_coefficient(b);
-            a -= tmp * b;
-#ifndef NDEBUG
-            using boost::lambda::_1;
-            if (std::numeric_limits<T>::has_infinity)
-            {
-                T const &inf(std::numeric_limits<T>::infinity());
-                if (std::find_if(a.data().begin(), a.data().end(), _1 == inf || _1 == -inf) != a.data().end())
-                    throw std::domain_error("Floating point overflow.");
-            }
-#endif
-        }
-    }
-};
+
+// Note: Use these trait classes for Z[x] and R[x], etc to customize for operations
+// on different kinds of polynomials.
+
+// gcd_traits for Z[x].
+template <typename T>
+struct gcd_traits_polynomial<T, typename enable_if_c<std::numeric_limits<T>::is_integer>::type> : public gcd_traits_polynomial_defaults<T>
+{};
+
+
+// gcd_traits for R[x].
+template <typename T>
+struct gcd_traits_polynomial<T, typename enable_if_c<is_floating_point<T>::value || (std::numeric_limits<T>::is_specialized && !std::numeric_limits<T>::is_integer) >::type> : public gcd_traits_polynomial_defaults<T>
+{};
 
 
 template <typename T>
 struct gcd_traits< boost::math::tools::polynomial<T> > : public gcd_traits_polynomial<T>
-{
-    
-};
+{};
 
 } // namespace math
 } // namespace boost
