94d4f95070fbe11fce3a9f9a82b3c52bbaccfade
Nick Thompson <nathompson7@protonmail.com>
2017-06-30 11:35:51 -0600

    Updates to chebyshev

--- include/boost/math/special_functions/chebyshev.hpp
+++ include/boost/math/special_functions/chebyshev.hpp
@@ -6,6 +6,7 @@
 #ifndef BOOST_MATH_SPECIAL_CHEBYSHEV_HPP
 #define BOOST_MATH_SPECIAL_CHEBYSHEV_HPP
 #include <cmath>
+#include <boost/math/constants/constants.hpp>
 
 namespace boost { namespace math {
 
@@ -16,6 +17,26 @@ inline Real chebyshev_next(Real const & x, Real const & Tn, Real const & Tn_1)
 }
 
 namespace detail {
+
+template<class Real>
+std::vector<Real> discrete_cosine_transform_type_II(const Real* const x, size_t n)
+{
+    using std::cos;
+    using boost::math::constants::pi;
+    using boost::math::constants::half;
+    std::vector<Real> X(n, 0);
+    Real scale = static_cast<Real>(2)/static_cast<Real>(n);
+    for(size_t k = 0; k < n; ++k)
+    {
+        Real g = pi<Real>()*k/n;
+        for (size_t j = 0; j < n; ++j)
+        {
+           X[k] += x[k]*cos(g*(j+half<Real>()));
+        }
+        X[k] *= scale;
+    }
+    return X;
+}
 
 template<class Real, bool second=false>
 inline Real chebyshev_imp(unsigned n, Real const & x)
@@ -91,6 +112,174 @@ Real chebyshev_t_prime(unsigned n, Real const & x)
     }
     return n*detail::chebyshev_imp<Real, true>(n - 1, x);
 }
+
+/*
+ * This is Algorithm 3.1 of
+ * Gil, Amparo, Javier Segura, and Nico M. Temme.
+ * Numerical methods for special functions.
+ * Society for Industrial and Applied Mathematics, 2007.
+ * https://www.siam.org/books/ot99/OT99SampleChapter.pdf
+ * However, our definition of c0 differs . . .
+ */
+template<class Real>
+inline Real chebyshev_clenshaw_recurrence(const Real* const c, size_t length, Real x)
+{
+    using boost::math::constants::half;
+    Real b2 = 0;
+    Real b1 = c[length -1];
+    for(size_t j = length - 2; j >= 1; --j)
+    {
+        Real tmp = 2*x*b1 - b2 + c[j];
+        b2 = b1;
+        b1 = tmp;
+    }
+    return x*b1 - b2 + half<Real>()*c[0];
+}
+
+template<class Real>
+class chebyshev_transform
+{
+public:
+    template<class F>
+    chebyshev_transform(const F& f, Real a, Real b): m_a(a), m_b(b)
+    {
+        if (a >= b)
+        {
+            throw std::domain_error("a < b is required.\n");
+        }
+        using boost::math::constants::half;
+        using boost::math::constants::pi;
+        using std::cos;
+        using std::abs;
+        size_t n = 1024;
+        std::vector<Real> vf(n);
+        Real bma = (b-a)*half<Real>();
+        Real bpa = (b+a)*half<Real>();
+        for(size_t k = 0; k < n; ++k)
+        {
+            Real y = cos(pi<Real>()*(k+half<Real>())/static_cast<Real>(n));
+            vf[k] = f(y*bma + bpa);
+        }
+        m_coeffs = detail::discrete_cosine_transform_type_II(vf.data(), vf.size());
+        /*Real scale = static_cast<Real>(2)/static_cast<Real>(n);
+        m_coeffs.resize(n);
+        Real max_coeff = 0;
+        for(size_t j = 0; j < n; ++j)
+        {
+            Real compensator = 0;
+            Real sum = 0;
+            for(size_t k = 0; k < n; ++k)
+            {
+                Real y = vf[k]*cos(pi<Real>()*j*(k+half<Real>())/n);
+                Real t = sum + y;
+                compensator = (t - sum) - y;
+                sum = t;
+            }
+            m_coeffs[j] = scale*sum;
+            if (abs(m_coeffs[j]) > max_coeff)
+            {
+                max_coeff = abs(m_coeffs[j]);
+            }
+        }
+        for(auto c : m_coeffs)
+        {
+           std::cout << c << "\t";
+        }
+        std::cout << std::endl;
+        size_t j = m_coeffs.size() - 1;
+        while (abs(m_coeffs[j])/max_coeff < 10*std::numeric_limits<Real>::epsilon())
+        {
+            --j;
+        }
+
+        n = j + 2;
+        vf.resize(n);
+        for(size_t k = 0; k < n; ++k)
+        {
+            Real y = cos(pi<Real>()*(k+half<Real>())/static_cast<Real>(n));
+            vf[k] = f(y*bma + bpa);
+        }
+        scale = static_cast<Real>(2)/static_cast<Real>(n);
+        m_coeffs.resize(n);
+        for(size_t j = 0; j < n; ++j)
+        {
+            Real compensator = 0;
+            Real sum = 0;
+            for(size_t k = 0; k < n; ++k)
+            {
+                Real y = vf[k]*cos(pi<Real>()*j*(k+half<Real>())/n);
+                Real t = sum + y;
+                compensator = (t - sum) - y;
+                sum = t;
+            }
+            m_coeffs[j] = scale*sum;
+        }
+        std::cout << "Needed " << m_coeffs.size() << " coefficients\n";
+        for(auto c : m_coeffs)
+        {
+           std::cout << c << "\t";
+        }
+        std::cout << std::endl;*/
+
+    }
+
+    Real operator()(Real x) const
+    {
+        using boost::math::constants::half;
+        if (x > m_b || x < m_a)
+        {
+            throw std::domain_error("x not in [a, b]\n");
+        }
+
+        Real z = (2*x - m_a - m_b)/(m_b - m_a);
+        return chebyshev_clenshaw_recurrence(m_coeffs.data(), m_coeffs.size(), z);
+    }
+
+    // Integral over entire domain [a, b]
+    Real integrate() const
+    {
+          Real Q = m_coeffs[0]/2;
+          for(size_t j = 2; j < m_coeffs.size(); j += 2)
+          {
+              Q += -m_coeffs[j]/((j+1)*(j-1));
+          }
+          return (m_b - m_a)*Q;
+    }
+
+    // Integral from a <= x1 <= x2 <= b
+    Real integrate(Real x1, Real x2) const
+    {
+        if (x1 < m_a || x2 > m_b)
+        {
+            throw std::domain_error("Cannot integrate outside boundary of interpolation.\n");
+        }
+        return std::numeric_limits<Real>::quiet_Nan();
+    }
+
+    const std::vector<Real>& coefficients() const
+    {
+        return m_coeffs;
+    }
+
+    Real prime(Real x) const
+    {
+        // T_{n+1} = 2*x*T_{n} - T_{n-1}
+        using boost::math::constants::half;
+        Real yp = 0;
+        Real z = (2*x - m_a - m_b)/(m_b - m_a);
+        for (size_t i = 1; i < m_coeffs.size(); ++i)
+        {
+            yp += m_coeffs[i]*chebyshev_t_prime(i, z);
+        }
+        return 2*yp/(m_b - m_a);
+    }
+
+
+private:
+    std::vector<Real> m_coeffs;
+    Real m_a;
+    Real m_b;
+};
 
 }}
 #endif
--- test/chebyshev_test.cpp
+++ test/chebyshev_test.cpp
@@ -10,6 +10,7 @@
 #include <boost/test/included/unit_test.hpp>
 #include <boost/test/floating_point_comparison.hpp>
 #include <boost/math/special_functions/chebyshev.hpp>
+#include <boost/math/special_functions/sinc.hpp>
 #include <boost/multiprecision/cpp_bin_float.hpp>
 #include <boost/multiprecision/cpp_dec_float.hpp>
 
@@ -71,8 +72,176 @@ void test_derivatives()
     }
 }
 
+template<class Real>
+void test_sin_chebyshev_transform()
+{
+    using boost::math::chebyshev_transform;
+    using boost::math::constants::half_pi;
+    using std::sin;
+    using std::cos;
+    using std::abs;
+
+    Real tol = 200*std::numeric_limits<Real>::epsilon();
+    auto f = [](Real x) { return sin(x); };
+    Real a = 0;
+    Real b = 1;
+    chebyshev_transform<Real> cheb(f, a, b);
+
+    Real x = a;
+    while (x < b)
+    {
+        Real s = sin(x);
+        Real c = cos(x);
+        if (abs(s) < tol)
+        {
+            BOOST_CHECK_SMALL(cheb(x), tol);
+            BOOST_CHECK_CLOSE_FRACTION(c, cheb.prime(x), tol);
+        }
+        else
+        {
+            BOOST_CHECK_CLOSE_FRACTION(s, cheb(x), tol);
+            if (abs(c) < tol)
+            {
+                BOOST_CHECK_SMALL(cheb.prime(x), tol);
+            }
+            else
+            {
+                BOOST_CHECK_CLOSE_FRACTION(c, cheb.prime(x), tol);
+            }
+        }
+        x += static_cast<Real>(1)/static_cast<Real>(1 << 7);
+    }
+
+    Real Q = cheb.integrate();
+
+    BOOST_CHECK_CLOSE_FRACTION(1 - cos(static_cast<Real>(1)), Q, tol);
+}
+
+template<class Real>
+void test_clenshaw_recurrence()
+{
+    using boost::math::chebyshev_clenshaw_recurrence;
+    std::vector<Real> c0{2, 0, 0, 0, 0};
+    std::vector<Real> c1{0, 1, 0, 0};
+    std::vector<Real> c2{0, 0, 1, 0};
+    std::vector<Real> c3{0, 0, 0, 1, 0};
+    std::vector<Real> c4{0, 0, 0, 0, 1};
+    std::vector<Real> c5{0, 0, 0, 0, 0, 1};
+    std::vector<Real> c6{0, 0, 0, 0, 0, 0, 1};
+
+    Real x = -1;
+    Real tol = 10*std::numeric_limits<Real>::epsilon();
+    while (x <= 1)
+    {
+        Real y = chebyshev_clenshaw_recurrence(c0.data(), c0.size(), x);
+        BOOST_CHECK_CLOSE_FRACTION(y, chebyshev_t(0, x), tol);
+
+        y = chebyshev_clenshaw_recurrence(c1.data(), c1.size(), x);
+        BOOST_CHECK_CLOSE_FRACTION(y, chebyshev_t(1, x), tol);
+
+        y = chebyshev_clenshaw_recurrence(c2.data(), c2.size(), x);
+        BOOST_CHECK_CLOSE_FRACTION(y, chebyshev_t(2, x), tol);
+
+        y = chebyshev_clenshaw_recurrence(c3.data(), c3.size(), x);
+        BOOST_CHECK_CLOSE_FRACTION(y, chebyshev_t(3, x), tol);
+
+        y = chebyshev_clenshaw_recurrence(c4.data(), c4.size(), x);
+        BOOST_CHECK_CLOSE_FRACTION(y, chebyshev_t(4, x), tol);
+
+        y = chebyshev_clenshaw_recurrence(c5.data(), c5.size(), x);
+        BOOST_CHECK_CLOSE_FRACTION(y, chebyshev_t(5, x), tol);
+
+        y = chebyshev_clenshaw_recurrence(c6.data(), c6.size(), x);
+        BOOST_CHECK_CLOSE_FRACTION(y, chebyshev_t(6, x), tol);
+
+        x += static_cast<Real>(1)/static_cast<Real>(1 << 7);
+    }
+}
+
+template<class Real>
+void test_sinc_chebyshev_transform()
+{
+    using boost::math::sinc_pi;
+    using boost::math::chebyshev_transform;
+    using boost::math::constants::half_pi;
+
+    Real tol = 100*std::numeric_limits<Real>::epsilon();
+    auto f = [](Real x) { return boost::math::sinc_pi(x); };
+    Real a = 0;
+    Real b = 1;
+    chebyshev_transform<Real> cheb(f, a, b);
+
+    Real x = a;
+    while (x < b)
+    {
+        Real s = sinc_pi(x);
+        if (s < tol)
+        {
+            BOOST_CHECK_SMALL(cheb(x), tol);
+        }
+        else
+        {
+            BOOST_CHECK_CLOSE_FRACTION(s, cheb(x), tol);
+        }
+        x += static_cast<Real>(1)/static_cast<Real>(1 << 7);
+    }
+
+    Real Q = cheb.integrate();
+    //NIntegrate[Sinc[x], {x, 0, 1}, WorkingPrecision -> 200, AccuracyGoal -> 150, PrecisionGoal -> 150, MaxRecursion -> 150]
+    Real Q_exp = boost::lexical_cast<Real>("0.94608307036718301494135331382317965781233795473811179047145477356668");
+    BOOST_CHECK_CLOSE_FRACTION(Q_exp, Q, tol);
+}
+
+/*
+ * Examples taken from "Approximation Theory and Approximation Practice", by Trefethen
+ */
+template<class Real>
+void test_atap_examples()
+{
+    using std::sin;
+    using boost::math::constants::half;
+    using boost::math::sinc_pi;
+    using boost::math::chebyshev_transform;
+    using boost::math::constants::half_pi;
+
+    Real tol = 100*std::numeric_limits<Real>::epsilon();
+    auto f1 = [](Real x) { return ((0 < x) - (x < 0)) - x/2; };
+    auto f2 = [](Real x) { Real t = sin(6*x); Real s = sin(x + exp(2*x));
+                           Real u = (0 < s) - (s < 0);
+                           return t + u; };
+
+    auto f3 = [](Real x) { sin(6*x) + sin(60*exp(x)); };
+
+    auto f4 = [](Real x) { return 1/(1+1000*(x+half<Real>())*(x+half<Real>())) + 1/sqrt(1+1000*(x-.5)*(x-0.5));};
+    Real a = -1;
+    Real b = 1;
+    //chebyshev_transform<Real> cheb1(f1, a, b);
+    chebyshev_transform<Real> cheb2(f2, a, b);
+
+    Real x = a;
+    while (x < b)
+    {
+        Real s = f1(x);
+        //BOOST_CHECK_CLOSE_FRACTION(s, cheb1(x), tol);
+        //BOOST_CHECK_CLOSE_FRACTION(f2(x), cheb2(x), tol);
+        x += static_cast<Real>(1)/static_cast<Real>(1 << 7);
+    }
+}
+
+
 BOOST_AUTO_TEST_CASE(chebyshev_test)
 {
+    test_atap_examples<long double>();
+    //test_sin_chebyshev_transform<long double>();
+    /*
+    test_clenshaw_recurrence<double>();
+
+    test_sinc_chebyshev_transform<double>();
+    test_sinc_chebyshev_transform<long double>();
+    test_sinc_chebyshev_transform<cpp_bin_float_quad>();
+    test_sinc_chebyshev_transform<cpp_bin_float_100>();
+
+
     test_polynomials<float>();
     test_polynomials<double>();
     test_polynomials<long double>();
@@ -81,5 +250,5 @@ BOOST_AUTO_TEST_CASE(chebyshev_test)
     test_derivatives<float>();
     test_derivatives<double>();
     test_derivatives<long double>();
-    test_derivatives<cpp_bin_float_quad>();
+    test_derivatives<cpp_bin_float_quad>();*/
 }
