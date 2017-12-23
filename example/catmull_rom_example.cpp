d25677a7043c2f5765d094d7c3bb1c3bb587da13
Nick Thompson <nathompson7@protonmail.com>
2017-12-22 23:07:51 -0700

    Fix Archmedean spiral example for Catmull-Rom Curve.

--- example/catmull_rom_example.cpp
+++ example/catmull_rom_example.cpp
@@ -10,6 +10,8 @@
 #include <cmath>
 #include <boost/math/interpolators/catmull_rom.hpp>
 
+using std::sin;
+using std::cos;
 using boost::math::catmull_rom;
 
 int main()
@@ -21,24 +23,21 @@ int main()
     double theta_max = M_PI;
     for (size_t i = 0; i < spiral_points.size(); ++i)
     {
-        double theta = i/theta_max;
+        double theta = ((double) i/ (double) spiral_points.size())*theta_max;
         spiral_points[i] = {theta*cos(theta), theta*sin(theta)};
     }
 
     auto archimedean = catmull_rom<double, std::array<double,2>, 2>(spiral_points.data(), spiral_points.size());
     double max_s = archimedean.max_parameter();
-    for (double s = 0; s < max_s; s += 0.1)
+    std::cout << "Max s = " << max_s << std::endl;
+    for (double s = 0; s < max_s; s += 0.01)
     {
         auto p = archimedean(s);
         double x = p[0];
         double y = p[1];
         double r = sqrt(x*x + y*y);
         double theta = atan2(y/r, x/r);
-        if (theta < 0)
-        {
-           theta += 2*M_PI;
-        }
-        std::cout << "r = " << r << ", theta = " << theta << std::endl;
+        std::cout << "r = " << r << ", theta = " << theta << ", r - theta = " << r - theta << std::endl;
     }
 
     return 0;
