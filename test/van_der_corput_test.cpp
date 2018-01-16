d946d61d635d81296108e657b718e884162001ea
Nick Thompson <nathompson7@protonmail.com>
2018-01-16 10:42:32 -0600

    Maybe I forgot to add the van der Corput test last time . . .

---
+++ test/van_der_corput_test.cpp
@@ -0,0 +1,63 @@
+/*
+ *  (C) Copyright Nick Thompson 2018.
+ *  Use, modification and distribution are subject to the
+ *  Boost Software License, Version 1.0. (See accompanying file
+ *  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+ *
+ */
+
+#define BOOST_TEST_MODULE van_der_corput_test
+#include <iostream>
+#include <iomanip>
+#include <boost/test/included/unit_test.hpp>
+#include <boost/math/tools/van_der_corput.hpp>
+#include <boost/math/special_functions/prime.hpp>
+
+using boost::math::van_der_corput;
+using boost::math::prime;
+using std::numeric_limits;
+
+
+template<class Real>
+void test_base_2()
+{
+    Real tol = numeric_limits<Real>::epsilon();
+    Real z = van_der_corput<Real, int>(1, 2);
+    BOOST_CHECK_CLOSE_FRACTION(z, (Real) 1/ (Real) 2, tol);
+    z = van_der_corput<Real, int>(2, 2);
+    BOOST_CHECK_CLOSE_FRACTION(z, (Real) 1/ (Real) 4, tol);
+    z = van_der_corput<Real, int>(3, 2);
+    BOOST_CHECK_CLOSE_FRACTION(z, (Real) 3/ (Real) 4, tol);
+    z = van_der_corput<Real, int>(4, 2);
+    BOOST_CHECK_CLOSE_FRACTION(z, (Real) 1/ (Real) 8, tol);
+    z = van_der_corput<Real, int>(5, 2);
+    BOOST_CHECK_CLOSE_FRACTION(z, (Real) 5/ (Real) 8, tol);
+}
+
+template<class Real>
+void test_van_der_corput()
+{
+    Real tol = 5*numeric_limits<Real>::epsilon();
+    for (int b = 3; b < 10000; ++b)
+    {
+        Real z = van_der_corput<Real, int>(0, b);
+        BOOST_CHECK_EQUAL(z, 0);
+        z = van_der_corput<Real, int>(1, b);
+        BOOST_CHECK_CLOSE_FRACTION(z, (Real) 1/ (Real) b, tol);
+        z = van_der_corput<Real, int>(2, b);
+        BOOST_CHECK_CLOSE_FRACTION(z, (Real) 2/ (Real) b, tol);
+        z = van_der_corput<Real, int>(b, b);
+        Real expected = (Real) 1/ (Real) b;
+        expected /= (Real) b;
+        BOOST_CHECK_CLOSE_FRACTION(z, expected, tol);
+        z = van_der_corput<Real, int>(b*b, b);
+        expected /= (Real) b;
+        BOOST_CHECK_CLOSE_FRACTION(z, expected, tol);
+    }
+}
+
+BOOST_AUTO_TEST_CASE(van_der_corput_test)
+{
+    test_base_2<double>();
+    test_van_der_corput<double>();
+}
