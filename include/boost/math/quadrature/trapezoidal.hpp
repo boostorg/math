90273e12a7415e9d53e81e9c6900b58b25cf1fe7
Nick Thompson <nathompson7@protonmail.com>
2018-04-03 11:07:03 +0800

    Fix C++11/C++17 incompatibility by typdefs. Increase default max refinements.

--- include/boost/math/quadrature/trapezoidal.hpp
+++ include/boost/math/quadrature/trapezoidal.hpp
@@ -18,7 +18,7 @@
 #include <cmath>
 #include <limits>
 #include <stdexcept>
-#include <type_traits> // for std::result_of
+#include <type_traits> // for std::result_of, std::invoke_result
 #include <boost/math/constants/constants.hpp>
 #include <boost/math/special_functions/fpclassify.hpp>
 #include <boost/math/policies/error_handling.hpp>
@@ -26,33 +26,46 @@
 namespace boost{ namespace math{ namespace quadrature {
 
 template<class F, class Real, class Policy>
-typename std::result_of<F(Real)>::type trapezoidal(F f, Real a, Real b, Real tol, std::size_t max_refinements, Real* error_estimate, Real* L1, const Policy& pol)
+auto trapezoidal(F f, Real a, Real b, Real tol, std::size_t max_refinements, Real* error_estimate, Real* L1, const Policy& pol)
 {
     static const char* function = "boost::math::quadrature::trapezoidal<%1%>(F, %1%, %1%, %1%)";
     using std::abs;
     using boost::math::constants::half;
+    using std::numeric_limits;
+    // In many math texts, K represents the field of real or complex numbers.
+    // Too bad we can't put blackboard bold into C++ source!
+#ifdef BOOST_NO_CXX17_STD_INVOKE
+    typedef typename std::result_of<F(Real)>::type K;
+#else
+    typedef typename std::invoke_result<F, Real>::type K;
+#endif
     if(a >= b)
     {
-        return boost::math::policies::raise_domain_error(function, "a < b for integration over the region [a, b] is required, but got a = %1%.\n", a, pol);
+        boost::math::policies::raise_domain_error(function, "a < b for integration over the region [a, b] is required, but got a = %1%.\n", a, pol);
+        return static_cast<K>(numeric_limits<Real>::quiet_NaN());
     }
     if (!(boost::math::isfinite)(a))
     {
-       return boost::math::policies::raise_domain_error(function, "Left endpoint of integration must be finite for adaptive trapezoidal integration but got a = %1%.\n", a, pol);
+       boost::math::policies::raise_domain_error(function, "Left endpoint of integration must be finite for adaptive trapezoidal integration but got a = %1%.\n", a, pol);
+       return static_cast<K>(numeric_limits<Real>::quiet_NaN());
     }
     if (!(boost::math::isfinite)(b))
     {
-       return boost::math::policies::raise_domain_error(function, "Right endpoint of integration must be finite for adaptive trapedzoidal integration but got b = %1%.\n", b, pol);
+       boost::math::policies::raise_domain_error(function, "Right endpoint of integration must be finite for adaptive trapedzoidal integration but got b = %1%.\n", b, pol);
+       return static_cast<K>(numeric_limits<Real>::quiet_NaN());
     }
 
-    typename std::result_of<F(Real)>::type ya = f(a);
-    typename std::result_of<F(Real)>::type yb = f(b);
+
+    K ya = f(a);
+    K yb = f(b);
     Real h = (b - a)*half<Real>();
-    typename std::result_of<F(Real)>::type I0 = (ya + yb)*h;
+    K I0 = (ya + yb)*h;
     Real IL0 = (abs(ya) + abs(yb))*h;
 
-    typename std::result_of<F(Real)>::type yh = f(a + h);
-    typename std::result_of<F(Real)>::type I1 = half<Real>()*I0 + yh*h;
-    Real IL1 = half<Real>()*IL0 + abs(yh)*h;
+    K yh = f(a + h);
+    K I1;
+    I1 = I0*half<Real>() + yh*h;
+    Real IL1 = IL0*half<Real>() + abs(yh)*h;
 
     // The recursion is:
     // I_k = 1/2 I_{k-1} + 1/2^k \sum_{j=1; j odd, j < 2^k} f(a + j(b-a)/2^k)
@@ -66,16 +79,16 @@ typename std::result_of<F(Real)>::type trapezoidal(F f, Real a, Real b, Real tol
         I0 = I1;
         IL0 = IL1;
 
-        I1 = half<Real>()*I0;
-        IL1 = half<Real>()*IL0;
+        I1 = I0*half<Real>();
+        IL1 = IL0*half<Real>();
         std::size_t p = static_cast<std::size_t>(1u) << k;
         h *= half<Real>();
-        decltype(yb) sum = 0;
+        K sum = 0;
         Real absum = 0;
 
         for(std::size_t j = 1; j < p; j += 2)
         {
-            typename std::result_of<F(Real)>::type y = f(a + j*h);
+            K y = f(a + j*h);
             sum += y;
             absum += abs(y);
         }
@@ -96,18 +109,18 @@ typename std::result_of<F(Real)>::type trapezoidal(F f, Real a, Real b, Real tol
         *L1 = IL1;
     }
 
-    return I1;
+    return static_cast<K>(I1);
 }
 #if BOOST_WORKAROUND(BOOST_MSVC, < 1800)
 // Template argument dedcution failure otherwise:
 template<class F, class Real>
-typename std::result_of<F(Real)>::type trapezoidal(F f, Real a, Real b, Real tol = 0, std::size_t max_refinements = 10, Real* error_estimate = 0, Real* L1 = 0)
+auto trapezoidal(F f, Real a, Real b, Real tol = 0, std::size_t max_refinements = 12, Real* error_estimate = 0, Real* L1 = 0)
 #elif !defined(BOOST_NO_CXX11_NULLPTR)
 template<class F, class Real>
-typename std::result_of<F(Real)>::type trapezoidal(F f, Real a, Real b, Real tol = boost::math::tools::root_epsilon<Real>(), std::size_t max_refinements = 10, Real* error_estimate = nullptr, Real* L1 = nullptr)
+auto trapezoidal(F f, Real a, Real b, Real tol = boost::math::tools::root_epsilon<Real>(), std::size_t max_refinements = 12, Real* error_estimate = nullptr, Real* L1 = nullptr)
 #else
 template<class F, class Real>
-typename std::result_of<F(Real)>::type trapezoidal(F f, Real a, Real b, Real tol = boost::math::tools::root_epsilon<Real>(), std::size_t max_refinements = 10, Real* error_estimate = 0, Real* L1 = 0)
+auto trapezoidal(F f, Real a, Real b, Real tol = boost::math::tools::root_epsilon<Real>(), std::size_t max_refinements = 12, Real* error_estimate = 0, Real* L1 = 0)
 #endif
 {
 #if BOOST_WORKAROUND(BOOST_MSVC, <= 1600)
