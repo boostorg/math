e415650f86d46e630dc67643bacc30e4e24a5463
jzmaddock <john@johnmaddock.co.uk>
2019-05-26 11:45:35 +0100

    prime: Correct value of max_prime and add test. Fixes https://github.com/boostorg/math/issues/214.

--- test/compile_test/sf_prime_incl_test.cpp
+++ test/compile_test/sf_prime_incl_test.cpp
@@ -20,7 +20,8 @@ void compile_and_link_test()
    // Add constexpr tests here:
    //
 #ifdef BOOST_MATH_HAVE_CONSTEXPR_TABLES
-   constexpr boost::uint32_t ce_f = boost::math::prime(2);
+   constexpr boost::uint32_t ce_f = boost::math::prime(boost::math::max_prime);
+   static_assert(ce_f == 104729, "max_prime had incorrect value");
    check_result<boost::uint32_t>(ce_f);
 #endif
 }
