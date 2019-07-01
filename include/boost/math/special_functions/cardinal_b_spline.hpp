07d9c3d1f3962ab28c9b677cccae6c23461d3e0b
Nick Thompson <nathompson7@protonmail.com>
2019-07-01 09:42:18 -0400

    Cardinal B-splines: Add forward fractional B-splines [CI SKIP]

--- include/boost/math/special_functions/cardinal_b_spline.hpp
+++ include/boost/math/special_functions/cardinal_b_spline.hpp
@@ -7,7 +7,7 @@
 #define BOOST_MATH_SPECIAL_CARDINAL_B_SPLINE_HPP
 
 #include <boost/math/special_functions/factorials.hpp>
-//#include <boost/math/special_functions/binomial.hpp>
+#include <boost/math/special_functions/gamma.hpp>
 
 namespace boost { namespace math {
 
@@ -25,13 +25,14 @@ Real cardinal_b_spline_summation_impl(unsigned n, Real x)
         return Real(0);
     }
     Real z = x + Real(n+1)/2;
-    unsigned kmax = floor(z);
+    int kmax = floor(z);
 
     Real result = pow(z,n);
     z -= 1;
     Real kfact = 1;
     Real num = n+1;
-    for (unsigned k = 1; k <= kmax; ++k) {
+    for (int k = 1; k <= kmax; ++k)
+    {
         kfact *= k;
         Real term = pow(z,n)*num/kfact;
         num *= n-k+1;
@@ -46,23 +47,29 @@ Real cardinal_b_spline_summation_impl(unsigned n, Real x)
 }
 
 template<class Real>
-Real cardinal_b_spline_recursive_impl(unsigned n, Real x) {
+Real cardinal_b_spline_recursive_impl(unsigned n, Real x)
+{
     if (x < 0)
     {
         return detail::cardinal_b_spline_recursive_impl(n, -x);
     }
-    if (n == 0) {
-        if (x < Real(1)/Real(2)) {
+    if (n == 0)
+    {
+        if (x < Real(1)/Real(2))
+        {
             return Real(1);
         }
-        else if (x > Real(1)/Real(2)) {
+        else if (x > Real(1)/Real(2))
+        {
             return Real(0);
         }
-        else if (x == Real(1)/Real(2)) {
+        else if (x == Real(1)/Real(2))
+        {
             // Value of the Fourier series at the discontinuity:
             return Real(1)/Real(2);
         }
-        else {
+        else
+        {
             // Who knows what happened?
             return std::numeric_limits<Real>::quiet_NaN();
         }
@@ -70,7 +77,8 @@ Real cardinal_b_spline_recursive_impl(unsigned n, Real x) {
 
     Real n_ = n;
     Real supp_max = (n_ + 1)/Real(2);
-    if (x >= supp_max) {
+    if (x >= supp_max)
+    {
         return Real(0);
     }
     Real a = (supp_max + x)/n_;
@@ -80,8 +88,10 @@ Real cardinal_b_spline_recursive_impl(unsigned n, Real x) {
 }
 
 template<class Real>
-Real cardinal_b_spline(unsigned n, Real x) {
-    if (n <= 5) {
+Real cardinal_b_spline(unsigned n, Real x)
+{
+    if (n <= 5)
+    {
         return detail::cardinal_b_spline_recursive_impl(n, x);
     }
     return detail::cardinal_b_spline_summation_impl(n,x);
@@ -89,15 +99,19 @@ Real cardinal_b_spline(unsigned n, Real x) {
 
 
 template<class Real>
-Real cardinal_b_spline_prime(unsigned n, Real x) {
-    if (x < 0) {
+Real cardinal_b_spline_prime(unsigned n, Real x)
+{
+    if (x < 0)
+    {
         return -cardinal_b_spline_prime(n, -x);
     }
 
-    if (n==0) {
+    if (n==0)
+    {
         // What is the sensible thing to do for the weak derivative?
         // Any better ideas are welcome.
-        if (x == Real(1)/Real(2)) {
+        if (x == Real(1)/Real(2))
+        {
             return std::numeric_limits<Real>::infinity();
         }
         return Real(0);
@@ -107,15 +121,19 @@ Real cardinal_b_spline_prime(unsigned n, Real x) {
 
 template<class Real>
 Real cardinal_b_spline_double_prime(unsigned n, Real x) {
-    if (x < 0) {
+    if (x < 0)
+    {
         return cardinal_b_spline_double_prime(n, -x);
     }
 
-    if (n==0) {
+    if (n==0)
+    {
         throw std::domain_error("The second derivative of a discontinuous function does not exist.");
     }
-    if (n==1) {
-        if (x==1) {
+    if (n==1)
+    {
+        if (x==1)
+        {
             return std::numeric_limits<Real>::infinity();
         }
         return Real(0);
@@ -127,6 +145,36 @@ Real cardinal_b_spline_double_prime(unsigned n, Real x) {
 template<class Real>
 Real forward_cardinal_b_spline(unsigned n, Real x) {
     return cardinal_b_spline(n, x - (n+1)/Real(2));
+}
+
+// Fractional Splines and Wavelets, Unser and Blu, Siam Review, 2000:
+// https://infoscience.epfl.ch/record/63073/files/unser9901.pdf, equation 2.2:
+template<class Real>
+Real forward_fractional_cardinal_b_spline(Real alpha, Real x) {
+    using std::pow;
+    using std::floor;
+
+    if (alpha <= -1)
+    {
+        throw std::domain_error("Fractional B-splines are not defined for alpha < -1.");
+    }
+    if (x <= 0)
+    {
+        return Real(0);
+    }
+    using std::floor;
+    int kmax = static_cast<int>(floor(x));
+    Real kfact = 1;
+    Real numerator = 1;
+    Real result = pow(x, alpha);
+    for (int k = 1; k <= kmax; ++k)
+    {
+        x -= 1;
+        kfact *= -k;
+        numerator *=  (alpha + 2 - k);
+        result += numerator*pow(x, alpha)/kfact;
+    }
+    return result/boost::math::tgamma(alpha+1);
 }
 
 }}
