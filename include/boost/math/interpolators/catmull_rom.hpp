1c45b8f1c8b1e25a51130187b7630ce0cdaa5ace
jzmaddock <john@johnmaddock.co.uk>
2019-07-07 09:22:45 +0100

    catmull_rom: correct sign of loop variable. Corrects previous commit. See: https://github.com/boostorg/math/issues/226

--- include/boost/math/interpolators/catmull_rom.hpp
+++ include/boost/math/interpolators/catmull_rom.hpp
@@ -110,7 +110,7 @@ catmull_rom<Point, RandomAccessContainer>::catmull_rom(RandomAccessContainer&& p
     m_pnts[num_pnts+2] = m_pnts[1];
 
     auto tmp = m_pnts[num_pnts-1];
-    for (std::size_t i = num_pnts-1; i >= 0; --i)
+    for (std::ptrdiff_t i = num_pnts-1; i >= 0; --i)
     {
         m_pnts[i+1] = m_pnts[i];
     }
