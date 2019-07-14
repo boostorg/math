de7cf6d67fa0c2f37e7868d3134800556d1b6bed
NAThompson <nathompson7@protonmail.com>
2019-07-14 15:56:56 -0400

    More on B-splines [CI SKIP]

--- include/boost/math/special_functions/cardinal_b_spline.hpp
+++ include/boost/math/special_functions/cardinal_b_spline.hpp
@@ -6,206 +6,84 @@
 #ifndef BOOST_MATH_SPECIAL_CARDINAL_B_SPLINE_HPP
 #define BOOST_MATH_SPECIAL_CARDINAL_B_SPLINE_HPP
 
-#include <boost/math/special_functions/factorials.hpp>
-#include <boost/math/special_functions/gamma.hpp>
-#include <boost/math/special_functions/binomial.hpp>
+#include <array>
 
 namespace boost { namespace math {
 
 namespace detail {
 
-template<class Real>
-Real cardinal_b_spline_summation_impl(unsigned n, Real x)
-{
-    using std::floor;
-    using std::pow;
-    using std::ceil;
-    if (x < 0) {
-        return cardinal_b_spline_summation_impl(n, -x);
+  template<class Real>
+  inline Real B1(Real x)
+  {
+    if (x < 0)
+    {
+      return B1(-x);
     }
-    if (x >= Real(n+1)/2) {
-        return Real(0);
+    if (x < Real(1))
+    {
+      return 1 - x;
     }
-    if (x <= Real(n+1)/4) {
-        Real z = x + Real(n+1)/2;
-        int kmax = floor(z);
+    return Real(0);
+  }
+}
 
-        Real result = pow(z,n);
-        z -= 1;
-        Real kfact = 1;
-        Real num = n+1;
-        for (int k = 1; k <= kmax; ++k)
-        {
-            kfact *= k;
-            Real term = pow(z,n)*num/kfact;
-            num *= n-k+1;
-            if (k&1) {
-                result -= term;
-            } else {
-                result += term;
-            }
-            z -= 1;
-        }
-        return result/factorial<Real>(n);
+template<unsigned n, typename Real>
+Real cardinal_b_spline(Real x) {
+  if (x < 0) {
+      // All B-splines are even functions:
+      return cardinal_b_spline<n>(-x);
+  }
+
+  if constexpr (n==0)
+  {
+    if (x < Real(1)/Real(2)) {
+      return Real(1);
+    }
+    else if (x == Real(1)/Real(2)) {
+      return Real(1)/Real(2);
     }
     else {
-        // Schoenberg, Cardinal Spline interpolation, equation 1.4:
-        using std::ceil;
-        int kmin = ceil(x + Real(n+1)/2);
-        Real z = kmin - (x + Real(n+1)/2);
-        Real result = 0;
-        for (int k = kmin; k <= n+1; ++k)
-        {
-            Real term = binomial_coefficient<Real>(n+1, k)*pow(z, n);
-            if (k&1)
-            {
-                result -= term;
-            } else {
-                result += term;
-            }
-            z += 1;
-        }
-        if (n & 1)
-        {
-            return result/factorial<Real>(n);
-        }
-        else
-        {
-            return -result/factorial<Real>(n);
-        }
-
+      return Real(0);
     }
+  }
+
+  if constexpr (n==1) {
+    return detail::B1(x);
+  }
+
+  Real supp_max = (n+1)/Real(2);
+  if (x >= supp_max)
+  {
+    return Real(0);
+  }
+
+  // Fill v with values of B
+  std::array<Real, n> v;
+  Real z = x + 1 - supp_max;
+  for (unsigned i = 0; i < n; ++i)
+  {
+      v[i] = detail::B1(z);
+      z += 1;
+  }
+
+  for (unsigned j = 2; j <= n; ++j)
+  {
+    Real a = (x + j + 1 - supp_max);
+    Real b = (supp_max - x);
+    for(unsigned k = 0; k <= n - j; ++k)
+    {
+      v[k] = (a*v[k+1] + b*v[k])/Real(j);
+      a += 1;
+      b -= 1;
+    }
+  }
+
+  return v[0];
 }
 
-template<class Real>
-Real cardinal_b_spline_recursive_impl(unsigned n, Real x)
-{
-    if (x < 0)
-    {
-        return detail::cardinal_b_spline_recursive_impl(n, -x);
-    }
-    if (n == 0)
-    {
-        if (x < Real(1)/Real(2))
-        {
-            return Real(1);
-        }
-        else if (x > Real(1)/Real(2))
-        {
-            return Real(0);
-        }
-        else if (x == Real(1)/Real(2))
-        {
-            // Value of the Fourier series at the discontinuity:
-            return Real(1)/Real(2);
-        }
-        else
-        {
-            // Who knows what happened?
-            return std::numeric_limits<Real>::quiet_NaN();
-        }
-    }
-
-    Real n_ = n;
-    Real supp_max = (n_ + 1)/Real(2);
-    if (x >= supp_max)
-    {
-        return Real(0);
-    }
-    Real a = (supp_max + x)/n_;
-    Real b = (supp_max - x)/n_;
-    return a*cardinal_b_spline_recursive_impl(n-1, x+1/Real(2)) + b*cardinal_b_spline_recursive_impl(n-1, x - 1/Real(2));
-}
-}
-
-template<class Real>
-Real cardinal_b_spline(unsigned n, Real x)
-{
-    if (n <= 5)
-    {
-        return detail::cardinal_b_spline_recursive_impl(n, x);
-    }
-    return detail::cardinal_b_spline_summation_impl(n,x);
-}
-
-
-template<class Real>
-Real cardinal_b_spline_prime(unsigned n, Real x)
-{
-    if (x < 0)
-    {
-        return -cardinal_b_spline_prime(n, -x);
-    }
-
-    if (n==0)
-    {
-        // What is the sensible thing to do for the weak derivative?
-        // Any better ideas are welcome.
-        if (x == Real(1)/Real(2))
-        {
-            return std::numeric_limits<Real>::infinity();
-        }
-        return Real(0);
-    }
-    return cardinal_b_spline(n-1, x+1/Real(2)) - cardinal_b_spline(n-1, x-1/Real(2));
-}
-
-template<class Real>
-Real cardinal_b_spline_double_prime(unsigned n, Real x) {
-    if (x < 0)
-    {
-        return cardinal_b_spline_double_prime(n, -x);
-    }
-
-    if (n==0)
-    {
-        throw std::domain_error("The second derivative of a discontinuous function does not exist.");
-    }
-    if (n==1)
-    {
-        if (x==1)
-        {
-            return std::numeric_limits<Real>::infinity();
-        }
-        return Real(0);
-    }
-    return cardinal_b_spline(n-2, x+1) - 2*cardinal_b_spline(n-2, x) + cardinal_b_spline(n-2, x-1);
-}
-
-
-template<class Real>
-Real forward_cardinal_b_spline(unsigned n, Real x) {
-    return cardinal_b_spline(n, x - (n+1)/Real(2));
-}
-
-// Fractional Splines and Wavelets, Unser and Blu, Siam Review, 2000:
-// https://infoscience.epfl.ch/record/63073/files/unser9901.pdf, equation 2.2:
-template<class Real>
-Real forward_fractional_cardinal_b_spline(Real alpha, Real x) {
-    using std::pow;
-    using std::floor;
-
-    if (alpha <= -1)
-    {
-        throw std::domain_error("Fractional B-splines are not defined for alpha < -1.");
-    }
-    if (x <= 0)
-    {
-        return Real(0);
-    }
-    using std::floor;
-    int kmax = static_cast<int>(floor(x));
-    Real kfact = 1;
-    Real numerator = 1;
-    Real result = pow(x, alpha);
-    for (int k = 1; k <= kmax; ++k)
-    {
-        x -= 1;
-        kfact *= -k;
-        numerator *=  (alpha + 2 - k);
-        result += numerator*pow(x, alpha)/kfact;
-    }
-    return result/boost::math::tgamma(alpha+1);
+template<unsigned n, class Real>
+Real forward_cardinal_b_spline(Real x) {
+    return cardinal_b_spline<n>(x - (n+1)/Real(2));
 }
 
 }}
--- test/cardinal_b_spline_test.cpp
+++ test/cardinal_b_spline_test.cpp
@@ -18,17 +18,16 @@ using boost::multiprecision::float128;
 
 using std::abs;
 using boost::math::cardinal_b_spline;
-using boost::math::cardinal_b_spline_prime;
 using boost::math::forward_cardinal_b_spline;
 
 template<class Real>
 void test_box()
 {
-    Real t = cardinal_b_spline(0,1.1);
+    Real t = cardinal_b_spline<0>(1.1);
     Real expected = 0;
     CHECK_ULP_CLOSE(expected, t, 0);
 
-    t = cardinal_b_spline(0, -1.1);
+    t = cardinal_b_spline<0>(-1.1);
     expected = 0;
     CHECK_ULP_CLOSE(expected, t, 0);
 
@@ -36,24 +35,24 @@ void test_box()
     for (Real t = -Real(1)/Real(2)+h; t < Real(1)/Real(2); t += h)
     {
         expected = 1;
-        CHECK_ULP_CLOSE(expected, cardinal_b_spline(0, t), 0);
+        CHECK_ULP_CLOSE(expected, cardinal_b_spline<0>(t), 0);
     }
 
     for (Real t = h; t < 1; t += h)
     {
         expected = 1;
-        CHECK_ULP_CLOSE(expected, forward_cardinal_b_spline(0, t), 0);
+        CHECK_ULP_CLOSE(expected, forward_cardinal_b_spline<0>(t), 0);
     }
 }
 
 template<class Real>
 void test_hat()
 {
-    Real t = cardinal_b_spline(1,2.1);
+    Real t = cardinal_b_spline<1>(2.1);
     Real expected = 0;
     CHECK_ULP_CLOSE(expected, t, 0);
 
-    t = cardinal_b_spline(1, -2.1);
+    t = cardinal_b_spline<1>(-2.1);
     expected = 0;
     CHECK_ULP_CLOSE(expected, t, 0);
 
@@ -61,29 +60,16 @@ void test_hat()
     for (Real t = -1; t <= 1; t += h)
     {
         expected = 1-abs(t);
-        if(!CHECK_ULP_CLOSE(expected, cardinal_b_spline(1, t), 0) )
+        if(!CHECK_ULP_CLOSE(expected, cardinal_b_spline<1>(t), 0) )
         {
             std::cerr << "  Problem at t = " << t << "\n";
-        }
-        if (t < 0 && t > -1) {
-            Real expected = 1;
-            if(!CHECK_ULP_CLOSE(expected, cardinal_b_spline_prime(1, t), 0) )
-            {
-                std::cerr << "  Problem at t = " << t << "\n";
-            }
-        }
-        else if (t > 0 && t < 1) {
-            if(!CHECK_ULP_CLOSE(-1, cardinal_b_spline_prime(1, t), 0) )
-            {
-                std::cerr << "  Problem at t = " << t << "\n";
-            }
         }
     }
 
     for (Real t = 0; t < 2; t += h)
     {
         expected = 1 - abs(t-1);
-        CHECK_ULP_CLOSE(expected, forward_cardinal_b_spline(1,t), 0);
+        CHECK_ULP_CLOSE(expected, forward_cardinal_b_spline<1>(t), 0);
     }
 }
 
@@ -108,8 +94,45 @@ void test_quadratic()
     Real h = 1/Real(256);
     for (Real t = -5; t <= 5; t += h) {
         Real expected = b2(t);
-        CHECK_ULP_CLOSE(expected, cardinal_b_spline(2,t), 0);
+        CHECK_ULP_CLOSE(expected, cardinal_b_spline<2>(t), 0);
     }
+}
+
+template<class Real>
+void test_cubic()
+{
+    Real expected = Real(2)/Real(3);
+    Real computed = cardinal_b_spline<3, Real>(0);
+    CHECK_ULP_CLOSE(expected, computed, 0);
+
+    expected = Real(1)/Real(6);
+    computed = cardinal_b_spline<3, Real>(1);
+    CHECK_ULP_CLOSE(expected, computed, 0);
+
+    expected = Real(0);
+    computed = cardinal_b_spline<3, Real>(2);
+    CHECK_ULP_CLOSE(expected, computed, 0);
+}
+
+template<class Real>
+void test_quintic()
+{
+  Real expected = Real(11)/Real(20);
+  Real computed = cardinal_b_spline<5, Real>(0);
+  CHECK_ULP_CLOSE(expected, computed, 0);
+
+  expected = Real(13)/Real(60);
+  computed = cardinal_b_spline<5, Real>(1);
+  CHECK_ULP_CLOSE(expected, computed, 1);
+
+  expected = Real(1)/Real(120);
+  computed = cardinal_b_spline<5, Real>(2);
+  CHECK_ULP_CLOSE(expected, computed, 0);
+
+  expected = Real(0);
+  computed = cardinal_b_spline<5, Real>(3);
+  CHECK_ULP_CLOSE(expected, computed, 0);
+
 }
 
 
@@ -127,10 +150,21 @@ int main()
     test_quadratic<double>();
     test_quadratic<long double>();
 
+    test_cubic<float>();
+    test_cubic<double>();
+    test_cubic<long double>();
+
+    test_quintic<float>();
+    test_quintic<double>();
+    test_quintic<long double>();
+
+
 #ifdef BOOST_HAS_FLOAT128
     test_box<float128>();
     test_hat<float128>();
     test_quadratic<float128>();
+    test_cubic<float128>();
+    test_quintic<float128>();
 #endif
 
     return boost::math::test::report_errors();
