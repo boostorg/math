b57c27b245928857ed3abe4d819a22f1bf32c823
jzmaddock <john@johnmaddock.co.uk>
2019-07-19 09:54:27 +0100

    1F1: remove dead code from test_1F0.cpp, rebalance test grouping in Jamfile to avoid timeouts.

--- test/test_1F0.cpp
+++ test/test_1F0.cpp
@@ -8,39 +8,9 @@
 #include <boost/multiprecision/cpp_bin_float.hpp>
 #include <boost/multiprecision/cpp_dec_float.hpp>
 
-void expected_results()
-{
-   //
-   // Define the max and mean errors expected for
-   // various compilers and platforms.
-   //
-   const char* largest_type;
-#ifndef BOOST_MATH_NO_LONG_DOUBLE_MATH_FUNCTIONS
-   if(boost::math::policies::digits<double, boost::math::policies::policy<> >() == boost::math::policies::digits<long double, boost::math::policies::policy<> >())
-   {
-      largest_type = "(long\\s+)?double";
-   }
-   else
-   {
-      largest_type = "long double";
-   }
-#else
-   largest_type = "(long\\s+)?double";
-#endif
-
-   //
-   // Finish off by printing out the compiler/stdlib/platform names,
-   // we do this to make it easier to mark up expected error rates.
-   //
-   std::cout << "Tests run with " << BOOST_COMPILER << ", " 
-      << BOOST_STDLIB << ", " << BOOST_PLATFORM << std::endl;
-}
 
 BOOST_AUTO_TEST_CASE( test_main )
 {
-   expected_results();
-   BOOST_MATH_CONTROL_FP;
-
 #ifndef BOOST_MATH_BUGGY_LARGE_FLOAT_CONSTANTS
    test_spots(0.0F);
 #endif
