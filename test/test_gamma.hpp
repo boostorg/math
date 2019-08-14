35c36e3c2dd4e3a97fe0bb386598e4064055971f
jzmaddock <john@johnmaddock.co.uk>
2019-08-14 11:24:22 +0100

    test_gamma.hpp: add missing L suffix to test value.

--- test/test_gamma.hpp
+++ test/test_gamma.hpp
@@ -331,7 +331,7 @@ void test_spots(T, const char* name)
    {
       BOOST_CHECK_CLOSE(::boost::math::lgamma(ldexp(static_cast<T>(11103367432951928LL), 32)), static_cast<T>(2.7719825960021351251696385101478518546793793286704974382373670822285114741208958e27L), tolerance);
       BOOST_CHECK_CLOSE(::boost::math::lgamma(ldexp(static_cast<T>(11103367432951928LL), 62)), static_cast<T>(4.0411767712186990905102512019058204792570873633363159e36L), tolerance);
-      BOOST_CHECK_CLOSE(::boost::math::lgamma(ldexp(static_cast<T>(11103367432951928LL), 326)), static_cast<T>(3.9754720509185529233002820161357111676582583112671658e116), tolerance);
+      BOOST_CHECK_CLOSE(::boost::math::lgamma(ldexp(static_cast<T>(11103367432951928LL), 326)), static_cast<T>(3.9754720509185529233002820161357111676582583112671658e116L), tolerance);
    }
 }
 
