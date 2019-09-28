c6858bd615c2c4e4b2eaa9a3efbb8f22a69df0f4
NAThompson <nathompson7@protonmail.com>
2019-09-28 14:52:02 -0400

    Anderson-Darling test: Compare to mathematica. [CI SKIP]

--- include/boost/math/distributions/anderson_darling.hpp
+++ include/boost/math/distributions/anderson_darling.hpp
@@ -3,54 +3,133 @@
 
 #include <cmath>
 #include <algorithm>
+#include <boost/math/distributions/normal.hpp>
+#include <boost/math/tools/univariate_statistics.hpp>
+#include <boost/math/quadrature/exp_sinh.hpp>
+#include <boost/math/quadrature/gauss_kronrod.hpp>
+#include <boost/math/special_functions/erf.hpp>
 
-namespace boost { namespace math { namespace distributions {
+namespace boost { namespace math {
 
-template<class RandomAccessContainer, class CDF>
-auto anderson_darling(RandomAccessContainer const & v, CDF& F) {
+template<class RandomAccessContainer>
+auto anderson_darling_normality_test(RandomAccessContainer const & v,
+                                     typename RandomAccessContainer::value_type mu = std::numeric_limits<typename RandomAccessContainer::value_type>::quiet_NaN(),
+                                     typename RandomAccessContainer::value_type sd = std::numeric_limits<typename RandomAccessContainer::value_type>::quiet_NaN())
+{
     using Real = typename RandomAccessContainer::value_type;
     using std::log;
     using std::pow;
-    if (!std::is_sorted(v.begin(), v.end())) {
-        throw std::domain_error("The input vector must be sorted in non-decreasing order v[0] <= v[1] <= ... <= v[n-1].");
+    using std::sqrt;
+    using boost::math::erfc;
+    using boost::math::erf;
+
+    if (std::isnan(mu)) {
+        mu = boost::math::tools::mean(v);
+    }
+    if (std::isnan(sd)) {
+        sd = sqrt(boost::math::tools::sample_variance(v));
     }
 
+    auto normal = boost::math::normal_distribution(mu, sd);
 
-    Real S = 0;
+    // This is where Knuth's literate programming could really come in handy!
+    // I need some LaTeX. The idea is that before any observation, the ecdf is identically zero.
+    // So we need to compute:
+    // \int_{0}^{v_0} \frac{F(x)F'(x)}{1- F(x)} \, \mathrm{d}x, where F(x) := \frac{1}{2}[1+\erf(\frac{x-\mu}{\sigma \sqrt{2}})]
+    // Astonishingly, there is an analytic evaluation to this integral, as you can validate with the following Mathematica command:
+    // Integrate[(1/2 (1 + Erf[(x - mu)/Sqrt[2*sigma^2]])*Exp[-(x - mu)^2/(2*sigma^2)]*1/Sqrt[2*\[Pi]*sigma^2])/(1 - 1/2 (1 + Erf[(x - mu)/Sqrt[2*sigma^2]])),
+    // {x, -Infinity, x0}, Assumptions -> {x0 \[Element] Reals && mu \[Element] Reals && sigma > 0}]
+
+    Real z0 = (v[0] - mu)/sd;
+    Real erfcz0 = erfc(z0/sqrt(Real(2)));
+    Real left_tail = -1 + erfcz0/2 + log(2/erfcz0);
+
+    // For the right tail, the ecdf is identically 1.
+    // Hence we need the integral:
+    // \int_{v_{n-1}}^{\infty} \frac{(1-F(x))F'(x)}{F(x)} \, \mathrm{d}x
+    // This also has an analytic evaluation! It can be found via the following Mathematica command:
+    // Integrate[(E^(-(z^2/2)) *(1 - 1/2 (1 + Erf[z/Sqrt[2]])))/(Sqrt[2 \[Pi]] (1/2 (1 + Erf[z/Sqrt[2]]))),
+    // {z, zn, \[Infinity]}, Assumptions -> {zn \[Element] Reals && mu \[Element] Reals}]
+
+    Real zf = (v[v.size()-1] - mu)/sd;
+    Real erfzf = erf(zf/sqrt(Real(2)));
+    Real right_tail = (-1 + erfzf + 2*log(2/(1+erfzf)))/2;
+
+    // Now we need each integral:
+    // \int_{v_i}^{v_{i+1}} \frac{(i+1/n - F(x))^2F'(x)}{F(x)(1-F(x))}  \, \mathrm{d}x
+    // Again we get an analytical evaluation via the following Mathematica command:
+    // Integrate[((E^(-(z^2/2))/Sqrt[2 \[Pi]])*(k1 - F[z])^2)/(F[z]*(1 - F[z])),
+    // {z, z1, z2}, Assumptions -> {z1 \[Element] Reals && z2 \[Element] Reals &&k1 \[Element] Reals}] // FullSimplify
+
+    Real integrals = 0;
     int64_t N = v.size();
-    for (int64_t i = 0; i < N; ++i) {
-        Real arg = F(v[i]);
-        if (arg < 0 || arg > 1) {
-            throw std::domain_error("CDF does not meet requirements of cumulative density function; range should be [0,1] but evaluated to " + std::to_string(arg));
+    Real s0 = (v[0]-mu)/(sd*sqrt(Real(2)));
+    Real erfcs0 = erfc(s0);
+    for (int64_t i = 0; i < N - 1; ++i) {
+        Real k = (i+1)/Real(N);
+        Real s1 = (v[i+1]-mu)/(sd*sqrt(Real(2)));
+        Real erfcs1 = erfc(s1);
+
+        Real term = -erfcs0 + erfcs1 + 2*log(erfcs0/erfcs1) + 2*k*(k*log(erfcs0*(-2 + erfcs1)/(erfcs1*(-2 + erfcs0))) + 2*log(erfcs1/erfcs0));
+        term /= 2;
+        if (v[i] > v[i+1]) {
+            throw std::domain_error("Input data must be sorted in increasing order v[0] <= v[1] <= . . .  <= v[n-1]");
         }
-        S += (2*i + 1)*log(F(v[i]))+ (2*v.size()+1-2*(i+1))*log(1-F(v[i]));
-        //S += log(pow(arg, 2*i+1)) + log(pow(1-arg, 2*(N-i) -1));
-        /*Real arg = F(v[i])*(1-F(v[v.size()-1-i]));
-        if(arg <= 0) {
-            if (F(v[i]) < 0) {
-                throw std::domain_error("The cumulative distribution function passed to the Anderson-Darling test returned a negative value!");
-            }
-
-            if (F(v[i]) > 1) {
-                throw std::domain_error("The cumulative distribution function passed to the Anderson-Darling test returned a value greater than one!");
-            }
-
-            if (F(v[v.size()-1-i]) < 0) {
-                throw std::domain_error("The cumulative distribution function passed to the Anderson-Darling test returned a negative value!");
-            }
-
-            if (F(v[v.size()-1-i]) > 1) {
-                throw std::domain_error("The cumulative distribution function passed to the Anderson-Darling test returned a value greater than one!");
-            }
-
-            throw std::domain_error("Argument to the logarithm of the Anderson-Darling test is negative; does the CDF satisfy all the properties of a cumulative distribution function?");
-        }
-        // TODO: Check if S *= pow(arg, 2*i+1) and then take a log at the end is faster.
-        S += (2*i+1)*log(arg);*/
+        integrals += term;
+        s0 = s1;
+        erfcs0 = erfcs1;
     }
-    S /= v.size();
-    return -static_cast<Real>(v.size()) - S;
+
+    return v.size()*(left_tail + right_tail + integrals);
 }
 
-}}}
+template<class RandomAccessContainer>
+auto anderson_darling_normality_step(RandomAccessContainer const & v, typename RandomAccessContainer::value_type mu = 0, typename RandomAccessContainer::value_type sd = 1)
+{
+    using Real = typename RandomAccessContainer::value_type;
+    using std::log;
+    using std::pow;
+
+    auto normal = boost::math::normal_distribution(mu, sd);
+
+    auto left_integrand = [&normal](Real x)->Real {
+        Real Fx = boost::math::cdf(normal, x);
+        Real dmu = boost::math::pdf(normal, x);
+        return Fx*dmu/(1-Fx);
+    };
+    auto es = boost::math::quadrature::exp_sinh<Real>();
+    Real left_tail = es.integrate(left_integrand, -std::numeric_limits<Real>::infinity(), v[0]);
+
+    auto right_integrand = [&normal](Real x)->Real {
+        Real Fx = boost::math::cdf(normal, x);
+        Real dmu = boost::math::pdf(normal, x);
+        return (1-Fx)*dmu/Fx;
+    };
+    Real right_tail = es.integrate(right_integrand, v[v.size()-1], std::numeric_limits<Real>::infinity());
+
+
+    auto integrator = boost::math::quadrature::gauss<Real, 7>();
+    Real integrals = 0;
+    int64_t N = v.size();
+    for (int64_t i = 0; i < N - 1; ++i) {
+        auto integrand = [&normal, &i, &N](Real x)->Real {
+            Real Fx = boost::math::cdf(normal, x);
+            Real dmu = boost::math::pdf(normal, x);
+            Real Fn = (i+1)/Real(N);
+            return (Fn - Fx)*(Fn-Fx)*dmu/(Fx*(1-Fx));
+        };
+        auto term = integrator.integrate(integrand, v[i], v[i+1]);
+        if (v[i] > v[i+1]) {
+            throw std::domain_error("Input data must be sorted in increasing order v[0] <= v[1] <= . . .  <= v[n-1]");
+        }
+        integrals += term;
+    }
+
+    //std::cout << "Step integrals = " << integrals << "\n";
+    //std::cout << "Tails = " << left_tail + right_tail << "\n";
+
+    return v.size()*(left_tail + right_tail + integrals);
+}
+
+}}
 #endif
--- test/anderson_darling_test.cpp
+++ test/anderson_darling_test.cpp
@@ -18,9 +18,9 @@
 using boost::multiprecision::float128;
 #endif
 
-using boost::math::distributions::anderson_darling;
+//using boost::math::anderson_darling;
 
-template<class Real>
+/*template<class Real>
 void test_uniform()
 {
     std::random_device rd;  //Will be used to obtain a seed for the random number engine
@@ -46,7 +46,7 @@ void test_uniform()
     };
 
     Real Asq = anderson_darling(v, CDF);
-    std::cout << "Uniform A^2 = " << Asq << "\n";
+    std::cout << "Uniform A^2 = " << Asq << "n";
 }
 
 
@@ -71,7 +71,7 @@ void test_normal()
 
     auto CDF = [&normal](Real x) { return boost::math::cdf(normal, x); };
     Real Asq = anderson_darling(v, CDF);
-    std::cout << "Normal A^2 = " << Asq << "\n";
+    std::cout << "Normal A^2 = " << Asq << "n";
 }
 
 template<class Real>
@@ -95,16 +95,80 @@ void test_cauchy()
 
     auto CDF = [&cauchy](Real x) { return boost::math::cdf(cauchy, x); };
     Real Asq = anderson_darling(v, CDF);
-    std::cout << "Cauchy A^2 = " << Asq << "\n";
-}
+    std::cout << "Cauchy A^2 = " << Asq << "n";
+}*/
 
+/*template<class Real>
+void test_normal()
+{
+    using std::sqrt;
+    std::random_device rd;  //Will be used to obtain a seed for the random number engine
+    std::mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd()
+    Real mu = 0.0;
+    Real sigma =  1.0;
+    std::normal_distribution<Real> dis(mu, sqrt(sigma));
+
+    std::vector<Real> v(100000);
+    for (auto & x : v) {
+        x = dis(gen);
+    }
+
+    std::sort(v.begin(), v.end());
+
+    auto normal = boost::math::normal_distribution<Real>(mu, sqrt(sigma));
+
+    auto CDF = [&normal](Real x) { return boost::math::cdf(normal, x); };
+    Real Asq = anderson_darling(v, CDF);
+    std::cout << "Normal A^2 = " << Asq << "\n";
+}*/
+
+
+void test_ad_normal_agreement_w_mathematica()
+{
+    // To reproduce:
+    // data = RandomVariate[NormalDistribution[0.0, 1], 32];
+    // AndersonDarlingTest[data, NormalDistribution[0.0, 1.0], "TestStatistic"]
+    /*std::vector<double> v{1.72027, -0.311542, -0.988322, -0.348269, -0.89273, -0.64337, -0.216784, -0.13379, 0.0415185, -0.648819, -1.61308, 1.73362,
+                          -0.735983, -0.448442, -0.510363, 0.504508, 0.291554, -0.29157, -2.09951, -0.348593, 0.47801, -0.476067, -1.10085, -0.000997571,
+                          0.0282674, -0.137707, -1.52685, 0.747441, 0.0936708, 1.48821, 0.138821, -0.367293};
+    double expected = 1.691257192985944;
+
+    std::sort(v.begin(), v.end());
+
+    double ADstep = boost::math::anderson_darling_normality_step(v);
+    double ADlinear = boost::math::anderson_darling_normality_linear(v);
+
+    std::cout << "ADStep   = " << ADstep << "n";
+    std::cout << "ADlinear = " << ADlinear << "n";
+    std::cout << "Expected = " << expected << "n";*/
+
+    std::vector<double> v {0.6018671202167422, 0.34935310534248165, -0.2975464874480147, 0.19075371678288966, 0.7871329594028097, 1.153968559452821,
+                           -0.22255484635945819, 0.08509539017654276, -1.3495873906132927, 0.5579622886226612, -0.8781637835839865, 1.144016010880056,
+                           0.3117638373078617, 1.2774825413798292, 0.24847416220730323, -0.5002468827503855, -0.23124277830160153, 0.1880980838556693,
+                           -1.2249409521605903, 2.583585311795, -1.4237305768919715, -0.7613985266437971, -1.089096700141564, -1.2595396780059587,
+                           -0.735507006841229, -0.6957024998691735, 0.24671590370045435, 0.24620128485023313, -2.4445908627621833, 0.1882647961008893,
+                            1.8527432579288248, -0.1908629038857823, -1.6542624501251562, 1.414542907065601, 0.8188282235404601, -0.5040501225935047,
+                           -1.634617885381599, -0.43608956281596023, -1.7347258501594351, 0.8387606547880405, 0.3554282034994122, -0.27872688042721255,
+                           -1.6445047421517192, 1.0262620986278987, -0.14517030192098415, -0.15386932114942647, -0.13364750619758223, 0.27194802295714676,
+                           -0.6396907621289796, 0.3390884293844603, -0.08298748318375798, -0.7165572035514544, -0.2580817977433051, -1.9250094572058072,
+                            -0.07995578394716944, -1.146629910012214, -2.0053073148074665, -1.6745995591714353, 0.2973860474005926, -0.4981763213542575,
+                            0.22190763958244972, 0.699111028057888, -1.1872952966557475, 0.6151420566782272};
+
+    double expected = 1.542329830419774;
+
+    std::sort(v.begin(), v.end());
+
+    double ADtest = boost::math::anderson_darling_normality_test(v, 0.0, 1.0);
+    double ADstep = boost::math::anderson_darling_normality_step(v);
+
+    std::cout << std::setprecision(std::numeric_limits<double>::digits10);
+    std::cout << "ADtest   = " << ADtest << "\n";
+    std::cout << "ADStep   = " << ADstep << "\n";
+    std::cout << "Expected = " << expected << "\n";
+}
 
 int main()
 {
-    test_uniform<double>();
-
-    test_normal<double>();
-
-    test_cauchy<double>();
+    test_ad_normal_agreement_w_mathematica();
     return boost::math::test::report_errors();
 }
