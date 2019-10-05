f9ee90a1f171dec7339bee70d7703a2c61c83d97
Nick <nathompson7@protonmail.com>
2019-10-05 16:54:38 -0400

    T-test: Begin to attempt to back out the p-value [CI SKIP]

--- include/boost/math/statistics/t_test.hpp
+++ include/boost/math/statistics/t_test.hpp
@@ -6,16 +6,25 @@
 #ifndef BOOST_MATH_STATISTICS_T_TEST_HPP
 #define BOOST_MATH_STATISTICS_T_TEST_HPP
 
+#include <boost/math/distributions/students_t.hpp>
 #include <boost/math/statistics/univariate_statistics.hpp>
 
 namespace boost::math::statistics {
 
 template<class RandomAccessContainer>
 auto one_sample_t_test_statistic(RandomAccessContainer const & v, typename RandomAccessContainer::value_type assumed_mean) {
-    using Real = typename RandomAccessContainer::value_type;
     auto [mu, s_sq] = mean_and_sample_variance(v.begin(), v.end());
     return (mu - assumed_mean)/sqrt(s_sq/v.size());
 }
+
+template<class RandomAccessContainer>
+auto one_sample_t_test_pvalue(RandomAccessContainer const & v, typename RandomAccessContainer::value_type assumed_mean) {
+    using Real = typename RandomAccessContainer::value_type;
+    auto statistic = one_sample_t_test_statistic(v, assumed_mean);
+    auto student = boost::math::students_t_distribution<Real>(v.size()-1);
+    return boost::math::cdf(student, statistic);
+}
+
 
 }
 #endif
