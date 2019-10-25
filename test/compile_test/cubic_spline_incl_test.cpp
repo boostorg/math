869949a657374a1a507fd1c8295ee7d4614dda27
NAThompson <nathompson7@protonmail.com>
2019-10-25 08:49:36 -0400

    Cut down on the warnings that I've introduced slowly over time

--- test/compile_test/cubic_spline_incl_test.cpp
+++ test/compile_test/cubic_spline_incl_test.cpp
@@ -3,10 +3,10 @@
 //  Boost Software License, Version 1.0. (See accompanying file
 //  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 //
-// Basic sanity check that header <boost/math/special_functions/gamma.hpp>
+// A sanity check that this file
 // #includes all the files that it needs to.
 //
-#include <boost/math/interpolators/cubic_b_spline.hpp>
+#include <boost/math/interpolators/cardinal_cubic_b_spline.hpp>
 //
 // Note this header includes no other headers, this is
 // important if this test is to be meaningful:
@@ -16,7 +16,7 @@
 void compile_and_link_test()
 {
    double data[] = { 1, 2, 3 };
-   boost::math::cubic_b_spline<double> s(data, 3, 2, 1), s2;
+   boost::math::interpolators::cardinal_cubic_b_spline<double> s(data, 3, 2, 1), s2;
    check_result<double>(s(1.0));
    check_result<double>(s.prime(1.0));
 }
