1d984f4fe739ed40126c7610fcd99657984f0fe0
Nick Thompson <nathompson7@protonmail.com>
2020-01-05 11:53:10 -0500

    Makima spline: Improve performance of evaluation [CI SKIP]

--- include/boost/math/interpolators/makima.hpp
+++ include/boost/math/interpolators/makima.hpp
@@ -40,7 +40,7 @@ public:
         }
 
         s_.resize(x_.size());
-        for (decltype(s_.size()) i = 2; i < s_.size()-1; ++i) {
+        for (decltype(s_.size()) i = 2; i < s_.size()-2; ++i) {
             Real mim2 = (y_[i-1]-y_[i-2])/(x_[i-1]-x_[i-2]);
             Real mim1 = (y_[i]-y_[i-1])/(x_[i]-x_[i-1]);
             Real mi = (y_[i+1]-y_[i])/(x_[i+1]-x_[i]);
@@ -53,21 +53,48 @@ public:
 
     Real operator()(Real x) const {
         if  (x < x_[0] || x > x_[x_.size()-1]) {
-            std::string err = "Requested abscissa outside of allowed range [" 
+            std::string err = "Requested abscissa x = " + std::to_string(x) + ", which is outside of allowed range [" 
                              + std::to_string(x_[0]) + ", "  + std::to_string(x_[x_.size()-1]) + "]";
             throw std::domain_error(err);
         }
+        // We need t := (x-x_k)/(x_{k+1}-x_k) \in [0,1) for this to work.
+        // Sadly this neccessitates this loathesome check:
+        if (x == x_[x_.size()-1]) {
+            return y_[y_.size()-1];
+        }
 
-        auto it = std::lower_bound(x_.begin(), x_.end(), x);
-        auto i = std::distance(x_.begin(), it);
+        auto it = std::upper_bound(x_.begin(), x_.end(), x);
+        auto i = std::distance(x_.begin(), it) -1;
+        Real x0 = *(it-1);
         Real x1 = *it;
-        Real x2 = *(it+1);
-        Real y1 = y_[i];
-        Real y2 = y_[i+1];
-        Real s1 = s_[i];
-        Real s2 = s_[i+1];
-        return std::numeric_limits<Real>::quiet_NaN();
-    }    
+        Real y0 = y_[i];
+        Real y1 = y_[i+1];
+        Real s0 = s_[i];
+        Real s1 = s_[i+1];
+        Real dx = (x1-x0);
+        Real t = (x-x0)/dx;
+
+        // See the section 'Representations' in the page
+        // https://en.wikipedia.org/wiki/Cubic_Hermite_spline
+        // This uses the factorized form:
+        //Real y = y0*(1+2*t)*(1-t)*(1-t) + dx*s0*t*(1-t)*(1-t)
+        //       + y1*t*t*(3-2*t) + dx*s1*t*t*(t-1);
+        // And then factorized further:
+        Real y = (1-t)*(1-t)*(y0*(1+2*t) + s0*dx)
+               + t*t*(y1*(3-2*t) + dx*s1*(t-1));
+        return y;
+    }
+
+    friend std::ostream& operator<<(std::ostream & os, const makima & m)
+    {
+        os << "(x,y,y') = {";
+        for (size_t i = 0; i < m.x_.size() - 1; ++i) {
+            os << "(" << m.x_[i] << ", " << m.y_[i] << ", " << m.s_[i] << "),  ";
+        }
+        auto n = m.x_.size()-1;
+        os << "(" << m.x_[n] << ", " << m.y_[n] << ", " << m.s_[n] << ")}";
+        return os;
+    }
 
 private:
     RandomAccessContainer x_;
