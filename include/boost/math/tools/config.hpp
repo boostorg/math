5cbab46e3a072acd15e957b54e91ff97e75523c6
jzmaddock <john@johnmaddock.co.uk>
2020-03-10 18:32:20 +0000

    Deprecate C++11.

--- include/boost/math/tools/config.hpp
+++ include/boost/math/tools/config.hpp
@@ -26,6 +26,24 @@
 #  include <limits>
 #endif
 
+#if defined(BOOST_NO_CXX11_NOEXCEPT) || defined(BOOST_NO_CXX11_RVALUE_REFERENCES) || defined(BOOST_NO_SFINAE_EXPR)\
+      || defined(BOOST_NO_CXX11_AUTO_DECLARATIONS) || defined(BOOST_NO_CXX11_LAMBDAS) || defined(BOOST_NO_CXX11_UNIFIED_INITIALIZATION_SYNTAX)\
+      || defined(BOOST_NO_CXX11_HDR_TUPLE) || defined(BOOST_NO_CXX11_HDR_INITIALIZER_LIST) || defined(BOOST_NO_CXX11_HDR_CHRONO)\
+      || defined(BOOST_NO_CXX11_THREAD_LOCAL) || defined(BOOST_NO_CXX11_CONSTEXPR) || defined(BOOST_NO_CXX11_NULLPTR)\
+      || defined(BOOST_NO_CXX11_NUMERIC_LIMITS) || defined(BOOST_NO_CXX11_DECLTYPE) || defined(BOOST_NO_CXX11_HDR_ARRAY)\
+      || defined(BOOST_NO_CXX11_ALLOCATOR) || defined(BOOST_NO_CXX11_HDR_ATOMIC) || defined(BOOST_NO_CXX11_NOEXCEPT)\
+      || defined(BOOST_NO_CXX11_EXPLICIT_CONVERSION_OPERATORS)
+//
+// The above list includes everything we use, plus a few we're likely to use soon.
+// As from March 2020, C++03 support is deprecated, and as from March 2021 will be removed,
+// so mark up as such:
+//
+#if defined(_MSC_VER) || defined(__GNUC__)
+#pragma message("CAUTION: One or more C++11 features were found to be unavailable")
+#pragma message("CAUTION: Compiling Boost.Math in pre-C++11 conformance modes is now deprecated and will be removed from March 2021.")
+#endif
+#endif
+
 #include <boost/math/tools/user.hpp>
 
 #if (defined(__CYGWIN__) || defined(__FreeBSD__) || defined(__NetBSD__) \
