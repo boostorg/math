3a34827563a9c282246577db898d2607c87527bb
Nick <nathompson7@protonmail.com>
2020-03-22 13:10:37 -0400

    Copyright statements. [CI SKIP]

--- include/boost/math/interpolators/septic_hermite.hpp
+++ include/boost/math/interpolators/septic_hermite.hpp
@@ -1,3 +1,9 @@
+/*
+ * Copyright Nick Thompson, 2020
+ * Use, modification and distribution are subject to the
+ * Boost Software License, Version 1.0. (See accompanying file
+ * LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+ */
 #ifndef BOOST_MATH_INTERPOLATORS_SEPTIC_HERMITE_HPP
 #define BOOST_MATH_INTERPOLATORS_SEPTIC_HERMITE_HPP
 #include <algorithm>
