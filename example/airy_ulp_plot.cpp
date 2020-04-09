e869337adb2ab123f85100565bd46247c22580e2
Nick <nathompson7@protonmail.com>
2020-04-09 06:41:48 -0400

    Add example of using ULP plot [CI SKIP]

---
+++ example/airy_ulp_plot.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <boost/math/tools/ulp_plot.hpp>
+#include <boost/core/demangle.hpp>
+#include <boost/multiprecision/float128.hpp>
+#include <boost/math/special_functions/airy.hpp>
+
+int main() {
+    using PreciseReal = long double;
+    using CoarseReal = float;
+
+    auto ai_coarse = boost::math::airy_ai<CoarseReal>;
+    auto ai_precise = boost::math::airy_ai<PreciseReal>;
+
+    std::string title = "Airy Ai ULP plot at " + boost::core::demangle(typeid(CoarseReal).name()) + " precision";
+    std::string filename = "airy_ai_" + boost::core::demangle(typeid(CoarseReal).name()) + ".svg";
+    int samples = 5000;
+    int width = 700;
+    int clip = 20;
+    auto plot = boost::math::tools::ulp_plot<decltype(ai_precise), PreciseReal, CoarseReal>(ai_precise, CoarseReal(-3), CoarseReal(3), samples);
+    plot.set_clip(clip);
+    plot.set_width(width);
+    plot.set_title(title);
+    plot.add_fn(ai_coarse);
+    plot.write(filename);
+}
