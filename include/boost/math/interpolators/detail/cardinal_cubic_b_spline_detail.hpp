7722117c2069b9322e0a0c590b1e7a687fec2e79
Nick <nathompson7@protonmail.com>
2020-06-26 09:14:37 -0400

    Fix B-spline compile test failure.

--- include/boost/math/interpolators/detail/cardinal_cubic_b_spline_detail.hpp
+++ include/boost/math/interpolators/detail/cardinal_cubic_b_spline_detail.hpp
@@ -13,6 +13,7 @@
 #include <memory>
 #include <boost/math/constants/constants.hpp>
 #include <boost/math/special_functions/fpclassify.hpp>
+#include <boost/math/special_functions/trunc.hpp>
 
 namespace boost{ namespace math{ namespace interpolators{ namespace detail{
 
