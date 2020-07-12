95c89493e87d14b2652af9f372b70fd8196ab2af
mborland <matt.borland1@gmail.com>
2020-07-11 19:35:32 -0500

    Complete revamp of algorithm. Hide implementation behind detail namespace. [CI SKIP]

--- reporting/performance/prime_sieve_performance.cpp
+++ reporting/performance/prime_sieve_performance.cpp
@@ -15,19 +15,7 @@ void prime_sieve(benchmark::State& state)
     for(auto _ : state)
     {
         std::vector<Z> primes;
-        benchmark::DoNotOptimize(boost::math::prime_sieve(static_cast<Z>(2), upper, std::back_inserter(primes)));
-    }
-    state.SetComplexityN(state.range(0));
-}
-
-template <typename Z>
-void prime_range(benchmark::State& state)
-{
-    Z upper = static_cast<Z>(state.range(0));
-    for(auto _ : state)
-    {
-        std::vector<Z> primes;
-        benchmark::DoNotOptimize(boost::math::prime_range(static_cast<Z>(2), upper, std::back_inserter(primes)));
+        benchmark::DoNotOptimize(boost::math::prime_sieve(upper, std::back_inserter(primes)));
     }
     state.SetComplexityN(state.range(0));
 }
@@ -40,7 +28,7 @@ void prime_sieve_partial_range(benchmark::State& state)
     for(auto _ : state)
     {
         std::vector<Z> primes;
-        benchmark::DoNotOptimize(boost::math::prime_sieve(lower, upper, std::back_inserter(primes)));
+        benchmark::DoNotOptimize(boost::math::prime_range(lower, upper, std::back_inserter(primes)));
     }
     state.SetComplexityN(state.range(0));
 }
@@ -51,16 +39,5 @@ BENCHMARK_TEMPLATE(prime_sieve, uint32_t)->RangeMultiplier(2)->Range(1 << 1, 1 <
 BENCHMARK_TEMPLATE(prime_sieve_partial_range, int32_t)->RangeMultiplier(2)->Range(1 << 1, 1 << 22)->Complexity();
 BENCHMARK_TEMPLATE(prime_sieve_partial_range, int64_t)->RangeMultiplier(2)->Range(1 << 1, 1 << 22)->Complexity();
 BENCHMARK_TEMPLATE(prime_sieve_partial_range, uint32_t)->RangeMultiplier(2)->Range(1 << 1, 1 << 22)->Complexity();
-BENCHMARK_TEMPLATE(prime_range, int32_t)->RangeMultiplier(2)->Range(1 << 1, 1 << 22)->Complexity();
-BENCHMARK_TEMPLATE(prime_range, int64_t)->RangeMultiplier(2)->Range(1 << 1, 1 << 22)->Complexity();
-BENCHMARK_TEMPLATE(prime_range, uint32_t)->RangeMultiplier(2)->Range(1 << 1, 1 << 22)->Complexity();
-
-// Direct comparison of lookup vs sieve using only range of lookup
-BENCHMARK_TEMPLATE(prime_sieve, int32_t)->RangeMultiplier(2)->Range(1 << 1, 1 << 16)->Complexity();
-BENCHMARK_TEMPLATE(prime_range, int32_t)->RangeMultiplier(2)->Range(1 << 1, 1 << 16)->Complexity();
-BENCHMARK_TEMPLATE(prime_sieve, int64_t)->RangeMultiplier(2)->Range(1 << 1, 1 << 16)->Complexity();
-BENCHMARK_TEMPLATE(prime_range, int64_t)->RangeMultiplier(2)->Range(1 << 1, 1 << 16)->Complexity();
-BENCHMARK_TEMPLATE(prime_sieve, uint32_t)->RangeMultiplier(2)->Range(1 << 1, 1 << 16)->Complexity();
-BENCHMARK_TEMPLATE(prime_range, uint32_t)->RangeMultiplier(2)->Range(1 << 1, 1 << 16)->Complexity();
 
 BENCHMARK_MAIN();
