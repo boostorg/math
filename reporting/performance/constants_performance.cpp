1a0f191926a4513cf46b68c340a433d0836132ce
Nick Thompson <nathompson7@protonmail.com>
2020-07-27 22:47:30 -0400

    Try 3 different methods to compute Ei(1) [CI SKIP]

--- reporting/performance/constants_performance.cpp
+++ reporting/performance/constants_performance.cpp
@@ -9,6 +9,112 @@
 
 using namespace boost::math::constants;
 using boost::multiprecision::mpfr_float;
+#include <boost/math/special_functions/expint.hpp>
+
+using namespace boost::math::constants;
+using boost::multiprecision::mpfr_float;
+// Ei(1) = 1.8951178163559367554665209343316342690170605817327075916462284318825138345338041535489007101261\
+             3895697181109531794465374258814916416306468808818668253882866963233854509522755525848139221216\
+             6459936359948543306285455761625228166868118802856637846665686888646424297019090790472890309099\
+             3380190917469997918302494807585852088867837050413737878407416460258387628362162273159751506105\
+             1925474727680703251963132680675670740814722824224269686684164320795874672671226770927559887547\
+             77656819174952725603954608115985285493707
+void Ei1Boost(benchmark::State& state)
+{
+    
+    using boost::math::expint;
+    mpfr_float::default_precision(state.range(0));
+    mpfr_float x{1};
+    for (auto _ : state)
+    {
+        benchmark::DoNotOptimize(expint(x));
+    }
+    //std::cout <<std::setprecision(state.range(0)) << "Expint(1) = " << expint(x) << "\n";
+    state.SetComplexityN(state.range(0));
+}
+
+BENCHMARK(Ei1Boost)->RangeMultiplier(2)->Range(32, 65536)->Complexity()->Unit(benchmark::kMicrosecond);
+
+template<typename Real>
+Real Ei1() {
+    Real sum = euler<Real>();
+    Real k = 1;
+    Real fact = 1;
+    Real term = 1/fact;
+    while (term > std::numeric_limits<Real>::epsilon()) {
+        sum += term;
+        ++k;
+        fact *= k;
+        term = 1/(k*fact);
+    }
+    return sum;
+}
+
+void Ei1Nick(benchmark::State& state)
+{   
+    using boost::math::expint;
+    mpfr_float::default_precision(state.range(0));
+    mpfr_float x{1};
+    for (auto _ : state)
+    {
+        benchmark::DoNotOptimize(Ei1<mpfr_float>());
+    }
+    //std::cout << std::setprecision(state.range(0)) << "Expint(1) = " << Ei1<mpfr_float>() << "\n";
+    state.SetComplexityN(state.range(0));
+}
+
+BENCHMARK(Ei1Nick)->RangeMultiplier(2)->Range(32, 65536)->Complexity()->Unit(benchmark::kMicrosecond);
+
+template<typename Real>
+Real Ei1Ramanujan()
+{
+    using std::pow;
+    using std::abs;
+    Real term = std::numeric_limits<Real>::max();
+    size_t n = 1;
+    Real ksum = 0;
+    Real fact = 1;
+    Real sum = 0;
+    while (abs(term) > std::numeric_limits<Real>::epsilon()) {
+        fact *= n;
+        Real denom = fact;
+        // Use shifts if possible:
+        /*if (n < 64)
+        {
+            denom *= (1uLL << n - 1);
+        }
+        else
+        {
+            denom *= pow(Real(2), n - 1);
+        }
+        Real scale = -Real(1)/denom;*/
+        Real scale = -Real(1)/(fact*(1uLL << n - 1));
+        if (n & 1)
+        {
+            ksum += 1/Real(n);
+            scale = -scale;
+        }
+        term = scale*ksum;
+        sum += term;
+        ++n;
+    }
+    return euler<Real>() + root_e<Real>()*sum;
+}
+
+void Ei1Ramanujan(benchmark::State& state)
+{
+    mpfr_float::default_precision(state.range(0));
+    for (auto _ : state)
+    {
+        benchmark::DoNotOptimize(Ei1Ramanujan<mpfr_float>());
+    }
+    //std::cout << std::setprecision(state.range(0)) << "Expint(1) = " << Ei1Ramanujan<mpfr_float>() << "\n";
+    state.SetComplexityN(state.range(0));
+}
+
+BENCHMARK(Ei1Ramanujan)->RangeMultiplier(2)->Range(32, 65536)->Complexity()->Unit(benchmark::kMicrosecond);
+
+
 
 void LaplaceLimit(benchmark::State& state)
 {
