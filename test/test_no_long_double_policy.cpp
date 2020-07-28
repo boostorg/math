801eff25049eb698c16c15650815755ee752e287
jzmaddock <john@johnmaddock.co.uk>
2020-07-28 08:39:16 +0100

    Correct test case for C++03, add missing copyright.

--- test/test_no_long_double_policy.cpp
+++ test/test_no_long_double_policy.cpp
@@ -1,6 +1,7 @@
-
-#include <type_traits>
-#include <cmath>
+//  Copyright John Maddock 2020.
+//  Use, modification and distribution are subject to the
+//  Boost Software License, Version 1.0. (See accompanying file
+//  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
 #define BOOST_MATH_ASSERT_UNDEFINED_POLICY false
 #include <boost/math/tools/config.hpp>
