de347f169b1187b1933d32260a2b271e76a5a9bd
Evan Miller <emmiller@gmail.com>
2020-08-19 12:42:37 -0400

    Fix copyright notices [CI SKIP]

--- test/compile_test/dist_kolmogorov_smirnov_incl_test.cpp
+++ test/compile_test/dist_kolmogorov_smirnov_incl_test.cpp
@@ -1,4 +1,4 @@
-//  Copyright John Maddock 2006.
+//  Copyright Evan Miller 2020
 //  Use, modification and distribution are subject to the
 //  Boost Software License, Version 1.0. (See accompanying file
 //  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
