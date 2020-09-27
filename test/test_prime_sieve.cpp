702efa69f43a0a160e3761a6435bc0f43ab18957
Matt Borland <matt.borland1@gmail.com>
2020-09-26 22:54:36 -0500

    Added interval sieve for output iterators Currently untested

--- test/test_prime_sieve.cpp
+++ test/test_prime_sieve.cpp
@@ -6,6 +6,7 @@
 // or copy at http://www.boost.org/LICENSE_1_0.txt)
 
 #include <boost/math/special_functions/prime_sieve.hpp>
+#include <boost/math/special_functions/prime_approximation.hpp>
 #include <boost/core/lightweight_test.hpp>
 #include <boost/multiprecision/cpp_int.hpp>
 #include <boost/multiprecision/gmp.hpp>
