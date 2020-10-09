26120e25ddedd3cd6395c79427f536030567ac1f
Matt Borland <matt@mattborland.com>
2020-10-09 16:28:52 -0500

    Refactored sequential first four moments impls [CI SKIP]

--- include/boost/math/statistics/detail/single_pass.hpp
+++ include/boost/math/statistics/detail/single_pass.hpp
@@ -13,6 +13,7 @@
 #include <thread>
 #include <type_traits>
 #include <future>
+#include <cmath>
 
 namespace boost::math::statistics::detail
 {
@@ -127,6 +128,127 @@ ReturnType parallel_variance_impl(ForwardIterator first, ForwardIterator last)
     const auto M2_ab = M2_a + M2_b + delta * delta * (range_a * range_b / n_ab);
 
     return std::make_tuple(mean_ab, M2_ab, n_ab);
+}
+
+// https://en.wikipedia.org/wiki/Algorithms_for_calculating_variance#Higher-order_statistics
+// Use first four moments of disjoint sets for policies with skewness and kurtosis
+template<typename ForwardIterator>
+auto first_four_moments_integral_impl(ForwardIterator first, ForwardIterator last)
+{
+    double M1 = *first;
+    double M2 = 0;
+    double M3 = 0;
+    double M4 = 0;
+    double n = 2;
+    for (auto it = std::next(first); it != last; ++it)
+    {
+        double delta21 = *it - M1;
+        double tmp = delta21/n;
+        M4 = M4 + tmp*(tmp*tmp*delta21*((n-1)*(n*n-3*n+3)) + 6*tmp*M2 - 4*M3);
+        M3 = M3 + tmp*((n-1)*(n-2)*delta21*tmp - 3*M2);
+        M2 = M2 + tmp*(n-1)*delta21;
+        M1 = M1 + tmp;
+        n += 1;
+    }
+
+    return std::make_tuple(M1, M2, M3, M4, n-1);
+}
+
+template<typename ForwardIterator>
+auto first_four_moments_real_impl(ForwardIterator first, ForwardIterator last)
+{
+    using Real = typename std::iterator_traits<ForwardIterator>::value_type;
+
+    Real M1 = *first;
+    Real M2 = 0;
+    Real M3 = 0;
+    Real M4 = 0;
+    Real n = 2;
+    for (auto it = std::next(first); it != last; ++it)
+    {
+        Real delta21 = *it - M1;
+        Real tmp = delta21/n;
+        M4 = M4 + tmp*(tmp*tmp*delta21*((n-1)*(n*n-3*n+3)) + 6*tmp*M2 - 4*M3);
+        M3 = M3 + tmp*((n-1)*(n-2)*delta21*tmp - 3*M2);
+        M2 = M2 + tmp*(n-1)*delta21;
+        M1 = M1 + tmp;
+        n += 1;
+    }
+
+    return std::make_tuple(M1, M2, M3, M4, n-1);
+}
+
+template<typename ReturnType, typename ForwardIterator>
+ReturnType parallel_first_four_moments_impl(ForwardIterator first, ForwardIterator last)
+{
+    using Real = typename std::iterator_traits<ForwardIterator>::value_type;
+    static unsigned num_threads {std::thread::hardware_concurrency()};
+
+    const auto elements {std::distance(first, last)};
+    const auto range_a {std::floor(elements / 2)};
+    const auto range_b {elements - range_a};
+
+    thread_counter.fetch_add(2);
+    auto future_a {std::async(std::launch::async, [first, range_a, &thread_counter, num_threads]() -> ReturnType
+    {
+        if constexpr (std::is_integral_v<Real>)
+        {
+            if(thread_counter + 2 <= num_threads && range_a > 10)
+                return parallel_first_four_moments_impl<ReturnType>(first, std::next(first, range_a));
+            else
+                return first_four_moments_integral_impl(first, std::next(first, range_a));
+        }
+        else
+        {
+            if(thread_counter + 2 <= num_threads && range_a > 10)
+                return parallel_first_four_moments_impl<ReturnType>(first, std::next(first, range_a));
+            else
+                return first_four_moments_real_impl(first, std::next(first, range_a));
+        }
+    })};
+    auto future_b {std::async(std::launch::async, [first, last, range_a, &thread_counter, num_threads]() -> ReturnType
+    {
+        if constexpr (std::is_integral_v<Real>)
+        {
+            if(thread_counter + 2 <= num_threads && range_a > 10)
+                return parallel_first_four_moments_impl<ReturnType>(std::next(first, range_a), last);
+            else
+                return first_four_moments_integral_impl(std::next(first, range_a), last);
+        }
+        else
+        {
+            if(thread_counter + 2 <= num_threads && range_a > 10)
+                return parallel_first_four_moments_impl<ReturnType>(std::next(first, range_a), last);
+            else
+                return first_four_moments_real_impl(std::next(first, range_a), last);
+        }
+    })};
+
+    const auto results_a {future_a.get()};
+    const auto results_b {future_b.get()};
+
+    const auto M1_a = std::get<0>(results_a);
+    const auto M2_a = std::get<0>(results_a);
+    const auto M3_a = std::get<0>(results_a);
+    const auto M4_a = std::get<0>(results_a);
+
+    const auto M1_b = std::get<0>(results_b);
+    const auto M2_b = std::get<0>(results_b);
+    const auto M3_b = std::get<0>(results_b);
+    const auto M4_b = std::get<0>(results_b);
+
+    const auto n_ab = elements;
+    const auto delta = M1_b - M1_a;
+    
+    const auto M1_ab = (range_a * M1_a + range_b * M1_b) / n_ab;
+    const auto M2_ab = M2_a + M2_b + delta * delta * (range_a * range_b / n_ab);
+    const auto M3_ab = M3_a + M3_ab + std::pow(delta, 3) * range_a * range_b * (range_a - range_b) / (n_ab * n_ab) 
+                       + 3 * delta * (range_a * M2_b - range_b * M2_a) / n_ab;
+    const auto M4_ab = M4_a + M4_b + std::pow(delta, 4) * range_a * range_b * (range_a * range_a - range_a * range_b + range_b * range_b) / std::pow(n_ab, 3) 
+                       + 6 * delta * delta * (range_a * range_a * M2_b + range_b * range_b * M2_a) / (n_ab * n_ab) 
+                       + 4 * delta * (range_a * M3_b - range_b * M3_a) / n_ab;
+
+    return std::make_tuple(M1_ab, M2_ab, M3_ab, M4_ab, n_ab-1);
 }
 }
 
--- include/boost/math/statistics/univariate_statistics.hpp
+++ include/boost/math/statistics/univariate_statistics.hpp
@@ -247,6 +247,7 @@ inline auto mean_and_sample_variance(Container const & v)
 // TODO(mborland): find/try decompistion algo with impl split by type similar to variance
 //                 Should satisfy this, first four momenets, kurtosis, and excess kurtosis
 //
+// TODO(mborland): Reorder skewness below first four moments and call it like kurtosis does
 // Follows equation 1.5 of:
 // https://prod.sandia.gov/techlib-noauth/access-control.cgi/2008/086212.pdf
 template<class ForwardIterator>
@@ -315,59 +316,29 @@ inline auto skewness(Container const & v)
     return skewness(v.cbegin(), v.cend());
 }
 
-// Follows equation 1.5/1.6 of:
-// https://prod.sandia.gov/techlib-noauth/access-control.cgi/2008/086212.pdf
 template<class ForwardIterator>
-auto first_four_moments(ForwardIterator first, ForwardIterator last)
+inline auto first_four_moments(ForwardIterator first, ForwardIterator last)
 {
     using Real = typename std::iterator_traits<ForwardIterator>::value_type;
-    BOOST_ASSERT_MSG(first != last, "At least one sample is required to compute the first four moments.");
-    if constexpr (std::is_integral<Real>::value)
-    {
-        double M1 = *first;
-        double M2 = 0;
-        double M3 = 0;
-        double M4 = 0;
-        double n = 2;
-        for (auto it = std::next(first); it != last; ++it)
-        {
-            double delta21 = *it - M1;
-            double tmp = delta21/n;
-            M4 = M4 + tmp*(tmp*tmp*delta21*((n-1)*(n*n-3*n+3)) + 6*tmp*M2 - 4*M3);
-            M3 = M3 + tmp*((n-1)*(n-2)*delta21*tmp - 3*M2);
-            M2 = M2 + tmp*(n-1)*delta21;
-            M1 = M1 + tmp;
-            n += 1;
-        }
 
-        return std::make_tuple(M1, M2/(n-1), M3/(n-1), M4/(n-1));
+    if constexpr (std::is_integral_v<Real>)
+    {
+        const auto results = detail::first_four_moments_integral_impl(first, last);
+        return std::make_tuple(std::get<0>(results), std::get<1>(results) / std::get<4>(results), std::get<2>(results) / std::get<4>(results), 
+                               std::get<3>(results) / std::get<4>(results));
     }
     else
     {
-        Real M1 = *first;
-        Real M2 = 0;
-        Real M3 = 0;
-        Real M4 = 0;
-        Real n = 2;
-        for (auto it = std::next(first); it != last; ++it)
-        {
-            Real delta21 = *it - M1;
-            Real tmp = delta21/n;
-            M4 = M4 + tmp*(tmp*tmp*delta21*((n-1)*(n*n-3*n+3)) + 6*tmp*M2 - 4*M3);
-            M3 = M3 + tmp*((n-1)*(n-2)*delta21*tmp - 3*M2);
-            M2 = M2 + tmp*(n-1)*delta21;
-            M1 = M1 + tmp;
-            n += 1;
-        }
-
-        return std::make_tuple(M1, M2/(n-1), M3/(n-1), M4/(n-1));
+        const auto results = detail::first_four_moments_real_impl(first, last);
+        return std::make_tuple(std::get<0>(results), std::get<1>(results) / std::get<4>(results), std::get<2>(results) / std::get<4>(results), 
+                               std::get<3>(results) / std::get<4>(results));
     }
 }
 
 template<class Container>
 inline auto first_four_moments(Container const & v)
 {
-    return first_four_moments(v.cbegin(), v.cend());
+    return first_four_moments(std::cbegin(v), std::cend(v));
 }
 
 
