eb9f9109447056027ac37c55180082c8e32cd587
Matt Borland <matt@mattborland.com>
2020-11-29 09:53:44 +0300

    Seperate approximation for multiprecision types [CI SKIP]

--- include/boost/math/special_functions/prime_approximation.hpp
+++ include/boost/math/special_functions/prime_approximation.hpp
@@ -6,6 +6,7 @@
 // or copy at http://www.boost.org/LICENSE_1_0.txt)
 
 #include <vector>
+#include <type_traits>
 #include <cmath>
 
 #ifndef BOOST_MATH_SPECIAL_FUNCTIONS_PRIME_APPROXIMATION_HPP
@@ -13,15 +14,29 @@
 
 namespace boost::math
 {
-template<typename Integer>
+// Constexpr does not work with multiprecision types
+template<typename Integer, std::enable_if_t<std::is_integral_v<Integer>, bool> = true>
 constexpr Integer prime_approximation(const Integer upper_bound) noexcept
 {
     constexpr auto c = 30 * ::log(113) / 113; // Magic numbers from wikipedia
     return static_cast<Integer>(::floor(c * static_cast<double>(upper_bound) / ::log(static_cast<double>(upper_bound))));
 }
 
-template<typename Integer>
+template<typename Integer, std::enable_if_t<std::is_integral_v<Integer>, bool> = true>
 constexpr Integer prime_approximation(const Integer lower_bound, const Integer upper_bound) noexcept
+{
+    return prime_approximation(upper_bound) - prime_approximation(lower_bound);
+}
+
+template<typename Integer, std::enable_if_t<!std::is_integral_v<Integer>, bool> = true>
+Integer prime_approximation(const Integer upper_bound) noexcept
+{
+    const auto c = 30 * std::log(113) / 113;
+    return static_cast<Integer>(std::floor(c * static_cast<long double>(upper_bound) / std::log(static_cast<long double>(upper_bound))));
+}
+
+template<typename Integer, std::enable_if_t<!std::is_integral_v<Integer>, bool> = true>
+Integer prime_approximation(const Integer lower_bound, const Integer upper_bound) noexcept
 {
     return prime_approximation(upper_bound) - prime_approximation(lower_bound);
 }
