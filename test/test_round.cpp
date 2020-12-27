a13451838add55420211ea92ad50af8877045052
Matt Borland <matt@mattborland.com>
2020-12-27 20:10:16 +0300

    Add test case of long long max + 1

--- test/test_round.cpp
+++ test/test_round.cpp
@@ -179,7 +179,7 @@ void test_round_number(T arg)
       k = ltrunc(static_cast<T>((std::numeric_limits<long>::min)()));
       check_trunc_result(static_cast<T>((std::numeric_limits<long>::min)()), T(k));
    }
-
+   
 #ifdef BOOST_HAS_LONG_LONG
    if(abs(r) < static_cast<T>(std::numeric_limits<boost::long_long_type>::max()))
    {
@@ -464,4 +464,10 @@ BOOST_AUTO_TEST_CASE( test_main )
 
    BOOST_CHECK_EQUAL(boost::math::round(9007199254740993.0), 9007199254740993.0);
    test_round_number(9007199254740993.0);
+
+   #ifdef BOOST_HAS_LONG_LONG
+   // std::numeric_limits<long long>::max() + 1
+   BOOST_CHECK_EQUAL(boost::math::round(9223372036854775808.0), 9223372036854775808.0);
+   test_round_number(9223372036854775808.0);
+   #endif
 }
