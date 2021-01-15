1d4979024f0d2b33c82eab81fc8072ceecbdc7ec
Nick Thompson <nathompson7@protonmail.com>
2021-01-15 14:02:07 -0500

    Works now. [CI SKIP]

--- include/boost/math/interpolators/pqhip.hpp
+++ include/boost/math/interpolators/pqhip.hpp
@@ -1,8 +1,22 @@
-// Copyright Nick Thompson, 2022
+// Copyright Nick Thompson, 2021
 // Use, modification and distribution are subject to the
 // Boost Software License, Version 1.0.
 // (See accompanying file LICENSE_1_0.txt
 // or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+// This is a scalar-valued PQUIP interpolator as discussed in
+// Corless, A Graduate Introduction to Numerical Methods, problem 8.40.
+// It's quite similar to PCHIP, but the PCHIP procedure is applied to the derivatives,
+// not to the function values.
+// An implementation in Matlab is found here:
+// https://web.archive.org/web/20210115165752/http://www.nfillion.com/coderepository/Graduate_Introduction_to_Numerical_Methods/pqhip.m
+
+// The generalized barycentric weights of problem 8.40 of the reference are easily found in Mathematica:
+// Apart[1/(z^3*(z - 1)^3)], giving:
+// 1/(z-1)^3 - 3/(z-1)^2 + 6/(z-1) - 1/z^3 - 3/z^2 - 6/z,
+// which gives
+// beta00 = -6, beta01 = -3, beta01 = -1
+// beta10 = 6, beta11 = -3, beta11 = 1
 
 #ifndef BOOST_MATH_INTERPOLATORS_PQHIP_HPP
 #define BOOST_MATH_INTERPOLATORS_PQHIP_HPP
@@ -14,60 +28,55 @@ namespace math {
 namespace interpolators {
 
 template<class RandomAccessContainer>
-class pchip {
+class pqhip {
 public:
     using Real = typename RandomAccessContainer::value_type;
 
-    pqhip(RandomAccessContainer && times, RandomAccessContainer && y, RandomAccessContainer && dydt
+    pqhip(RandomAccessContainer && times, RandomAccessContainer && y, RandomAccessContainer && dydt,
           Real left_endpoint_second_derivative = std::numeric_limits<Real>::quiet_NaN(),
           Real right_endpoint_second_derivative = std::numeric_limits<Real>::quiet_NaN())
     {
         using std::isnan;
-        if (x.size() < 4)
+        // Validation of inputs is done in the detail class.
+        RandomAccessContainer d2ydt2(times.size(), std::numeric_limits<Real>::quiet_NaN());
+        if (isnan(left_endpoint_second_derivative))
         {
-            throw std::domain_error("Must be at least four data points.");
-        }
-        RandomAccessContainer s(x.size(), std::numeric_limits<Real>::quiet_NaN());
-        if (isnan(left_endpoint_derivative))
-        {
-            // O(h) finite difference derivative:
-            // This, I believe, is the only derivative guaranteed to be monotonic:
-            s[0] = (y[1]-y[0])/(x[1]-x[0]);
+          //
         }
         else
         {
-            s[0] = left_endpoint_derivative;
+            d2ydt2[0] = left_endpoint_second_derivative;
         }
 
-        for (decltype(s.size()) k = 1; k < s.size()-1; ++k) {
-            Real hkm1 = x[k] - x[k-1];
-            Real dkm1 = (y[k] - y[k-1])/hkm1;
+        for (decltype(d2ydt2.size()) k = 1; k < d2ydt2.size()-1; ++k) {
+            Real hkm1 = times[k] - times[k-1];
+            Real dkm1 = (dydt[k] - dydt[k-1])/hkm1;
 
-            Real hk = x[k+1] - x[k];
-            Real dk = (y[k+1] - y[k])/hk;
+            Real hk = times[k+1] - times[k];
+            Real dk = (dydt[k+1] - dydt[k])/hk;
             Real w1 = 2*hk + hkm1;
             Real w2 = hk + 2*hkm1;
             if ( (dk > 0 && dkm1 < 0) || (dk < 0 && dkm1 > 0) || dk == 0 || dkm1 == 0)
             {
-                s[k] = 0;
+                d2ydt2[k] = 0;
             }
             else
             {
-                s[k] = (w1+w2)/(w1/dkm1 + w2/dk);
+                d2ydt2[k] = (w1+w2)/(w1/dkm1 + w2/dk);
             }
 
         }
         // Quadratic extrapolation at the other end:
-        auto n = s.size();
-        if (isnan(right_endpoint_derivative))
+        auto n = d2ydt2.size();
+        if (isnan(right_endpoint_second_derivative))
         {
-            s[n-1] = (y[n-1]-y[n-2])/(x[n-1] - x[n-2]);
+            //
         }
         else
         {
-            s[n-1] = right_endpoint_derivative;
+            d2ydt2[n-1] = right_endpoint_second_derivative;
         }
-        impl_ = std::make_shared<detail::cubic_hermite_detail<RandomAccessContainer>>(std::move(x), std::move(y), std::move(s));
+        impl_ = std::make_shared<detail::quintic_hermite_detail<RandomAccessContainer>>(std::move(times), std::move(y), std::move(dydt), std::move(d2ydt2));
     }
 
     Real operator()(Real x) const {
@@ -78,7 +87,7 @@ public:
         return impl_->prime(x);
     }
 
-    friend std::ostream& operator<<(std::ostream & os, const pchip & m)
+    friend std::ostream& operator<<(std::ostream & os, const pqhip & m)
     {
         os << *m.impl_;
         return os;
@@ -115,7 +124,7 @@ public:
     }
 
 private:
-    std::shared_ptr<detail::cubic_hermite_detail<RandomAccessContainer>> impl_;
+    std::shared_ptr<detail::quintic_hermite_detail<RandomAccessContainer>> impl_;
 };
 
 }
