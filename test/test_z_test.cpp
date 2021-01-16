941b7f52b26d7613240f3aafa32f52ac64a513d4
Matt Borland <matt@mattborland.com>
2021-01-16 19:31:26 +0300

    Fixes to docs and tests [CI SKIP]

--- test/test_z_test.cpp
+++ test/test_z_test.cpp
@@ -7,6 +7,7 @@
 #include <boost/math/statistics/z_test.hpp>
 #include <boost/math/statistics/univariate_statistics.hpp>
 #include <boost/multiprecision/cpp_bin_float.hpp>
+#include <limits>
 #include <vector>
 #include <random>
 
@@ -17,13 +18,13 @@ void test_one_sample_z()
 {
     auto [computed_statistic, computed_pvalue] = boost::math::statistics::one_sample_z_test(Real(10), Real(2), Real(100), Real(10));
     CHECK_ULP_CLOSE(Real(0), computed_statistic, 5);
-    CHECK_MOLLIFIED_CLOSE(Real(0), computed_pvalue, 1e-22);
+    CHECK_MOLLIFIED_CLOSE(Real(0), computed_pvalue, 5*std::numeric_limits<Real>::epsilon());
 
     auto [computed_statistic_2, computed_pvalue_2] = boost::math::statistics::one_sample_z_test(Real(10), Real(2), Real(100), Real(5));
     CHECK_ULP_CLOSE(Real(25), computed_statistic_2, 5);
 
     auto [computed_statistic_3, computed_pvalue_3] = boost::math::statistics::one_sample_z_test(Real(1.0/2.0), Real(10), Real(100), Real(1.0/3.0));
-    CHECK_ULP_CLOSE(Real(1.0/6.0), computed_statistic_3, 5);
+    CHECK_ULP_CLOSE(Real(1)/6, computed_statistic_3, 5);
 }
 
 template<typename Z>
@@ -31,7 +32,7 @@ void test_integer_one_sample_z()
 {
     auto [computed_statistic, computed_pvalue] = boost::math::statistics::one_sample_z_test(Z(10), Z(2), Z(100), Z(10));
     CHECK_ULP_CLOSE(0, computed_statistic, 5);
-    CHECK_MOLLIFIED_CLOSE(0, computed_pvalue, 1e-22);
+    CHECK_MOLLIFIED_CLOSE(0, computed_pvalue, 5*std::numeric_limits<double>::epsilon());
 
     auto [computed_statistic_2, computed_pvalue_2] = boost::math::statistics::one_sample_z_test(Z(10), Z(2), Z(100), Z(5));
     CHECK_ULP_CLOSE(25, computed_statistic_2, 5);
@@ -45,7 +46,7 @@ void test_two_sample_z()
 
     auto [computed_statistic, computed_pvalue] = boost::math::statistics::two_sample_z_test(set_2, set_1);
     CHECK_ULP_CLOSE(Real(1), computed_statistic, 5);
-    CHECK_MOLLIFIED_CLOSE(Real(0), computed_pvalue, 1e-22);
+    CHECK_MOLLIFIED_CLOSE(Real(0), computed_pvalue, 5*std::numeric_limits<Real>::epsilon());
 }
 
 template<typename Z>
@@ -56,7 +57,7 @@ void test_integer_two_sample_z()
 
     auto [computed_statistic, computed_pvalue] = boost::math::statistics::two_sample_z_test(set_2, set_1);
     CHECK_ULP_CLOSE(1, computed_statistic, 5);
-    CHECK_MOLLIFIED_CLOSE(0, computed_pvalue, 1e-22);
+    CHECK_MOLLIFIED_CLOSE(0, computed_pvalue, 5*std::numeric_limits<double>::epsilon());
 }
 
 int main()
