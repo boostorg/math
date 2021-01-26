a45ecea6cd436b013dd905e10660c94a34fbdf14
Matt Borland <matt@mattborland.com>
2021-01-26 19:31:37 +0300

    Fix MSVC MINMAX CI Failures

--- test/test_carlson.cpp
+++ test/test_carlson.cpp
@@ -5,6 +5,7 @@
 //  Boost Software License, Version 1.0. (See accompanying file
 //  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
+#define NOMINMAX
 #include <pch_light.hpp>
 #include "test_carlson.hpp"
 
