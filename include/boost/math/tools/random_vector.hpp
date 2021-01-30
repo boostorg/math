91ae2bfc771b538a4d4a9b2c98752c77547e75f7
Matt Borland <matt@mattborland.com>
2021-01-30 19:09:12 +0300

    Bivariate Stats Policies (#503)
    * Add parallel impl and change seq impl [CI SKIP]
    
    * Validate seq impl [CI SKIP]
    
    * Remove old impl
    
    * Add user interfaces [CI SKIP]
    
    * Floating point covariance validated [CI SKIP]
    
    * Integer covariance validated [CI SKIP]
    
    * Change correlation_coeff impl interface [CI SKIP]
    
    * Cleanup [CI SKIP]
    
    * correlation passes all parameters for par impl
    [CI SKIP]
    
    * Finish framework [CI SKIP]
    
    * Add correlation coefficient test cases
    
    * Add benchmark and make small changes
    [CI SKIP]
    
    * Update docs

---
+++ include/boost/math/tools/random_vector.hpp
@@ -0,0 +1,124 @@
+//  (C) Copyright Nick Thompson 2018.
+//  (C) Copyright Matt Borland 2021.
+//  Use, modification and distribution are subject to the
+//  Boost Software License, Version 1.0. (See accompanying file
+//  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#include <vector>
+#include <random>
+#include <type_traits>
+#include <cstddef>
+#include <boost/multiprecision/cpp_bin_float.hpp>
+#include <boost/multiprecision/cpp_complex.hpp>
+
+using boost::multiprecision::cpp_bin_float_50;
+using boost::multiprecision::cpp_complex_50;
+
+namespace boost { namespace math {
+
+// To stress test, set global_seed = 0, global_size = huge.
+static const std::size_t global_seed = 0;
+static const std::size_t global_size = 128;
+
+template<typename T, typename std::enable_if<std::is_floating_point<T>::value, bool>::type = true>
+std::vector<T> generate_random_vector(std::size_t size, std::size_t seed)
+{
+    if (seed == 0)
+    {
+        std::random_device rd;
+        seed = rd();
+    }
+    std::vector<T> v(size);
+
+    std::mt19937 gen(seed);
+
+    std::normal_distribution<T> dis(0, 1);
+    for(std::size_t i = 0; i < v.size(); ++i)
+    {
+        v[i] = dis(gen);
+    }
+    return v;
+}
+
+template<typename T, typename std::enable_if<std::is_integral<T>::value, bool>::type = true>
+std::vector<T> generate_random_vector(std::size_t size, std::size_t seed)
+{
+    if (seed == 0)
+    {
+        std::random_device rd;
+        seed = rd();
+    }
+    std::vector<T> v(size);
+
+    std::mt19937 gen(seed);
+
+    // Rescaling by larger than 2 is UB!
+    std::uniform_int_distribution<T> dis(std::numeric_limits<T>::lowest()/2, (std::numeric_limits<T>::max)()/2);
+    for (std::size_t i = 0; i < v.size(); ++i)
+    {
+        v[i] = dis(gen);
+    }
+    return v;
+}
+
+template<typename T, typename std::enable_if<boost::is_complex<T>::value, bool>::type = true>
+std::vector<T> generate_random_vector(std::size_t size, std::size_t seed)
+{
+    if (seed == 0)
+    {
+        std::random_device rd;
+        seed = rd();
+    }
+    std::vector<T> v(size);
+
+    std::mt19937 gen(seed);
+    
+    std::normal_distribution<typename T::value_type> dis(0, 1);
+    for (std::size_t i = 0; i < v.size(); ++i)
+    {
+        v[i] = {dis(gen), dis(gen)};
+    }
+    return v;  
+}
+
+template<typename T, typename std::enable_if<std::is_same<T, cpp_complex_50>::value , bool>::type = true>
+std::vector<T> generate_random_vector(std::size_t size, std::size_t seed)
+{
+    if (seed == 0)
+    {
+        std::random_device rd;
+        seed = rd();
+    }
+    std::vector<T> v(size);
+
+    std::mt19937 gen(seed);
+    
+    std::normal_distribution<long double> dis(0, 1);
+    for (std::size_t i = 0; i < v.size(); ++i)
+    {
+        v[i] = {dis(gen), dis(gen)};
+    }
+    return v;
+}
+
+template<typename T, typename std::enable_if<std::is_same<T, cpp_bin_float_50>::value , bool>::type = true>
+std::vector<T> generate_random_vector(std::size_t size, std::size_t seed)
+{
+    if (seed == 0)
+    {
+        std::random_device rd;
+        seed = rd();
+    }
+    std::vector<T> v(size);
+
+    std::mt19937 gen(seed);
+
+    std::normal_distribution<long double> dis(0, 1);
+    for (std::size_t i = 0; i < v.size(); ++i)
+    {
+        v[i] = dis(gen);
+    }
+    return v;
+}
+
+}} // Namesapces
