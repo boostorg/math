8a73fc3f93c307eabc03e75f5954f550c51c7be4
Christopher Kormanyos <ckormanyos@yahoo.com>
2021-02-07 15:54:28 +0100

    [CI SKIP] Baby steps investigate cbrt on metal

--- include/boost/math/tools/rational.hpp
+++ include/boost/math/tools/rational.hpp
@@ -10,7 +10,8 @@
 #pragma once
 #endif
 
-#include <boost/array.hpp>
+#include <array>
+
 #include <boost/math/tools/config.hpp>
 #include <boost/mpl/int.hpp>
 
@@ -209,7 +210,7 @@ inline V evaluate_polynomial(const T(&a)[N], const V& val) BOOST_MATH_NOEXCEPT(V
 }
 
 template <std::size_t N, class T, class V>
-inline V evaluate_polynomial(const boost::array<T,N>& a, const V& val) BOOST_MATH_NOEXCEPT(V)
+inline V evaluate_polynomial(const std::array<T,N>& a, const V& val) BOOST_MATH_NOEXCEPT(V)
 {
    typedef boost::integral_constant<int, N> tag_type;
    return detail::evaluate_polynomial_c_imp(static_cast<const T*>(a.data()), val, static_cast<tag_type const*>(0));
@@ -230,7 +231,7 @@ inline V evaluate_even_polynomial(const T(&a)[N], const V& z) BOOST_MATH_NOEXCEP
 }
 
 template <std::size_t N, class T, class V>
-inline V evaluate_even_polynomial(const boost::array<T,N>& a, const V& z) BOOST_MATH_NOEXCEPT(V)
+inline V evaluate_even_polynomial(const std::array<T,N>& a, const V& z) BOOST_MATH_NOEXCEPT(V)
 {
    return evaluate_polynomial(a, V(z*z));
 }
@@ -251,7 +252,7 @@ inline V evaluate_odd_polynomial(const T(&a)[N], const V& z) BOOST_MATH_NOEXCEPT
 }
 
 template <std::size_t N, class T, class V>
-inline V evaluate_odd_polynomial(const boost::array<T,N>& a, const V& z) BOOST_MATH_NOEXCEPT(V)
+inline V evaluate_odd_polynomial(const std::array<T,N>& a, const V& z) BOOST_MATH_NOEXCEPT(V)
 {
    typedef boost::integral_constant<int, N-1> tag_type;
    return a[0] + z * detail::evaluate_polynomial_c_imp(static_cast<const T*>(a.data()) + 1, V(z*z), static_cast<tag_type const*>(0));
@@ -317,7 +318,7 @@ inline V evaluate_rational(const T(&a)[N], const U(&b)[N], const V& z) BOOST_MAT
 }
 
 template <std::size_t N, class T, class U, class V>
-inline V evaluate_rational(const boost::array<T,N>& a, const boost::array<U,N>& b, const V& z) BOOST_MATH_NOEXCEPT(V)
+inline V evaluate_rational(const std::array<T,N>& a, const std::array<U,N>& b, const V& z) BOOST_MATH_NOEXCEPT(V)
 {
    return detail::evaluate_rational_c_imp(a.data(), b.data(), z, static_cast<boost::integral_constant<int, N>*>(0));
 }
