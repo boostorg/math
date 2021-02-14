34722bd6e6cb58acda5d556cdb643c31c654b5f3
Matt Borland <matt@mattborland.com>
2021-02-14 16:54:08 +0300

    Start TMP header [CI SKIP]

---
+++ include/boost/math/tools/mp.hpp
@@ -0,0 +1,20 @@
+//  Copyright Peter Dimov 2015.
+//  Copyright Matt Borland 2021.
+//  Use, modification and distribution are subject to the
+//  Boost Software License, Version 1.0. (See accompanying file
+//  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+//  Template metaprogramming classes and functions to replace MPL
+//  Source: http://www.pdimov.com/cpp2/simple_cxx11_metaprogramming.html
+
+#ifndef BOOST_MATH_TOOLS_MP
+#define BOOST_MATH_TOOLS_MP
+
+namespace boost { namespace math { namespace tools {
+
+template<typename... T>
+struct mp_list {};
+
+}}} // namespaces
+
+#endif // BOOST_MATH_TOOLS_MP
