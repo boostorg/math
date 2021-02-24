4528f83433b9a4fc7cbcc59218f496e3db5d1c59
Matt Borland <matt@mattborland.com>
2021-02-24 22:34:58 +0300

    Add coefficients struct and calculation [ci skip]

--- include/boost/math/interpolators/detail/2d_akima_detail.hpp
+++ include/boost/math/interpolators/detail/2d_akima_detail.hpp
@@ -19,6 +19,30 @@
 
 namespace boost { namespace math { namespace interpolators { namespace detail {
 
+// The 18 polynomial Coefficients
+template <typename Real>
+struct coefficients
+{
+    Real p00;
+    Real p10;
+    Real p01;
+    Real p20;
+    Real p11;
+    Real p02;
+    Real p30;
+    Real p40;
+    Real p50;
+    Real p03;
+    Real p04;
+    Real p05;
+    Real p41;
+    Real p14;
+    Real p21;
+    Real p31;
+    Real p12;
+    Real p13;
+};
+
 template <typename Matrix, typename VectorType = typename Matrix::value_type, typename Real = typename VectorType::value_type>
 class akima_2d_uniform_impl
 {
@@ -152,6 +176,72 @@ std::tuple<Real, Real, Real, Real> transform_coefficients(std::tuple<Real, Real,
     const Real D = cos(theta_us) / (lv * sin_theta_uv);
 
     return std::make_tuple(A, B, C, D);
+}
+
+// Determine the Coefficients of the Polynomial. EQNs A-20 through 25 calculated in sequential order
+template <typename Real>
+coefficients polynomial_coefficient(Real z00, std::tuple<Real, Real, Real, Real, Real> z00_partial_derivatives,
+                                    Real z10, std::tuple<Real, Real, Real, Real, Real> z10_partial_derivatives,
+                                    Real z01, std::tuple<Real, Real, Real, Real, Real> z01_partial_derivatives,
+                                    std::tuple<Real, Real, Real> vectors)
+{
+    using std::cos;
+    
+    coefficients<Real> c;
+
+    // EQN A-20
+    const Real zu00  = std::get<0>(z00_partial_derivatives);
+    const Real zv00  = std::get<1>(z00_partial_derivatives);
+    const Real zuu00 = std::get<2>(z00_partial_derivatives);
+    const Real zuv00 = std::get<3>(z00_partial_derivatives);
+    const Real zvv00 = std::get<4>(z00_partial_derivatives);
+
+    c.p00 = z00;
+    c.p10 = zu00;
+    c.p01 = zv00;
+    c.p20 = zuu00 / 2;
+    c.p11 = zuv00;
+    c.p02 = zvv00 / 2;
+
+    // EQN A-21
+    const Real zu10  = std::get<0>(z10_partial_derivatives);
+    const Real zv10  = std::get<1>(z10_partial_derivatives);
+    const Real zuu10 = std::get<2>(z10_partial_derivatives);
+    const Real zuv10 = std::get<3>(z10_partial_derivatives);
+    const Real zvv10 = std::get<4>(z10_partial_derivatives);
+
+    c.p30 = (10*z10 - 8*zu10 + zuu10 - 20*c.p00 - 12*c.p10 - 6*c.p20) / 2;
+    c.p40 = -15*z10 + 7*zu10 - zuu10 + 15*c.p00 + 8*c.p10 + 3*c.p20;
+    c.p50 = (12*z10 - 6*zu10 + zuu10 - 12*c.p00 - 6*c.p10 - 2*c.p20) / 2;
+
+    // EQN A-22
+    const Real zu01  = std::get<0>(z01_partial_derivatives);
+    const Real zv01  = std::get<1>(z01_partial_derivatives);
+    const Real zuu01 = std::get<2>(z01_partial_derivatives);
+    const Real zuv01 = std::get<3>(z01_partial_derivatives);
+    const Real zvv01 = std::get<4>(z01_partial_derivatives);
+
+    c.p03 = (20*z01 - 8*zv01 + zvv01 - 20*c.p00 - 12*c.p01 - 6*c.p02) / 2;
+    c.p04 = -15*z01 + 7*zv01 - zvv01 + 15*c.p00 + 8*c.p01 + 3*c.p02;
+    c.p05 = (12*z01 - 6*zv01 + zvv01 -12*c.p00 - 6*c.p01 - 2*c.p02) / 2;
+
+    // EQN A-23
+    const Real lu = std::get<0>(vectors);
+    const Real lv = std::get<1>(vectors);
+    const Real theta_uv = std::get<2>(vectors);
+
+    c.p41 = (5*lv*cos(theta_uv)/lu)*c.p50;
+    c.p14 = (5*lu*cos(theta_uv)/lv)*c.p05;
+
+    // EQN A-24
+    c.p21 = 3*zv10 - zuv10 - 3*c.p01 - 2*c.p11 + c.p41;
+    c.p31 = -2*zv10 + zuv10 + 2*c.p01 + c.p11 - 2*c.p41;
+
+    // EQN A-25
+    c.p12 = 3*zu01 - zuv01 - 3*c.p10 - 2*c.p11 + c.p14;
+    c.p13 = -2*zu01 + zuv01 + 2*c.p10 + c.p11 - 2*c.p14;
+
+    return c;
 }
 
 }}}} // namespaces
