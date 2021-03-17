3e7919d982392466e12addcdcc0400fe3e9a0c91
Matt Borland <matt@mattborland.com>
2021-03-17 21:24:32 +0300

    Limit ranges to GCC10

--- test/test_hyperexponential_dist.cpp
+++ test/test_hyperexponential_dist.cpp
@@ -387,8 +387,8 @@ BOOST_AUTO_TEST_CASE_TEMPLATE(error_cases, RealT, test_types)
    BOOST_MATH_CHECK_THROW(dist_t(probs.begin(), probs.begin(), rates.begin(), rates.begin()), std::domain_error);
    BOOST_MATH_CHECK_THROW(dist_t(rates.begin(), rates.begin()), std::domain_error);
 
-   // Test C++20 ranges
-   #if (__cplusplus > 202000L || _MSVC_LANG > 202000L) && defined(__cpp_lib_ranges)
+   // Test C++20 ranges (Currently only GCC10 has full support to P0896R4)
+   #if (__cplusplus > 202000L || _MSVC_LANG > 202000L) && __has_include(<ranges>) && __GNUC__ >= 10
    #include <ranges>
    #include <array>
 
