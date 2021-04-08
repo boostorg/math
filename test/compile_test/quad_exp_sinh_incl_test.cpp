db1e704a8de9cee4f3a08b7cad679b5914717b05
Matt Borland <matt@mattborland.com>
2021-04-08 20:57:03 +0300

    Reorganize/add quadrature compile tests

--- test/compile_test/quad_exp_sinh_incl_test.cpp
+++ test/compile_test/quad_exp_sinh_incl_test.cpp
@@ -3,21 +3,19 @@
 //  Boost Software License, Version 1.0. (See accompanying file
 //  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 //
-// Basic sanity check that header <boost/math/special_functions/gamma.hpp>
+// Basic sanity check that header
 // #includes all the files that it needs to.
 //
-#include <boost/math/quadrature/trapezoidal.hpp>
+#include <boost/math/quadrature/exp_sinh.hpp>
 //
 // Note this header includes no other headers, this is
 // important if this test is to be meaningful:
 //
 #include "test_compile_result.hpp"
 
-double func(double x) { return x; }
-
 void compile_and_link_test()
 {
-    double a = 0;
-    double b = 1;
-    check_result<double>(boost::math::quadrature::trapezoidal(func, a, b));
+    auto f = [](double x) { return x; };
+    boost::math::quadrature::exp_sinh<double> integrator;
+    check_result<double>(integrator.integrate(f));
 }
--- test/compile_test/quad_ooura_fourier_integrals_incl_test.cpp
+++ test/compile_test/quad_ooura_fourier_integrals_incl_test.cpp
@@ -3,10 +3,10 @@
 //  Boost Software License, Version 1.0. (See accompanying file
 //  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 //
-// Basic sanity check that header <boost/math/special_functions/gamma.hpp>
+// Basic sanity check that header
 // #includes all the files that it needs to.
 //
-#include <boost/math/quadrature/exp_sinh.hpp>
+#include <boost/math/quadrature/ooura_fourier_integrals.hpp>
 //
 // Note this header includes no other headers, this is
 // important if this test is to be meaningful:
@@ -16,6 +16,8 @@
 void compile_and_link_test()
 {
     auto f = [](double x) { return x; };
-    boost::math::quadrature::exp_sinh<double> integrator;
-    check_result<double>(integrator.integrate(f));
+    boost::math::quadrature::ooura_fourier_sin<double> sin_integrator;
+    boost::math::quadrature::ooura_fourier_cos<double> cos_integrator;
+    check_result<double>(sin_integrator.integrate(f, 1.0));
+    check_result<double>(cos_integrator.integrate(f, 1.0));
 }
