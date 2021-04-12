9de47e6a78d28e35e246a7652598961a4e9aeb67
Matt Borland <matt@mattborland.com>
2021-04-12 19:18:22 +0300

    Remove lexical cast from arcsine dist [ci skip]

--- include/boost/math/distributions/arcsine.hpp
+++ include/boost/math/distributions/arcsine.hpp
@@ -35,7 +35,6 @@
 #include <boost/math/constants/constants.hpp>
 
 #include <boost/math/special_functions/fpclassify.hpp> // isnan.
-#include <boost/math/tools/lexical_cast.hpp>
 
 #if defined (BOOST_MSVC)
 #  pragma warning(push)
@@ -86,20 +85,14 @@ namespace boost
       { // Check x_min < x_max
         if (x_min >= x_max)
         {
-          #ifndef BOOST_MATH_STANDALONE
-          std::string msg = "x_max argument is %1%, but must be > x_min = " + lexical_cast<std::string>(x_min) + "!";
-          #else
           std::string msg = "x_max argument is %1%, but must be > x_min";
-          #endif
           *result = policies::raise_domain_error<RealType>(
             function,
             msg.c_str(), x_max, pol);
-           // "x_max argument is %1%, but must be > x_min !", x_max, pol);
-            //  "x_max argument is %1%, but must be > x_min %2!", x_max, x_min, pol); would be better. 
-          // But would require replication of all helpers functions in /policies/error_handling.hpp for two values,
-          // as well as two value versions of raise_error, raise_domain_error and do_format ...
-          // so use slightly hacky lexical_cast to string instead.
-          // TODO: lexcial_cast workaround does not work with standalone mode...
+            // "x_max argument is %1%, but must be > x_min !", x_max, pol);
+            // "x_max argument is %1%, but must be > x_min %2!", x_max, x_min, pol); would be better. 
+            // But would require replication of all helpers functions in /policies/error_handling.hpp for two values,
+            // as well as two value versions of raise_error, raise_domain_error and do_format
           return false;
         }
         return true;
