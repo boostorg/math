daaefb7b772de62f01ec81878b1f7ed60b83f80e
Matt Borland <matt@mattborland.com>
2021-04-13 20:46:04 +0300

    Improve differentiation tests [ci skip]

--- test/compile_test/diff_lanczos_smoothing_incl_test.cpp
+++ test/compile_test/diff_lanczos_smoothing_incl_test.cpp
@@ -6,3 +6,25 @@
 // Basic sanity check that header
 // #includes all the files that it needs to.
 #include <boost/math/differentiation/lanczos_smoothing.hpp>
+//
+// Note this header includes no other headers, this is
+// important if this test is to be meaningful:
+//
+#include "test_compile_result.hpp"
+
+void compile_and_link_test()
+{
+    float f_temp;
+    boost::math::differentiation::discrete_lanczos_derivative f_lanczos(f_temp);
+    check_result<float>(f_lanczos.get_spacing());
+
+    double d_temp;
+    boost::math::differentiation::discrete_lanczos_derivative d_lanczos(d_temp);
+    check_result<double>(d_lanczos.get_spacing());
+    
+    #ifndef BOOST_MATH_NO_LONG_DOUBLE_MATH_FUNCTIONS
+    long double ld_temp;
+    boost::math::differentiation::discrete_lanczos_derivative ld_lanczos(ld_temp);
+    check_result<long double>(ld_lanczos.get_spacing());
+    #endif
+}
