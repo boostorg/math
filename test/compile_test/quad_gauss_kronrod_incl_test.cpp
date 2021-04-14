5747e14f8a0a5d7f6f29130a68c6b8b6e978f78e
Matt Borland <matt@mattborland.com>
2021-04-14 21:58:42 +0300

    Improve quadrature tests [ci skip]

--- test/compile_test/quad_gauss_kronrod_incl_test.cpp
+++ test/compile_test/quad_gauss_kronrod_incl_test.cpp
@@ -1,4 +1,4 @@
-//  Copyright Nick Thompson 2017.
+//  Copyright Matt Borland 2021.
 //  Use, modification and distribution are subject to the
 //  Boost Software License, Version 1.0. (See accompanying file
 //  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
@@ -7,3 +7,15 @@
 // #includes all the files that it needs to.
 //
 #include <boost/math/quadrature/gauss_kronrod.hpp>
+//
+// Note this header includes no other headers, this is
+// important if this test is to be meaningful:
+//
+#include "test_compile_result.hpp"
+
+void compile_and_link_test()
+{
+    auto f = [](double x) { return x; };
+    boost::math::quadrature::gauss<double, 1> integrator;
+    check_result<double>(integrator.integrate(f));
+}
