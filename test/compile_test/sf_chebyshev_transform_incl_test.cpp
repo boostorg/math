6b20b869f83a8d65465c94f6c2c4d8990c9075b3
Matt Borland <matt@mattborland.com>
2021-04-15 20:07:32 +0300

    Improve sf tests [ci skip]

--- test/compile_test/sf_chebyshev_transform_incl_test.cpp
+++ test/compile_test/sf_chebyshev_transform_incl_test.cpp
@@ -1,4 +1,4 @@
-//  Copyright John Maddock 2006.
+//  Copyright Matt Borland 2021
 //  Use, modification and distribution are subject to the
 //  Boost Software License, Version 1.0. (See accompanying file
 //  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
@@ -7,3 +7,15 @@
 // #includes all the files that it needs to.
 //
 #include <boost/math/special_functions/chebyshev_transform.hpp>
+//
+// Note this header includes no other headers, this is
+// important if this test is to be meaningful:
+//
+#include "test_compile_result.hpp"
+
+void compile_and_link_test()
+{   
+    auto f = [](double x) { return x; };
+    boost::math::chebyshev_transform test(f, 0.0, 1.0);
+    check_result<double>(test(1.0));
+}
