ce185afd825a0da598642911ca14619c4c92842d
Matt Borland <matt@mattborland.com>
2021-08-11 23:01:54 +0300

    Add missing header to tests

--- test/ccmath_abs_test.cpp
+++ test/ccmath_abs_test.cpp
@@ -3,6 +3,7 @@
 //  Boost Software License, Version 1.0. (See accompanying file
 //  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
+#include <cstdint>
 #include <limits>
 #include <type_traits>
 #include <boost/math/ccmath/abs.hpp>
