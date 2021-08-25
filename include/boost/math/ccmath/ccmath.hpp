d8fe127b827e496c0dc8546d419cdda3aa2e0a00
Matt Borland <matt@mattborland.com>
2021-08-25 23:43:43 +0300

    constexpr fpclassify (#679)
    * Implement fpclassify and tests

--- include/boost/math/ccmath/ccmath.hpp
+++ include/boost/math/ccmath/ccmath.hpp
@@ -14,5 +14,6 @@
 #include <boost/math/ccmath/fabs.hpp>
 #include <boost/math/ccmath/isfinite.hpp>
 #include <boost/math/ccmath/isnormal.hpp>
+#include <boost/math/ccmath/fpclassify.hpp>
 
 #endif // BOOST_MATH_CCMATH
