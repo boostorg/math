f7efaa7b43b908cb332a5d939a77c47439277c3a
jzmaddock <john@johnmaddock.co.uk>
2021-09-03 16:08:43 +0100

    More bessel J probing.

--- t.cpp
+++ t.cpp
@@ -10,6 +10,6 @@ int main()
 {
    std::cout << std::setprecision(18) << boost::math::cyl_bessel_j(0, 11.791534423828125) << std::endl;
    std::cout << std::setprecision(35) << boost::math::cyl_bessel_j(0, 11.791534423828125L) << std::endl;
-   std::cout << std::setprecision(35) << boost::math::cyl_bessel_j(0, 11.791534423828125Q) << std::endl;
+   //std::cout << std::setprecision(35) << boost::math::cyl_bessel_j(0, 11.791534423828125Q) << std::endl;
    return 0;
 }
