000aaae2d18862c552910434fa085ea7967f953f
Matt Borland <matt@mattborland.com>
2021-09-06 15:44:02 +0300

    Add testing

--- include/boost/math/ccmath/ldexp.hpp
+++ include/boost/math/ccmath/ldexp.hpp
@@ -9,6 +9,7 @@
 #include <cmath>
 #include <limits>
 #include <type_traits>
+#include <stdexcept>
 #include <boost/math/tools/is_constant_evaluated.hpp>
 #include <boost/math/special_functions/pow.hpp>
 #include <boost/math/ccmath/abs.hpp>
@@ -17,6 +18,68 @@
 #include <boost/math/ccmath/isfinite.hpp>
 
 namespace boost::math::ccmath {
+
+namespace detail {
+
+template <typename T>
+inline constexpr T negative_exp(int exp)
+{
+    int abs_exp = boost::math::ccmath::abs(exp);
+
+    // std::feclearexcept and std::fetestexcept are not constexpr
+    // Simple test if approaching underflow
+    constexpr T denorm_min_limit = std::numeric_limits<T>::denorm_min() * 2;
+    T result = 2;
+
+    while(abs_exp >= 0)
+    {
+        if(result < denorm_min_limit)
+        {
+            throw std::underflow_error("Exponent leads to a value that is smaller than minimum positive subnormal value");
+        }
+        
+        result /= 2;
+        --abs_exp;
+    }
+
+    return result;
+}
+
+template <typename T>
+inline constexpr T positive_exp(int exp)
+{
+    constexpr T max_limit = (std::numeric_limits<T>::max)() / 2;
+    T result = 2;
+
+    while((exp-1) > 0)
+    {
+        if(result > max_limit)
+        {
+            throw std::overflow_error("Exponent leads to a value that is larger than the maximum allowable value without overflow");
+        }
+
+        result *= 2;
+        --exp;
+    }
+
+    return result;
+}
+
+// A rudimentary implementation of exp2 that only deals with int exponents for simplicity
+template <typename T>
+inline constexpr T intexp2(int exp)
+{
+    if(exp > 0)
+    {
+        return positive_exp<T>(exp);
+    }
+    else
+    {
+        return negative_exp<T>(exp);
+    }
+}
+
+} // Namespace detail
 
 template <typename Real, std::enable_if_t<!std::is_integral_v<Real>, bool> = true>
 inline constexpr Real ldexp(Real arg, int exp)
@@ -27,7 +90,7 @@ inline constexpr Real ldexp(Real arg, int exp)
                boost::math::ccmath::isinf(arg) ? arg :
                boost::math::ccmath::isnan(arg) ? arg :
                exp == 0 ? arg :
-               arg * boost::math::pow<exp>(2); // Todo replace with boost::math::ccmath::exp2
+               arg * boost::math::ccmath::detail::intexp2<Real>(exp);
     }
     else
     {
