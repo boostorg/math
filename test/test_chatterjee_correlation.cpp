ea41f945a5a2cfa1fb458fb7e56c17385910e188
Matt Borland <matt@mattborland.com>
2022-05-24 09:00:42 -0700

    Change tolerance for sin comparison

--- test/test_chatterjee_correlation.cpp
+++ test/test_chatterjee_correlation.cpp
@@ -127,7 +127,7 @@ void test_paper()
     }
 
     result = chatterjee_correlation(x, y);
-    CHECK_MOLLIFIED_CLOSE(result, Real(0.885), 0.005);
+    CHECK_MOLLIFIED_CLOSE(result, Real(0.885), 0.01);
 }
 
 int main(void)
