3e818ac43fb761ba1b7319bf2bad2536841f607a
Matt Borland <matt@mattborland.com>
2022-10-17 13:52:00 -0700

    Spell my name correctly

--- test/git_issue_845.cpp
+++ test/git_issue_845.cpp
@@ -1,4 +1,4 @@
-// Copyright Matt Boralnd, 2022
+// Copyright Matt Borland, 2022
 // Use, modification and distribution are subject to the
 // Boost Software License, Version 1.0. (See accompanying file
 // LICENSE or copy at http://www.boost.org/LICENSE_1_0.txt)
