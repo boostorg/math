da18adecd87363f33f9f5e8590e7b71236f8657d
jzmaddock <john@johnmaddock.co.uk>
2022-12-06 09:00:14 +0000

    Add licence/copyright to new file.

--- test/issue893.cpp
+++ test/issue893.cpp
@@ -1,3 +1,8 @@
+// Copyright John Maddock, 2022
+// Use, modification and distribution are subject to the
+// Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt
+// or copy at http://www.boost.org/LICENSE_1_0.txt)
 
 #define BOOST_TEST_MODULE issue893
 
