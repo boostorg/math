a4140eedbc273ecedabdcd8b0b5a7108994bb9f0
Nick <NAThompson@users.noreply.github.com>
2023-06-12 08:32:12 -0700

    Update example/calculate_fourier_transform_daubechies_constants.cpp
    Co-authored-by: Matt Borland <matt@mattborland.com>

--- example/calculate_fourier_transform_daubechies_constants.cpp
+++ example/calculate_fourier_transform_daubechies_constants.cpp
@@ -1,3 +1,8 @@
+//  (C) Copyright Nick Thompson 2023.
+//  Use, modification and distribution are subject to the
+//  Boost Software License, Version 1.0. (See accompanying file
+//  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
 #include <utility>
 #include <boost/math/filters/daubechies.hpp>
 #include <boost/math/tools/polynomial.hpp>
