3b995fc9bee789a99a90b3f3ae6eaf75c76e7b08
Nick <NAThompson@users.noreply.github.com>
2023-06-12 08:32:21 -0700

    Update example/fourier_transform_daubechies_ulp_plot.cpp
    Co-authored-by: Matt Borland <matt@mattborland.com>

--- example/fourier_transform_daubechies_ulp_plot.cpp
+++ example/fourier_transform_daubechies_ulp_plot.cpp
@@ -1,3 +1,8 @@
+//  (C) Copyright Nick Thompson 2023.
+//  Use, modification and distribution are subject to the
+//  Boost Software License, Version 1.0. (See accompanying file
+//  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
 #include <boost/math/special_functions/fourier_transform_daubechies_scaling.hpp>
 #include <boost/math/tools/ulps_plot.hpp>
 
