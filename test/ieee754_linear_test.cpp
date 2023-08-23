0a64a5fece5dc8c570dec1f3455631c13cec9ecc
Ryan <ryan.elandt@gmail.com>
2023-08-22 21:43:19 -0400

    fix issue type issue for std::float32_t + 64_t

--- test/ieee754_linear_test.cpp
+++ test/ieee754_linear_test.cpp
@@ -25,27 +25,24 @@ using boost::math::tools::detail::ieee754_linear::Layout_IEEE754Linear;
 using boost::math::tools::detail::ieee754_linear::Layout_Unspecified;
 
 
-template <typename L>
-bool is_layout_equal(L, L) { return true; }
-
-template <typename L1, typename L2>
-bool is_layout_equal(L1, L2) { return false; }
-
 template <typename T>
 bool is_unspeci(Layout_Unspecified<T>) { return true; }
+
+template <typename T>
+bool is_paired_with_uint32_t(Layout_IEEE754Linear<T, uint32_t>, T) { return true; }
+
+template <typename T>
+bool is_paired_with_uint64_t(Layout_IEEE754Linear<T, uint64_t>, T) { return true; }
+
+template <typename T>
+bool is_paired_with_uint128_t(Layout_IEEE754Linear<T, boost::uint128_type>, T) { return true; }
 
 void test_layout() {
    using boost::math::tools::detail::ieee754_linear::LayoutIdentifier;
 
-   const auto layout_f32_u32 = Layout_IEEE754Linear<float, uint32_t>();
-   const auto layout_f64_u64 = Layout_IEEE754Linear<double, uint64_t>();
-#if defined(BOOST_HAS_FLOAT128) && defined(BOOST_HAS_INT128)
-   const auto layout_f128_u128 = Layout_IEEE754Linear<__float128, boost::uint128_type>();
-#endif
-
    // Check layout for float types
-   BOOST_CHECK(is_layout_equal(layout_f32_u32, LayoutIdentifier::get_layout<float>()));
-   BOOST_CHECK(is_layout_equal(layout_f64_u64, LayoutIdentifier::get_layout<double>()));
+   BOOST_CHECK(is_paired_with_uint32_t(LayoutIdentifier::get_layout<float>(), float()));
+   BOOST_CHECK(is_paired_with_uint64_t(LayoutIdentifier::get_layout<double>(), double()));
    BOOST_CHECK(is_unspeci(LayoutIdentifier::get_layout<boost::math::concepts::real_concept>()));
    BOOST_CHECK(is_unspeci(LayoutIdentifier::get_layout<boost::multiprecision::cpp_bin_float_50>()));
    BOOST_CHECK(is_unspeci(LayoutIdentifier::get_layout<boost::multiprecision::cpp_bin_float_100>()));
@@ -54,17 +51,17 @@ void test_layout() {
 
 // Float128
 #if defined(BOOST_HAS_FLOAT128) && defined(BOOST_HAS_INT128)
-   BOOST_CHECK(is_layout_equal(layout_f128_u128, LayoutIdentifier::get_layout<boost::multiprecision::float128>()));
-   BOOST_CHECK(is_layout_equal(layout_f128_u128, LayoutIdentifier::get_layout<__float128>()));
+   BOOST_CHECK(is_paired_with_uint128_t(LayoutIdentifier::get_layout<__float128>(), __float128()));
+   BOOST_CHECK(is_paired_with_uint128_t(LayoutIdentifier::get_layout<boost::multiprecision::float128>(), __float128()));
 #endif
 
 // stdfloat
 #if __has_include(<stdfloat>)
 #ifdef __STDCPP_FLOAT32_T__
-   BOOST_CHECK(is_layout_equal(layout_f32_u32, LayoutIdentifier::get_layout<std::float32_t>()));
+   BOOST_CHECK(is_paired_with_uint32_t(LayoutIdentifier::get_layout<std::float32_t>(), std::float32_t()));
 #endif
 #ifdef __STDCPP_FLOAT64_T__
-   BOOST_CHECK(is_layout_equal(layout_f64_u64, LayoutIdentifier::get_layout<std::float64_t>()));
+   BOOST_CHECK(is_paired_with_uint64_t(LayoutIdentifier::get_layout<std::float64_t>(), std::float64_t()));
 #endif 
 #endif
 
