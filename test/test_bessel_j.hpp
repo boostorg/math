1bf40962734807c338de7eb1caf25b8328f8c895
jzmaddock <john@johnmaddock.co.uk>
2024-01-22 16:29:27 +0000

    increase tolerance

--- test/test_bessel_j.hpp
+++ test/test_bessel_j.hpp
@@ -274,7 +274,7 @@ void test_bessel(T, const char* name)
     //
     // special cases for code coverage:
     //
-    T tolerance = boost::math::tools::epsilon<T>() * 200;
+    T tolerance = boost::math::tools::epsilon<T>() * 2000;
     BOOST_CHECK_CLOSE_FRACTION(boost::math::sph_bessel(200, T(0.5)), T(3.070403008048099934928128420285169174541102108657574230431e-497L), tolerance);
     BOOST_MATH_CHECK_THROW(boost::math::sph_bessel(2, T(-2.0)), std::domain_error);
 }
