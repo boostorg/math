bdcecaa7be801d31ad040bfe6faab4ff045b443d
jzmaddock <john@johnmaddock.co.uk>
2024-02-10 10:42:45 +0000

    Correct variable name.

--- include/boost/math/special_functions/bessel_iterators.hpp
+++ include/boost/math/special_functions/bessel_iterators.hpp
@@ -156,7 +156,7 @@ namespace boost {
                boost::math::policies::raise_domain_error("bessel_i_forwards_iterator<%1%>", "Order must be < 0 stable forwards recurrence but got %1%", v, Policy());
          }
          bessel_i_forwards_iterator(const T& v, const T& x, const T& I_v_minus_1, const T& I_v)
-            : it(detail::bessel_ik_recurrence<T>(v, x), I_v_plus_1, I_v)
+            : it(detail::bessel_ik_recurrence<T>(v, x), I_v_minus_1, I_v)
          {
             if (v > 1)
                boost::math::policies::raise_domain_error("bessel_i_forwards_iterator<%1%>", "Order must be < 0 stable forwards recurrence but got %1%", v, Policy());
