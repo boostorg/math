3801e07c9b684f010e4650a6851a0eb667467644
Christopher Kormanyos <ckormanyos@yahoo.com>
2024-02-15 18:24:28 +0100

    Start finishing instantiate-tests (more to come)

--- test/test_compile_test_and_run.cpp
+++ test/test_compile_test_and_run.cpp
@@ -4,6 +4,8 @@
 //  Boost Software License, Version 1.0. (See accompanying file
 //  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
+#define TEST_COMPLEX
+
 #include <boost/core/lightweight_test.hpp>
 #include <boost/cstdfloat.hpp>
 
