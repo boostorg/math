e844686f9d2dabe9fe4f88242c4c4af329db0127
jzmaddock <john@johnmaddock.co.uk>
2024-06-29 11:50:50 +0100

    tgamma coverage #1.

---
+++ test/test_tgamma_eh.cpp
@@ -0,0 +1,16 @@
+//  (C) Copyright John Maddock 2024.
+//  Use, modification and distribution are subject to the
+//  Boost Software License, Version 1.0. (See accompanying file
+//  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#include "math_unit_test.hpp"
+#include <cfenv>
+#include <iostream>
+#include <boost/math/special_functions/gamma.hpp>
+
+
+int main()
+{
+   CHECK_EQUAL(boost::math::tgamma(-200.5), 0.0); // triggers internal exception handling
+   return boost::math::test::report_errors();
+}
