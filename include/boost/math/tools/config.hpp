530296e5605dba961e68d22a2d970030a9716360
Matt Borland <matt@mattborland.com>
2024-07-24 13:36:36 -0400

    Fix GPU safe functions and add macro for local static variables

--- include/boost/math/tools/config.hpp
+++ include/boost/math/tools/config.hpp
@@ -734,15 +734,15 @@ BOOST_MATH_GPU_ENABLED constexpr T gpu_safe_min(const T& a, const T& b) { return
 template <class T>
 BOOST_MATH_GPU_ENABLED constexpr T cuda_safe_max(const T& a, const T& b) { return a > b ? a : b; }
 
-#define BOOST_MATH_GPU_SAFE_SWAP(a, b) gpu_safe_swap(a, b);
-#define BOOST_MATH_GPU_SAFE_MIN(a, b) gpu_safe_min(a, b);
-#define BOOST_MATH_GPU_SAFE_MAX(a, b) gpu_safe_max(a, b);
+#define BOOST_MATH_GPU_SAFE_SWAP(a, b) gpu_safe_swap(a, b)
+#define BOOST_MATH_GPU_SAFE_MIN(a, b) gpu_safe_min(a, b)
+#define BOOST_MATH_GPU_SAFE_MAX(a, b) gpu_safe_max(a, b)
 
 #else
 
-#define BOOST_MATH_GPU_SAFE_SWAP(a, b) std::swap(a, b);
-#define BOOST_MATH_GPU_SAFE_MIN(a, b) (std::min)(a, b);
-#define BOOST_MATH_GPU_SAFE_MAX(a, b) (std::max)(a, b);
+#define BOOST_MATH_GPU_SAFE_SWAP(a, b) std::swap(a, b)
+#define BOOST_MATH_GPU_SAFE_MIN(a, b) (std::min)(a, b)
+#define BOOST_MATH_GPU_SAFE_MAX(a, b) (std::max)(a, b)
 
 #endif
 
@@ -752,13 +752,20 @@ BOOST_MATH_GPU_ENABLED constexpr T cuda_safe_max(const T& a, const T& b) { retur
 #if defined(__cpp_inline_variables) && __cpp_inline_variables >= 201606L
 #  define BOOST_MATH_STATIC_CONSTEXPR inline constexpr
 #  define BOOST_MATH_STATIC static
+#  ifndef BOOST_MATH_HAS_GPU_SUPPORT
+#    define BOOST_MATH_STATIC_LOCAL_VARIABLE static
+#  else
+#    define BOOST_MATH_STATIC_LOCAL_VARIABLE
+#  endif
 #else
 #  ifndef BOOST_MATH_HAS_GPU_SUPPORT
 #    define BOOST_MATH_STATIC_CONSTEXPR static constexpr
 #    define BOOST_MATH_STATIC static
+#    define BOOST_MATH_STATIC_LOCAL_VARIABLE
 #  else
 #    define BOOST_MATH_STATIC_CONSTEXPR constexpr
 #    define BOOST_MATH_STATIC constexpr
+#    define BOOST_MATH_STATIC_LOCAL_VARIABLE static
 #  endif
 #endif
 
