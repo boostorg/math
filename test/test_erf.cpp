5ab4837495b6922a417c4935faa9f7fec8a37885
Matt Borland <matt@mattborland.com>
2024-07-29 17:10:47 -0400

    Add SYCL erf testing

--- test/test_erf.cpp
+++ test/test_erf.cpp
@@ -4,7 +4,11 @@
 //  Boost Software License, Version 1.0. (See accompanying file
 //  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
+#ifndef SYCL_LANGUAGE_VERSION
 #include <pch_light.hpp>
+#endif
+
+#include <boost/math/special_functions/erf.hpp>
 #include "test_erf.hpp"
 
 //
