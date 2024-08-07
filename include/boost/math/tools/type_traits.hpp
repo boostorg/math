d05e3c39bb5f5690f68fe7298068dca1b2caafc6
Matt Borland <matt@mattborland.com>
2024-08-07 11:43:18 -0400

    Begin to add <type_traits>

---
+++ include/boost/math/tools/type_traits.hpp
@@ -0,0 +1,43 @@
+//  Copyright (c) 2024 Matt Borland
+//  Use, modification and distribution are subject to the
+//  Boost Software License, Version 1.0. (See accompanying file
+//  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+//  Regular use of <type_traits> is not compatible with CUDA
+//  Adds aliases to unify the support
+//  Also adds convience overloads like is_same_v so we don't have to wait for C++17
+
+#ifndef BOOST_MATH_TOOLS_TYPE_TRAITS
+#define BOOST_MATH_TOOLS_TYPE_TRAITS
+
+#include <boost/math/tools/config.hpp>
+
+namespace boost {
+namespace math {
+
+#ifdef BOOST_MATH_CUDA_ENABLED
+
+using cuda::std::is_void;
+using cuda::std::is_integral;
+using cuda::std::enable_if_t;
+
+#else // STD versions
+
+#include <type_traits>
+
+using std::is_void;
+using std::is_integral;
+using std::enable_if_t;
+
+#endif 
+
+template <typename T>
+BOOST_MATH_STATIC_CONSTEXPR bool is_void_v = boost::math::is_void<T>::value;
+
+template <typename T>
+BOOST_MATH_STATIC_CONSTEXPR bool is_integral_v = boost::math::is_integral<T>::value;
+
+} // namespace math
+} // namespace boost
+
+#endif // BOOST_MATH_TOOLS_TYPE_TRAITS
