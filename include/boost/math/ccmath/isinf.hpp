b101f106cc1818a0c67e7c7c8930dad63c83ed5e
Matt Borland <matt@mattborland.com>
2024-12-17 06:35:24 -0500

    Change minimum clang version for -Wnan-infinity-disabled

--- include/boost/math/ccmath/isinf.hpp
+++ include/boost/math/ccmath/isinf.hpp
@@ -23,9 +23,11 @@ constexpr bool isinf BOOST_MATH_PREVENT_MACRO_SUBSTITUTION(T x) noexcept
         if constexpr (std::numeric_limits<T>::is_signed)
         {
 #if defined(__clang_major__) && __clang_major__ >= 6
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wtautological-constant-compare"
-#pragma clang diagnostic ignored "-Wnan-infinity-disabled"
+#  pragma clang diagnostic push
+#  pragma clang diagnostic ignored "-Wtautological-constant-compare"
+#  if __clang_major__ >= 18
+#    pragma clang diagnostic ignored "-Wnan-infinity-disabled"
+#  endif
 #endif
             return x == std::numeric_limits<T>::infinity() || -x == std::numeric_limits<T>::infinity();
 #if defined(__clang_major__) && __clang_major__ >= 6
