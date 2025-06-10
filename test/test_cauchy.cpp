f79479b40e25952a166288debd4fab5a0a987081
Matt Borland <matt@mattborland.com>
2025-06-10 10:33:34 -0400

    Avoid tails on SYCL platform

--- test/test_cauchy.cpp
+++ test/test_cauchy.cpp
@@ -136,6 +136,8 @@ void test_spots(RealType T)
          static_cast<RealType>(-15000000.0)),
          static_cast<RealType>(0.000000021220659078919346664504384865488560725L),
          tolerance); // %
+
+   #ifndef SYCL_LANGUAGE_VERSION // Returns infinity
    BOOST_CHECK_CLOSE(
       // Test the CDF at -max_value()/4.
       // For an input x of this magnitude, the reference value is 4/|x|/pi.
@@ -146,6 +148,7 @@ void test_spots(RealType T)
                       / boost::math::tools::max_value<RealType>()
                       / boost::math::constants::pi<RealType>(),
          tolerance); // %
+   #endif
 
    //
    // Complements:
@@ -216,6 +219,8 @@ void test_spots(RealType T)
          static_cast<RealType>(15000000.0))),
          static_cast<RealType>(0.000000021220659078919346664504384865488560725L),
          tolerance); // %
+
+   #ifndef SYCL_LANGUAGE_VERSION // Returns infinity
    BOOST_CHECK_CLOSE(
       // Test the complemented CDF at max_value()/4.
       // For an input x of this magnitude, the reference value is 4/x/pi.
@@ -226,6 +231,7 @@ void test_spots(RealType T)
                       / boost::math::tools::max_value<RealType>()
                       / boost::math::constants::pi<RealType>(),
          tolerance); // %
+   #endif
 
    //
    // Quantiles:
