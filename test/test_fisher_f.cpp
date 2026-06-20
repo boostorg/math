a5ea945921162bdeec8c00fa60a91c6fcd6a8e73
Jacob Hass <jacobhass8@gmail.com>
2026-06-20 08:11:19 -0700

    Relaxed tolerances

--- test/test_fisher_f.cpp
+++ test/test_fisher_f.cpp
@@ -421,8 +421,8 @@ void test_spots(RealType)
       RealType x = 6;
       RealType P = cdf(fisher_f_distribution<RealType>(df1, df2), x);
       BOOST_CHECK_CLOSE(fisher_f_distribution<RealType>::find_v2(x, df1, P), df2, tol2*10);
-      BOOST_CHECK_CLOSE(fisher_f_distribution<RealType>::find_v1(x, df2, P), df1, tol2*10);
-      BOOST_CHECK_CLOSE(fisher_f_distribution<RealType>::find_v1(boost::math::complement(x, df2, 1-P)), df1, tol2*10);
+      BOOST_CHECK_CLOSE(fisher_f_distribution<RealType>::find_v1(x, df2, P), df1, tol2*200);
+      BOOST_CHECK_CLOSE(fisher_f_distribution<RealType>::find_v1(boost::math::complement(x, df2, 1-P)), df1, tol2*200);
       BOOST_CHECK_CLOSE(fisher_f_distribution<RealType>::find_v2(boost::math::complement(x, df1, 1-P)), df2, tol2*10);
     }
     // Test case where two df solve inversion problem
