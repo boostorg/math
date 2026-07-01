2431ddb08af3bb5ddece14ce8f41c8f7ad8875fa
Jacob Hass <jacobhass8@gmail.com>
2026-06-30 19:50:13 -0700

    Added boost license

--- test/test_dist_helpers.hpp
+++ test/test_dist_helpers.hpp
@@ -1,3 +1,10 @@
+/*
+ * Copyright Jacob Hass, 2026
+ * Use, modification and distribution are subject to the
+ * Boost Software License, Version 1.0. (See accompanying file
+ * LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+ */
+
 #ifndef BOOST_MATH_TEST_DIST_HELPERS_HPP
 #define BOOST_MATH_TEST_DIST_HELPERS_HPP
 
