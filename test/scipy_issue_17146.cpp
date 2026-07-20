30fbc9755d529d06ea156621906e4adfb248c186
Matt Borland <matt@mattborland.com>
2026-07-20 13:35:25 -0400

    Fix missing name for clang

--- test/scipy_issue_17146.cpp
+++ test/scipy_issue_17146.cpp
@@ -22,7 +22,7 @@ int main()
     const auto binom_dist = boost::math::binomial_distribution<double>(n, p);
     std::array<double, 3> vals {};
 
-    for (size_t i = 0; i < 2; ++i)
+    for (std::size_t i = 0; i < 2; ++i)
     {
         vals[i] = boost::math::pdf(binom_dist, i);
     }
