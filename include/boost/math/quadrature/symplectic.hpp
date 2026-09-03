8c4e14c5511ae732ba4c2ae5bd52df29f2f12210
Jacob Hass <jacobhass8@gmail.com>
2026-09-02 20:49:12 -0700

    Added cubic spline

--- include/boost/math/quadrature/symplectic.hpp
+++ include/boost/math/quadrature/symplectic.hpp
@@ -23,10 +23,11 @@
 #include <boost/math/policies/policy.hpp>
 #include <boost/math/policies/error_handling.hpp>
 #include <boost/math/special_functions/fpclassify.hpp>
+#include <boost/math/interpolators/cubic_hermite.hpp>
 
-namespace boost{ namespace math { namespace quadrature { 
+namespace boost{ namespace math { namespace quadrature {
 
-BOOST_MATH_EXPORT enum class available_methods 
+BOOST_MATH_EXPORT enum class available_methods
 {
     Y6,
     Y4,
@@ -34,7 +35,7 @@ BOOST_MATH_EXPORT enum class available_methods
     SRKNB6,
     SRKNB11
 };
-    
+
 namespace detail {
 
 template<typename...>
@@ -60,7 +61,7 @@ BOOST_MATH_INLINE_CONSTEXPR bool has_size_v = has_size<T>::value;
 
 template <typename T, typename U>
 typename std::enable_if_t<has_plus_v<T, U>, void>
-add(T& x, const U& y) 
+add(T& x, const U& y)
 {
     x = x + y;
 }
@@ -86,14 +87,14 @@ BOOST_MATH_INLINE_CONSTEXPR bool has_mult_v = has_mult<T, U>::value;
 
 template <typename T, typename U>
 typename std::enable_if_t<has_mult_v<T, U>, void>
-mult_prefactor(T& x, U prefactor) 
+mult_prefactor(T& x, U prefactor)
 {
     x = x * prefactor;
 }
 
 template <typename T, typename U>
 typename std::enable_if_t<!has_plus_v<T, U>, void>
-mult_prefactor(T& vec1, U prefactor) 
+mult_prefactor(T& vec1, U prefactor)
 {
     for (std::size_t i=0; i < vec1.size(); i++)
     {
@@ -135,7 +136,7 @@ initialize_array(const T& p0, const size_t N)
 }
 
 template <typename RandomAccessContainer, typename RealType, class Func>
-void second_order_yoshida(RandomAccessContainer& p0, RandomAccessContainer& q0, const RealType dt, 
+void second_order_yoshida(RandomAccessContainer& p0, RandomAccessContainer& q0, const RealType dt,
                           const Func dHdp, const Func dHdq)
 {
 
@@ -156,13 +157,13 @@ void second_order_yoshida(RandomAccessContainer& p0, RandomAccessContainer& q0, 
 }
 
 template <typename RandomAccessContainer, typename RealType, class Func>
-void fourth_order_yoshida(RandomAccessContainer& p0, RandomAccessContainer& q0, const RealType dt, 
+void fourth_order_yoshida(RandomAccessContainer& p0, RandomAccessContainer& q0, const RealType dt,
                           const Func dHdp, const Func dHdq)
 {
     BOOST_MATH_STD_USING
 
     RealType x1 = RealType(1) / (RealType(2) - cbrt(RealType(2)));
-    RealType x0 = RealType(1) - RealType(2) * x1; 
+    RealType x0 = RealType(1) - RealType(2) * x1;
 
     std::array<RealType, 3> weights = { x1, x0, x1 };
 
@@ -173,15 +174,15 @@ void fourth_order_yoshida(RandomAccessContainer& p0, RandomAccessContainer& q0, 
 }
 
 template <typename RandomAccessContainer, typename RealType, class Func>
-void sixth_order_yoshida(RandomAccessContainer& p0, RandomAccessContainer& q0, const RealType dt, 
+void sixth_order_yoshida(RandomAccessContainer& p0, RandomAccessContainer& q0, const RealType dt,
                          const Func dHdp, const Func dHdq)
 {
-    
+
     // Choosing "System A" solution
     // The following Mathematica command can calculate these coefficients to arbitrary precision
     // FindRoot[{w0+2(w1+w2+w3)==1,
-    //           w0^3 + 2(w1^3 + w2^3+w3^3)==0, 
-    //           w0^5 + 2(w1^5 + w2^5+w3^5)==0, 
+    //           w0^3 + 2(w1^3 + w2^3+w3^3)==0,
+    //           w0^5 + 2(w1^5 + w2^5+w3^5)==0,
     //           1/6(w0^2w1^3-w0^4*w1) - 1/6(w0^3w1^2-w0*w1^4)+1/6((w0+2w1)^2w2^3-(w0+2w1)(w0^3+2w1^3)w2) - 1/6((w0^3+2w1^3)w2^2-(w0+2w1)w2^4) +1/6((w0+2w1+2w2)^2w3^3-(w0+2w1+2w2)(w0^3+2w1^3+2w2^3)w3)-1/6((w0^3+2w1^3+2w2^3)w3^2-(w0+2w1+2w2)w3^4)==0}, {{w0,1.3151863206839063}, {w1,-1.17767998417887}, {w2,0.235573213359357}, {w3,0.784513610477560}}, WorkingPrecision->64]
     RealType w1 = static_cast<RealType>(-1.17767998417887100694641568096431573463926925263459848447536851379674155618156L);
     RealType w2 = static_cast<RealType>(0.23557321335935813368479318297853460168646808210340111900349313095621471215223L);
@@ -197,11 +198,11 @@ void sixth_order_yoshida(RandomAccessContainer& p0, RandomAccessContainer& q0, c
 }
 
 template <typename RandomAccessContainer, typename RealType, class Func>
-void SRKN_b_order_6(RandomAccessContainer& p0, RandomAccessContainer& q0, const RealType dt, 
+void SRKN_b_order_6(RandomAccessContainer& p0, RandomAccessContainer& q0, const RealType dt,
                     const Func dHdp, const Func dHdq)
-{ // This method implements SRKN_b^6 in Table 3 here 
+{ // This method implements SRKN_b^6 in Table 3 here
   // https://www.sciencedirect.com/science/article/pii/S0377042701004927
-    
+
     RealType b1 = static_cast<RealType>(0.0829844064174052);
     RealType b2 = static_cast<RealType>(0.396309801498368);
     RealType b3 = static_cast<RealType>(-0.0390563049223486);
@@ -213,13 +214,13 @@ void SRKN_b_order_6(RandomAccessContainer& p0, RandomAccessContainer& q0, const 
 
     std::array<RealType, 6> b_weights = {b1, b2, b3, b4, b3, b2};
     std::array<RealType, 6> a_weights = {a1, a2, a3, a3, a2, a1};
-    
+
     RealType a, b;
     for (std::size_t i=0; i < b_weights.size(); i++)
     {
         b = b_weights[i];
         a = a_weights[i];
-        
+
         RandomAccessContainer dq = dHdp(p0);
         mult_prefactor(dq, dt * b);
         add(q0, dq);
@@ -235,11 +236,11 @@ void SRKN_b_order_6(RandomAccessContainer& p0, RandomAccessContainer& q0, const 
 }
 
 template <typename RandomAccessContainer, typename RealType, class Func>
-void SRKN_b_order_11(RandomAccessContainer& p0, RandomAccessContainer& q0, const RealType dt, 
+void SRKN_b_order_11(RandomAccessContainer& p0, RandomAccessContainer& q0, const RealType dt,
                      const Func dHdp, const Func dHdq)
-{ // This method implements SRKN_b^11 in Table 3 here 
+{ // This method implements SRKN_b^11 in Table 3 here
   // https://www.sciencedirect.com/science/article/pii/S0377042701004927
-    
+
     RealType b1 = static_cast<RealType>(0.0414649985182624);
     RealType b2 = static_cast<RealType>(0.198128671918067);
     RealType b3 = static_cast<RealType>(-0.0400061921041533);
@@ -256,13 +257,13 @@ void SRKN_b_order_11(RandomAccessContainer& p0, RandomAccessContainer& q0, const
 
     std::array<RealType, 11> b_weights = {b1, b2, b3, b4, b5, b6, b6, b5, b4, b3, b2};
     std::array<RealType, 11> a_weights = {a1, a2, a3, a4, a5, a6, a5, a4, a3, a2, a1};
-    
+
     RealType a, b;
     for (std::size_t i=0; i < b_weights.size(); i++)
     {
         b = b_weights[i];
         a = a_weights[i];
-        
+
         RandomAccessContainer dq = dHdp(p0);
         mult_prefactor(dq, dt * b);
         add(q0, dq);
@@ -299,11 +300,11 @@ std::pair<std::vector<RandomAccessContainer>, std::vector<RandomAccessContainer>
     size_check(p0, q0, function, pol);
     #ifdef _MSC_VER
         #  pragma warning(pop)
-    #endif 
+    #endif
 
     typedef void (*stepperType)(RandomAccessContainer&, RandomAccessContainer&, RealType, Func, Func);
 
-    stepperType stepper; 
+    stepperType stepper;
     switch (method) {
         case available_methods::Y6:       stepper = sixth_order_yoshida; break;
         case available_methods::Y4:       stepper = fourth_order_yoshida; break;
@@ -338,7 +339,7 @@ BOOST_MATH_EXPORT std::pair<std::vector<RandomAccessContainer>, std::vector<Rand
                                                                                                                            available_methods method,
                                                                                                                            const Policy& pol)
 {
-    return detail::integrate_hamiltonian_imp(p0, q0, dt, steps, dHdp, dHdq, method, pol); 
+    return detail::integrate_hamiltonian_imp(p0, q0, dt, steps, dHdp, dHdq, method, pol);
 }
 
 template <typename RandomAccessContainer, typename RealType, class Func>
@@ -350,7 +351,7 @@ BOOST_MATH_EXPORT std::pair<std::vector<RandomAccessContainer>, std::vector<Rand
                                                                                                                            Func dHdq,
                                                                                                                            available_methods method)
 {
-    return integrate_hamiltonian(p0, q0, dt, steps, dHdp, dHdq, method, boost::math::policies::policy<>()); 
+    return integrate_hamiltonian(p0, q0, dt, steps, dHdp, dHdq, method, boost::math::policies::policy<>());
 }
 
 template <typename RandomAccessContainer, typename RealType, class Func>
@@ -361,8 +362,56 @@ BOOST_MATH_EXPORT std::pair<std::vector<RandomAccessContainer>, std::vector<Rand
                                                                                                                            Func dHdp,
                                                                                                                            Func dHdq)
 {
-    return integrate_hamiltonian(p0, q0, dt, steps, dHdp, dHdq, available_methods::Y6, boost::math::policies::policy<>()); 
+    return integrate_hamiltonian(p0, q0, dt, steps, dHdp, dHdq, available_methods::Y6, boost::math::policies::policy<>());
 }
+
+template <typename RealType, class Func, class Policy>
+BOOST_MATH_EXPORT boost::math::interpolators::cubic_hermite<std::vector<RealType> > integrate_hamiltonian_cubic(RealType p0,
+                                                                                                                RealType q0,
+                                                                                                                const RealType dt,
+                                                                                                                const unsigned steps,
+                                                                                                                Func dHdp,
+                                                                                                                Func dHdq,
+                                                                                                                available_methods method,
+                                                                                                                const Policy& pol)
+{
+    std::vector<RealType> p;
+    std::vector<RealType> q;
+    std::tie(p, q) = detail::integrate_hamiltonian_imp(p0, q0, dt, steps, dHdp, dHdq, method, pol);
+
+    std::vector<RealType> dpdt(p.size());
+    std::vector<RealType> time(p.size(), 0);
+    for (std::size_t i=0; i < p.size(); i++)
+    {
+        dpdt[i] = -dHdq(p[i]);
+        time[i] = i * dt;
+    }
+    return boost::math::interpolators::cubic_hermite<std::vector<RealType> >(std::move(time), std::move(p), std::move(dpdt));
+}
+
+template <typename RealType, class Func>
+BOOST_MATH_EXPORT boost::math::interpolators::cubic_hermite<std::vector<RealType> > integrate_hamiltonian_cubic(RealType p0,
+                                                                                                                RealType q0,
+                                                                                                                const RealType dt,
+                                                                                                                const unsigned steps,
+                                                                                                                Func dHdp,
+                                                                                                                Func dHdq,
+                                                                                                                available_methods method)
+{
+    return integrate_hamiltonian_cubic(p0, q0, dt, steps, dHdp, dHdq, method, boost::math::policies::policy<>());
+}
+
+template <typename RealType, class Func>
+BOOST_MATH_EXPORT boost::math::interpolators::cubic_hermite<std::vector<RealType> > integrate_hamiltonian_cubic(RealType p0,
+                                                                                                                RealType q0,
+                                                                                                                const RealType dt,
+                                                                                                                const unsigned steps,
+                                                                                                                Func dHdp,
+                                                                                                                Func dHdq)
+{
+    return integrate_hamiltonian_cubic(p0, q0, dt, steps, dHdp, dHdq, available_methods::Y6, boost::math::policies::policy<>());
+}
+
 }}}
 
 #endif
